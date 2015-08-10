4af49b377b04e7ea94f8ebbbb1081e93b31668ce
Paul Ossientis <paul_ossientis@riseup.net>
2015-08-10 11:45:51 +0300

    modified:   c/test.c

--- c/test.c
+++ c/test.c
@@ -9,18 +9,13 @@ int main(int argc, char * argv[])
 
   using namespace std;
 
-  long e;
-  int a;
-  int b;
-  short c;
-  short d;
-  char f;
-  long g;
+  char a;
+  short b;
+  int c;
+  long d;
 
-  cout << "One long, two int, two short and one char" << endl;
 
-  cout << hex << &e << endl << &a << endl << &b << endl << &c << endl << &d << endl;
-  cout << hex << (void*) &f << endl << &g << endl;
+  uint32_t e;
 
 
   return 0;
