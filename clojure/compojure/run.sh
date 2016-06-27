3d40b1e5a179252fe57416c024abf216f24da7a6
Paul Ossientis <paul_ossientis@riseup.net>
2016-06-27 14:31:36 +0100

    new file:   clojure/compojure/README 	new file:   clojure/compojure/clojure 	new file:   clojure/compojure/clojure-1.6.0.jar 	new file:   clojure/compojure/clojure-contrib.jar 	new file:   clojure/compojure/clout 	new file:   clojure/compojure/commons-codec.jar 	renamed:    clojure/compojure -> clojure/compojure/compojure 	new file:   clojure/compojure/instaparse 	new file:   clojure/compojure/medley 	new file:   clojure/compojure/ring 	new file:   clojure/compojure/run.sh 	renamed:    clojure/test_compojure.clj -> clojure/compojure/test_compojure.clj

---
+++ clojure/compojure/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+COMPOJURE_JARS=
+for d in `dirname $0`/*.jar
+do
+    COMPOJURE_JARS="$d:$COMPOJURE_JARS"
+done
+
+echo $COMPOJURE_JARS
+
+clojurec -cp "$COMPOJURE_JARS" "$@" 1> /dev/null
+# java -cp "$COMPOJURE_JARS" "$@"
+rm *.class; rm compojure/*.class
