f9f134671c9c066c28f5c326042118394a537382
Paul Ossientis <paul_ossientis@riseup.net>
2016-12-05 18:07:49 +0000

    modified:   test.sh

--- test.sh
+++ test.sh
@@ -94,6 +94,14 @@ then
   exit 1
 fi
 
+echo 'Testing coq ...'
+${HOME}/coq/test.sh >> test.log 2>&1
+if [ $? -ne 0 ]
+then
+  echo 'TESTING FAILED !!!'
+  exit 1
+fi
+
 
 #echo 'Testing Number ...'
 #${BITCOIN}/Number/test.sh >> test.log  2>&1
