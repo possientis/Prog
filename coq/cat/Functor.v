759da30473f2000b37997eb400c809e99a823d53
Paul Ossientis <paul ossientis@riseup.net>
2018-05-05 16:40:53 +0100

    jessie coq fix

--- coq/cat/Functor.v
+++ coq/cat/Functor.v
@@ -13,7 +13,7 @@ Record Functor (C D:Category) : Type := functor
     }
     .
 
-Arguments Func_ {C} {D} _.
+Arguments Func_ {C} {D} _ _.
 
 Lemma Functor_obj_ : forall (C D:Category)(F:Functor C D)(a:Arr C),
     source a == a -> source (Func_ F a) == Func_ F a.
@@ -53,8 +53,10 @@ Qed.
 
 Arguments lift_source_ {C} {D} {a} {b} _ _.
      
+Open Scope categ.
+
 Definition lift_target_:forall (C D:Category)(a b:Obj C)(F:Functor C D)(f:Hom a b),
-    target (lift_arrow_ F f) == arr F$b.
+    target (lift_arrow_ F f) == arr F $ b.
 Proof.
     intros C D [a Ha] [b Hb] F [f p q]. simpl. simpl in q.
     apply trans with (Func_ F (target f)).
@@ -64,8 +66,8 @@ Qed.
 
 Arguments lift_target_ {C} {D} {a} {b} _ _.
 
-Definition lift(C D:Category)(a b:Obj C)(F:Functor C D)(f:Hom a b):Hom (F$a) (F$b)
-    := hom (lift_arrow_ F f)(lift_source_ F f)(lift_target_ F f).
+Definition lift(C D:Category)(a b:Obj C)(F:Functor C D)(f:Hom a b):
+  Hom (F $ a) (F $ b) := hom (lift_arrow_ F f)(lift_source_ F f)(lift_target_ F f).
 
 Arguments lift {C} {D} {a} {b} _ _.
 
@@ -73,7 +75,7 @@ Notation "F <$> f" := (lift F f) (at level 0, right associativity) : categ.
 
 
 Lemma functor_id : forall (C D:Category) (F:Functor C D) (a:Obj C),
-    i F<$>(id a) == i (id F$a).
+    i F<$>(id a) == i (id F $ a).
 Proof. intros C D F [a Ha]. simpl. apply refl. Qed.
 
 Lemma functor_law : forall (C D:Category) (F:Functor C D) (a b c:Obj C),
