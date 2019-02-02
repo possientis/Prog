a5ef7bcab340887d49329981e50dd34935963846
Paul Ossientis <paul_ossientis@riseup.net>
2019-02-02 15:47:48 +0000

    small change to clean.sh

--- coq/cpdt/clean.sh
+++ coq/cpdt/clean.sh
@@ -4,9 +4,8 @@ DIR=`pwd`
 HOME=/home/john/Prog/coq/cpdt
 cd ${HOME}
 
-rm -f ./{*,.*}.{aux,glob,vo} 
-rm -f Test/{*,.*}.{aux,glob,vo} 
-rm -f Utils/{*,.*}.{aux,glob,vo} 
+rm -f ./{*,.*}.{aux,glob,vo}
+rm -f */{*,.*}.{aux,glob,vo}
 
 
 cd ${DIR}
