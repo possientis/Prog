93bf8e3c82063bf4fa91b46b9402d8839a6ba449
Paul Ossientis <paul_ossientis@riseup.net>
2019-03-16 08:04:27 +0000

    coq

---
+++ coq/cpdt/unit.v
@@ -0,0 +1,46 @@
+Print unit.
+
+(*
+Inductive unit : Set :=  tt : unit
+*)
+
+
+Print True.
+
+(*
+Inductive True : Prop :=  I : True
+*)
+
+(* it would seem that the types unit and True are isomorphic *)
+
+Definition toUnit (x:True) : unit := tt.
+
+Definition toTrue (x:unit) : True := I.
+
+Lemma check1 : forall (x:True), toTrue (toUnit x) = x.
+Proof. intros x. unfold toTrue. destruct x. reflexivity. Qed.
+
+Lemma check2 : forall (x:unit), toUnit (toTrue x) = x.
+Proof. intros x. unfold toUnit. destruct x. reflexivity. Qed.
+
+
+Definition unitToBool (x:unit) : bool :=
+    match x with
+    | tt            => true
+    end.
+
+Lemma check3 : forall (x:unit), unitToBool x = true.
+Proof. intros x. destruct x. reflexivity. Qed.
+
+
+Definition TrueToBool (x:True) : bool :=
+    match x with
+    | I             => true
+    end.
+
+Lemma check4 : forall (x:True), TrueToBool x = true.
+Proof. intros x. destruct x. reflexivity. Qed.
+
+
+
+
