b1d0857101e43ac7d81c046a01293ed738d6d046
Paul Ossientis <paul_ossientis@riseup.net>
2019-04-05 17:46:01 +0100

    more

--- haskell/song/Main.hs
+++ haskell/song/Main.hs
@@ -2,30 +2,9 @@ module  Main
     (   main
     )   where
 
-import Prelude hiding ((+),(*))
-import F13
-import Field
-
-a :: F13
-a  = F13 7
-
-b :: F13
-b  = F13 6
+import Test.QuickCheck
 
 main :: IO ()
 main = do
-    putStrLn $ "a = " ++ show a
-    putStrLn $ "b = " ++ show b
-    putStrLn $ "a + b = " ++ show (a + b)
-    putStrLn $ "a * b = " ++ show (a * b)
-    putStrLn $ "-a = " ++ show (neg a)
-    putStrLn $ "-b = " ++ show (neg b)
-    putStrLn $ "a^(-2) = " ++ show (pow a (-2))
-    putStrLn $ "a^(-1) = " ++ show (pow a (-1))
-    putStrLn $ "a^0 = " ++ show (pow a 0)
-    putStrLn $ "a^1 = " ++ show (pow a 1)
-    putStrLn $ "a^2 = " ++ show (pow a 2)
-    putStrLn $ "a^3 = " ++ show (pow a 3)
-    putStrLn $ "inv 1 =  " ++ show (inv one :: F13)
-    putStrLn $ "inv a =  " ++ show (inv a)
-    putStrLn $ "inv b =  " ++ show (inv b)
+    ls <- generate $ vectorOf 20 arbitrary :: IO [Int]
+    print ls
