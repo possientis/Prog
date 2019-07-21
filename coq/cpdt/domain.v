5884127a4ad01749ab825914b0b9864ab8483767
Paul Ossientis <paul ossientis@riseup.net>
2019-07-21 17:29:44 +0100

    coq cpdt

--- coq/cpdt/domain.v
+++ coq/cpdt/domain.v
@@ -26,7 +26,7 @@ Arguments run {a} _ _.
 (* defined the proof separately as some sort of lemma and defined the value     *)
 (* 'bot' in the usual direct way by referring to this lemma                     *)
 Definition bot (a:Type) : Computation a.
-    unfold Computation. exists (fun (_:nat) => @None a).
+    unfold Computation. exists (fun (_:nat) => None).
     unfold monotone. intros n v H. inversion H.
 Defined.
 
@@ -42,20 +42,49 @@ Defined.
 
 Arguments pure {a} _.
 
+Lemma run_pure : forall (a:Type) (v:a), run (pure v) v. 
+Proof.
+    intros a v. unfold run. exists 0. reflexivity.
+Qed.
 
 Definition bind(a b:Type)(k:Computation a)(g:a -> Computation b):Computation b.
-    destruct k as [f p].
-    remember ( fun (n:nat) =>
-        match f n with
-        | Some v    => proj1_sig (g v) n
-        | None      => @None b
-        end) as h eqn:H.
-    unfold Computation. exists h.
-    unfold monotone. intros n v E m H'.
-    destruct (f n) as [w q|H1] eqn:H2.
-        - rewrite H in E. rewrite H2 in E. rewrite H.
-          destruct (f m) as [x r|I1] eqn:I2.
-          + unfold monotone in p.
+    unfold Computation. 
+    remember (fun (n:nat) =>
+        match proj1_sig k n with
+        |   Some va => proj1_sig (g va) n
+        |   None    => None
+        end) as gf eqn:GF.
+    exists gf.
+    unfold monotone. 
+    intros n vb H m I.
+    destruct k as [f Mf]. simpl in GF. 
+    unfold monotone in Mf.
+    rewrite GF in H. rewrite GF.
+    destruct (f n) as [va|] eqn:Fn.
+        - destruct (f m) as [va'|] eqn:Fm.
+            + assert (f m = Some va) as E. 
+                { apply Mf with n; assumption. }
+              rewrite Fm in E. inversion E.
+              destruct (g va) as [g' Mg']. simpl. simpl in H.
+              unfold monotone in Mg'. apply Mg' with n; assumption.
+            + assert (f m = Some va) as E.
+                { apply Mf with n; assumption. }
+              rewrite Fm in E. inversion E.
+        - inversion H.
+Defined.
+
+Arguments bind {a} {b} _ _.
+
+
+Notation "k >>= g" := (bind k g) (at level 50).
+
+(*
+Lemma run_bind : forall (a b:Type) (k:Computation a) (g:a -> Computation b),
+    forall (x:a) (y:b), run k x -> run (g x) y -> run (k >>= g) y.
+Proof.
 
 
 Show.
+*)
+
+
