dda983dcd3848c3733c5d043f15b14d7a506edb3
Paul Ossientis <paul_ossientis@riseup.net>
2019-07-26 19:15:04 +0100

    automate guile test setup

---
+++ scheme/guile/setup.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+root="/home/john/Prog/scheme"
+
+current=$(pwd)
+
+cd /usr/share/guile/2.0/
+
+sudo mkdir -p foo
+cd foo
+sudo ln -nfs ${root}/guile/bar-module.scm bar-module.scm
+cd ..
+
+sudo mkdir -p math
+cd math
+sudo ln -nfs ${root}/guile/bessel.scm bessel.scm
+cd ..
+
+sudo mkdir -p bitcoin
+cd bitcoin
+sudo ln -nfs ${root}/bitcoin/secp256k1/secp256k1.scm secp256k1.scm
+
+cd /usr/lib/x86_64-linux-gnu/guile/2.0
+sudo mkdir -p extensions
+cd extensions
+sudo ln -nfs /home/john/Prog/scheme/guile/libguile-bessel.so libguile-bessel.so
+
+cd ${current}
