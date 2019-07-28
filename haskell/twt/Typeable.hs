44d5a483c5d6f24ac1eb76a8bf69c191878cdead
Paul Ossientis <paul ossientis@riseup.net>
2019-07-28 09:00:09 +0100

    haskell: twt

--- haskell/twt/Typeable.hs
+++ haskell/twt/Typeable.hs
@@ -2,6 +2,24 @@ import Data.Proxy
 import Data.Typeable
 
 -- typeRep :: Typeable a => proxy a -> TypeRep 
+-- 'proxy' is a type variable
+-- so you have more flexibility than just passing
+-- a value of type 'Proxy a' , you can use a a value
+-- of type '[a]'
 
---x1 :: TypeRep
---x1 = typeRep (Proxy :: Int)
+x1 :: TypeRep
+x1 = typeRep [1::Int]   -- Proxy :: Proxy Int 
+
+x2 :: TypeRep 
+x2 = typeRep (Proxy :: Proxy Double)
+
+x3 :: TypeRep 
+x3 = typeRep (Proxy :: Proxy [Int -> Double])
+
+
+
+main :: IO ()
+main = do
+    putStrLn $ "typeRep for Int: "                  ++ show x1
+    putStrLn $ "typeRep for Double: "               ++ show x2
+    putStrLn $ "typeRep for [Int -> Double]: "      ++ show x3
