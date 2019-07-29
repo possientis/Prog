4c298dcc6f6574b7fc359195331ab1c44c7a8974
Paul Ossientis <paul_ossientis@riseup.net>
2019-07-29 18:53:54 +0100

    remove pycoin test

--- python/bitcoin/test.sh
+++ python/bitcoin/test.sh
@@ -20,9 +20,9 @@ echo "testing utxo selection ..."
 python3 select_utxo.py
 echo
 
-echo "testing pycoin installation ..."
-python3 pycoin_test.py
-echo
+#echo "testing pycoin installation ..."
+#python3 pycoin_test.py
+#echo
 
 #echo "testing ku with private key from Mastering Bitcoin ..."
 #ku -j KxFC1jmwwCoACiCAWZ3eXa96mBM6tb3TYzGmf6YwgdGWZgawvrtJ
