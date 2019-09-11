48f3f912c6e94bd175ca264cdea82ced6251209f
Paul Ossientis <paul_ossientis@riseup.net>
2019-09-11 07:45:48 +0100

    haskell twt

---
+++ haskell/twt/Printf.hs
@@ -0,0 +1,40 @@
+{-# LANGUAGE PolyKinds              #-}
+{-# LANGUAGE DataKinds              #-}
+{-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE TypeOperators          #-}
+{-# LANGUAGE KindSignatures         #-}
+{-# LANGUAGE TypeApplications       #-}
+{-# LANGUAGE FlexibleInstances      #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
+
+
+import Data.Kind
+import Data.Proxy
+import GHC.TypeLits
+
+data (a :: k1) :<< (b :: k2)
+infixr 5 :<<
+
+type T1 = Int :<< ":" :<< Bool :<< "!"
+
+class HasPrintf a where
+    type Printf a :: Type
+    format :: String -> Proxy a -> Printf a
+
+
+instance KnownSymbol text => HasPrintf (text :: Symbol) where
+    type Printf text = String
+    format s _ = s ++ symbolVal (Proxy @text)
+
+instance (HasPrintf a, KnownSymbol text) => 
+    HasPrintf ((text :: Symbol) :<< a) where
+    type Printf (text :<< a) = Printf a
+    format s _ = format (s ++ symbolVal (Proxy @text)) (Proxy @a)
+
+instance (HasPrintf a, Show param) => 
+    HasPrintf ((param :: Type) :<< a) where
+    type Printf (param :<< a) = param -> Printf a
+    format s _ param = format (s ++ show param) (Proxy @a)
+
+-- kind! T2 = Int -> Bool -> String
+type T2 = Printf T1
