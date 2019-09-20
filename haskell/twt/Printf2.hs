3902eebfcfedc36bb80894c394408c5627f7cb30
Paul Ossientis <paul_ossientis@riseup.net>
2019-09-20 07:46:47 +0100

    haskell twt

--- haskell/twt/Printf2.hs
+++ haskell/twt/Printf2.hs
@@ -1,15 +1,17 @@
 {-# LANGUAGE DataKinds              #-}
 {-# LANGUAGE TypeFamilies           #-}
+{-# LANGUAGE ScopedTypeVariables    #-}
 
 import Data.Kind
 import Data.Proxy
+import GHC.TypeLits
 
 data Format
     = Number Format
     | Str    Format
     | Chr    Format
     | Dbl    Format
-    | Lit    String Format
+    | Lit    Symbol Format
     | End
 
 
@@ -21,11 +23,30 @@ type family PrintfType (t :: Format) :: Type where
     PrintfType ('Lit _  fmt) =             PrintfType fmt
     PrintfType 'End          =             String
 
+
 class HasPrintf (t :: Format) where
     printfFmt :: Proxy t -> String -> PrintfType t
 
 instance HasPrintf 'End where
     printfFmt _ acc = acc
+
+instance HasPrintf (t :: Format) => HasPrintf ('Number t) where
+    printfFmt _ acc = \i -> printfFmt (Proxy :: Proxy t) (acc ++ show i)
+
+instance HasPrintf (t :: Format) => HasPrintf ('Str t) where
+    printfFmt _ acc = \s -> printfFmt (Proxy :: Proxy t) (acc ++ s)
+
+instance HasPrintf (t :: Format) => HasPrintf ('Chr t) where
+    printfFmt _ acc = \c -> printfFmt (Proxy :: Proxy t) (acc ++ show c)
+
+instance HasPrintf (t :: Format) => HasPrintf ('Dbl t) where
+    printfFmt _ acc = \d -> printfFmt (Proxy :: Proxy t) (acc ++ show d)
+
+instance (KnownSymbol str, HasPrintf (t :: Format)) => 
+    HasPrintf ('Lit str t) where
+        printfFmt (Proxy :: Proxy ('Lit str t)) acc = printfFmt 
+            (Proxy :: Proxy t) 
+            (acc ++ symbolVal (Proxy :: Proxy str))
 
 
 {-
