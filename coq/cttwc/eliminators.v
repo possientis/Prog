28fe6a617a2da2864b0a1692accec7fa257b908a
Paul Ossientis <paul_ossientis@riseup.net>
2020-03-24 08:40:30 +0000

    coq: cttwc

--- coq/cttwc/eliminators.v
+++ coq/cttwc/eliminators.v
@@ -27,3 +27,60 @@ Definition L2 : forall (x:bool), negb (negb x) = x :=
         (eq_refl true) 
         (eq_refl false) 
         x.
+
+
+Definition BoolRec : forall (p:bool -> Type), p true -> p false -> forall (x:bool), p x :=
+    fun (p:bool -> Type) => 
+        fun (tt:p true) =>
+            fun (ff: p false) =>
+                fun (x:bool) =>
+                    match x with
+                    | true  => tt
+                    | false => ff
+                    end.
+
+Definition not : bool -> bool := BoolRec (fun (x:bool) => bool) false true.
+
+
+Definition L3 : not = fun(x:bool) => match x with true => false | false => true end :=
+    eq_refl not.
+
+Definition BoolE' : forall (p:bool -> Prop), p true -> p false -> forall (x:bool), p x :=
+    fun (p:bool -> Prop) => BoolRec p.
+    
+Variable BoolE1 : forall (p:bool -> Prop), p true -> p false -> forall (x:bool), p x.
+
+Definition L4 : forall (x:bool), x = true \/ x = false :=
+    fun (x:bool) =>
+        match x with
+        | true  => or_introl (eq_refl true)
+        | false => or_intror (eq_refl false)
+        end.
+
+Definition L5 : forall (x:bool), x = true \/ x = false := BoolE1 
+    (fun (x:bool) => x = true \/ x = false) 
+    (or_introl (eq_refl true))
+    (or_intror (eq_refl false)).
+
+Definition L6 : forall (p:bool -> Prop), (forall (x y:bool), y = x -> p x) -> forall (x:bool), p x :=
+    fun (p:bool -> Prop) => 
+        fun (q:forall (x y:bool), y = x -> p x) =>
+            fun (x:bool) => q x x (eq_refl x).
+
+Definition type (x:bool) : Type :=
+    match x with
+    | true  => x = true
+    | false => x = false
+    end.
+
+Definition refl : forall (x:bool), type x := BoolRec type (eq_refl true) (eq_refl false). 
+
+Definition L7 : forall (p:bool -> Prop) (x:bool), (x = true -> p true) -> (x = false -> p false) -> p x :=
+    fun (p:bool -> Prop) =>
+        fun (x:bool) =>
+            fun (tt:x = true -> p true) =>
+                fun (ff:x = false -> p false) =>
+                    match x with
+                    | true  => tt (refl x)
+                    | false => ff (refl x)
+                    end.
