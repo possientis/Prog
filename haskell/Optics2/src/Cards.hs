cd52cca8d9375d1b047aa8d35d0928a690344447
Paul Ossientis <paul_ossientis@riseup.net>
2020-03-25 18:52:22 +0000

    haskell: optics

--- haskell/Optics2/src/Cards.hs
+++ haskell/Optics2/src/Cards.hs
@@ -9,6 +9,7 @@ module Cards
     ,   moveName
     ,   movePower
     ,   deck
+    ,   ex1, ex2, ex3, ex4, ex5
     )   where
 
 import Control.Lens
@@ -33,6 +34,50 @@ makeLenses ''Move
 
 
 deck :: [Card]
-deck =  [Card "Skortul"     Wet False   [Move "Squirt" 20]
-        ,Card "Scorchander" Hot False   [Move "Scorch" 20]
+deck =  [Card "Skortul"     Wet     False   [Move "Squirt"          20]
+        ,Card "Scorchander" Hot     False   [Move "Scorch"          20]
+        ,Card "Seedasaur"   Leafy   False   [Move "Allergize"       20]
+        ,Card "Kapichu"     Spark   False   [Move "Poke"            10
+                                            ,Move "Zap"             30]
+        ,Card "Elecdude"    Spark   False   [Move "Asplode"         50]
+        ,Card "Garydose"    Wet     True    [Move "Gary's move"     40]
+        ,Card "Moisteon"    Wet     False   [Move "Soggy"            3]
+        ,Card "Grasseon"    Leafy   False   [Move "Leaf Cut"        30]
+        ,Card "Spicyeon"    Hot     False   [Move "Capsaicisize"    40]
+        ,Card "Sparkeon"    Spark   True    [Move "Shock"           40
+                                            ,Move "Battery"         50]
         ]
+
+-- How many Spark cards do I have
+ex1 :: Int
+ex1 = lengthOf (folded . aura . filtered (== Spark)) deck
+
+-- How many moves have an attack power above 30
+ex2 :: Int
+ex2 = lengthOf (folded . moves . folded . movePower . filtered (> 30)) deck
+
+-- List all cards which have any move with an attack power greater than 40
+ex3 :: [String]
+ex3 = deck 
+    ^.. folded 
+      . filtered (anyOf (moves . folded . movePower) (>40)) 
+      . name
+
+-- How many moves do my Spark cards have in total
+ex4 :: Int
+ex4 = lengthOf ( folded     -- fold over each card
+               . filtered ((== Spark) . _aura)
+               . moves      -- select moves
+               . folded     -- fold over all moves
+               ) deck
+
+
+-- List all my Spark moves with a power greater than 30
+ex5 :: [String]
+ex5 = toListOf ( folded     -- fold over each card
+               . filtered ((== Spark) . _aura)
+               . moves
+               . folded
+               . filtered ((>30) . _movePower)
+               . moveName
+               ) deck
