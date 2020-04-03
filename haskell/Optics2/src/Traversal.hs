9d99cde19f840b49f40a4893ef1bdce6fb940988
Paul Ossientis <paul_ossientis@riseup.net>
2020-04-03 13:00:20 +0100

    haskell: optics

--- haskell/Optics2/src/Traversal.hs
+++ haskell/Optics2/src/Traversal.hs
@@ -1,6 +1,8 @@
 module  Traversal
     (   ex1, ex2, ex3, ex4, ex5, ex6, ex7, ex8, ex9, ex10
-    ,   ex11, ex12, ex13, ex14, ex15, ex16
+    ,   ex11, ex12, ex13, ex14, ex15, ex16, ex17, ex18, ex19, ex20
+    ,   ex21, ex22, ex23, ex24, ex25, ex26, ex27, ex28, ex29, ex30
+    ,   ex31, ex32
     )   where
 
 import Data.Char        (toUpper)
@@ -90,3 +92,85 @@ ex17 = opticsTree
      %~ reverse
 
 
+-- simple
+-- worded :: Traversal' String String
+-- lined  :: Traversal' String String
+
+-- real
+-- worded :: Applicative f => IndexedLensLike' Int f String String
+-- lined  :: Applicative f => IndexedLensLike' Int f String String
+
+ex18 :: [String]
+ex18 = "I'll be back!" ^.. worded 
+
+ex19 :: [String]
+ex19 = "Run\nForrest\nRun" ^.. lined
+
+-- surroundeach word with '*'s
+ex20 :: String
+ex20 = "blue suede shoes" & worded %~ \s -> "*" ++ s ++ "*"
+
+ex21 :: String
+ex21 = "blue suede shoes" & worded %~ \(x:xs) -> toUpper x : xs
+
+-- add a '#' at the start of each line
+ex22 :: String
+ex22 = "blue\nsuede\nshoes" & lined %~ ('#':)
+
+
+-- simple
+-- beside :: Traversal s t a b -> Traversal s' t' a b -> Traversal (s,s') (t,t') a b
+-- beside :: Lens s t a b -> Lens s' t' a b -> Traversal (s,s') (t,t') a b
+-- beside :: Fold s a -> Fold s' a -> Fold (s,s') a
+
+dinos :: (String, (Int, String))
+dinos = ("T-Rex", (42, "Stegosaurus"))
+
+ex23 :: [String]
+ex23 = dinos ^.. beside id _2
+
+
+numbers :: ([(Int,Int)], [Int])
+numbers = ([(1,2),(3,4)],[5,6,7])
+
+ex24 :: [Int]
+ex24 = numbers ^.. beside (traversed . both) traversed
+
+ex25 :: [String]
+ex25 = ("T-Rex", ("Ankylosaurus","Stegosaurus")) ^.. beside id both
+
+ex26 :: (String, [String])
+ex26 = ("Cowabunga", ["let's", "order", "pizza"])
+     & beside traversed (traversed . traversed)
+     %~ toUpper
+
+traversal1 :: Traversal' (Either (Int, Int) [Int]) Int
+traversal1 = beside both traversed
+
+ex27 :: Either (Int, Int) [Int]
+ex27 = Left (1, 2) & traversal1 %~ negate
+
+
+ex28 :: Either (Int, Int) [Int]
+ex28 = Right [3,4] & traversal1 %~ negate
+
+-- element :: Traversable f => Int -> Traversal' (f a) a
+
+
+ex29 :: Maybe Int
+ex29 = [0,1,2,3,4] ^? element 2
+
+ex30 :: [Int]
+ex30 = [0,1,2,3,4] & element 2 %~ (*100)
+
+-- elementOf :: Traversal' s a -> Int -> Traversal' s a
+-- elementOf :: Fold s a       -> Int -> Fold s a
+
+
+-- element is basically 'elementOf traversed'
+ex31 :: Maybe Int
+ex31 = [0,1,2,3,4] ^? elementOf traversed 2
+
+
+ex32 :: Maybe Int
+ex32 = [[0,1,2],[3,4],[5,6,7,8]] ^? elementOf (traversed . traversed) 6
