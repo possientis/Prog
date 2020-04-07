df2ee9c5232470625ad768f752025904a8d211e0
Paul Ossientis <paul_ossientis@riseup.net>
2020-04-07 07:41:32 +0100

    coq: cttwc

--- coq/cttwc/nat.v
+++ coq/cttwc/nat.v
@@ -26,3 +26,46 @@ refine ( fun(n:nat) =>
 ).
 Qed.
 
+Definition z_not_s : forall (n:nat), 0 = S n -> False :=
+    fun (n:nat) (e:0 = S n) =>
+        subst nat (fun (m:nat) =>
+            match m with
+            | 0     => True
+            | S _   => False
+            end) 0 (S n) e I.
+
+Definition sinj : forall (m n:nat), S m = S n -> m = n :=
+    fun (m n:nat) (e:S m = S n) => 
+        subst nat (fun (k:nat) =>
+            match k with
+            | 0     => True
+            | S k   => m = k
+            end) (S m) (S n) e (eq_refl m).
+
+
+Definition L2 : forall (n:nat), S n = n -> False.
+Proof.
+refine ( NatE (fun (n:nat) => S n = n -> False) 
+    (fun (e:1 = 0) => z_not_s 0 (eq_sym e))
+    (fun (n:nat) (IH:S n = n -> False) (e:S (S n) = S n) => 
+        IH (sinj (S n) n e)
+)).
+Qed.
+
+Definition L3 : forall (n k:nat), n + S k = n -> False.
+Proof.
+refine ( NatE (fun (n:nat) => forall (k:nat), n + S k = n -> False)
+    (fun (k:nat) (e:0 + S k = 0) => z_not_s k (eq_sym e))
+    (fun (n:nat) (IH:forall (k:nat), n + S k = n -> False) =>
+        fun (k:nat) (e:S n + S k = S n) => IH k (sinj (n + S k) n e) 
+)).
+Qed.
+
+Definition L4 : forall (m n p:nat), m + n = m + p -> n = p.
+Proof.
+refine ( NatE (fun (m:nat) => forall (n p:nat), m + n = m + p -> n = p)
+    (fun(n p:nat) (e:0 + n = 0 + p) => e)
+    (fun(m:nat) (IH:forall (n p:nat), m + n = m + p -> n = p) =>
+        fun (n p:nat) (e:S m + n = S m + p) => IH n p (sinj (m + n) (m + p) e)
+)).
+Qed.
