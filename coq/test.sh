7a5843f25386efa84539d0672b43feb0aca6666d
Paul Ossientis <paul_ossientis@riseup.net>
2020-04-23 08:44:58 +0100

    coq test clean up

--- coq/test.sh
+++ coq/test.sh
@@ -18,44 +18,8 @@ echo "testing coq..."
 ./cpdt/test.sh
 ./logic/test.sh
 
-echo
-echo "testing sort..."
-
-# coqtop introduces change of behaviour for -I option between 8.4 and 8.5 
-# Need to figure out version number to create portable script
-case `coqtop --version | grep '8\.'` in
-  *8.4*) VERSION=8.4;;
-  *8.6*) VERSION=8.6;;
-  *)     VERSION=0;;
-esac
-
-# compiling sample library files
-cd lib
-coqc *.v
-cd ..
-
-# running main test file with appropriate 8.4 or 8.5 syntax
-if [ ${VERSION} = 8.4 ];
-then
-
-  coqtop -batch -l sort.v -I lib
-
-elif [ ${VERSION} = 8.6 ];
-then
-
-  coqtop -batch -l sort.v -R lib ''
-
-else
-
-  echo '\ncoqtop version is not supported'
-  exit 1
-
-fi
-
-# clean up
 ./clean.sh
-echo "test completed succesfully"
- 
+
 cd ${DIR}
 echo '\nAll coq tests completed successfully\n'
 
