0998ac8699950ebfd5ab9ed5cd51ecaff51d00f7
Paul Ossientis <paul_ossientis@riseup.net>
2020-04-23 07:59:19 +0100

    coq: cttwc

--- coq/cttwc/existential.v
+++ coq/cttwc/existential.v
@@ -2,8 +2,7 @@
 Variable Exists   : forall (a:Type), (a -> Prop) -> Prop. 
 Variable mkExists : forall (a:Type) (p:a -> Prop) (x:a), p x -> Exists a p.
 Variable ExistsInd : forall (a:Type) (p:a -> Prop) (c:Exists a p -> Prop), (forall (x:a)(q:p x), c (mkExists a p x q)) -> forall (e:Exists a p), c e.
-Definition ExistsE : forall (a:Type) (p:a -> Prop) (A:Prop), 
-    (forall (x:a), p x -> A) -> Exists a p -> A := 
+Definition ExistsE : forall (a:Type) (p:a -> Prop) (A:Prop), (forall (x:a), p x -> A) -> Exists a p -> A := 
     fun a p A H e => ExistsInd a p (fun _ => A) H e.
 
 (* THese primitives exist                                                       *)
@@ -85,3 +84,54 @@ refine (
 ).
 Defined.
 
+Definition L5 : forall (a:Type) (p q : a -> Prop),
+    Exists (fun x => p x \/ q x) <-> Exists p \/ Exists q.
+Proof.
+    intros a p q. split.
+    - intros H. apply (ExistsE a (fun x => p x \/ q x)).
+        + intros x [H'|H'].
+            { left.  apply (mkExists x). exact H'. }
+            { right. apply (mkExists x). exact H'. }
+        + exact H.
+    - intros [H|H].
+        + apply (ExistsE a p).
+            { intros x H'. apply (mkExists x). left. exact H'. }
+            { assumption. }
+        + apply (ExistsE a q).
+            { intros x H'. apply (mkExists x). right. exact H'. }
+            { assumption. }
+Qed.
+
+Definition L6 : forall (a:Type) (p q : a -> Prop),
+    Exists (fun x => p x \/ q x) <-> Exists p \/ Exists q.
+Proof.
+refine (
+    fun (a:Type) (p q:a -> Prop) => conj
+        (fun (H:Exists (fun x => p x \/ q x)) => 
+            ExistsE a (fun x => p x \/ q x) (Exists p \/ Exists q)
+                (fun (x:a) (H':p x \/ q x) =>
+                    match H' with
+                    | or_introl H'  => or_introl (mkExists x H')
+                    | or_intror H'  => or_intror (mkExists x H')
+                    end) H)
+        (fun (H:Exists p \/ Exists q) => 
+            match H with
+            | or_introl H   => ExistsE a p (Exists (fun x => p x \/ q x)) 
+                (fun (x:a) (H':p x) => mkExists x (or_introl H')) H
+            | or_intror H   => ExistsE a q (Exists (fun x => p x \/ q x))
+                (fun (x:a) (H':q x) => mkExists x (or_intror H')) H
+            end)).
+Defined.
+
+Definition L7 : forall (a:Type) (p:a -> Prop),
+    Exists p -> ~ forall (x:a), ~ p x.
+Proof.
+    intros a p H1 H2. apply (ExistsE a p).
+    - intros x H3. apply (H2 x). exact H3.
+    - exact H1.
+Qed.
+
+Definition L8 : forall (a:Type) (p:a -> Prop), Exists p -> ~ forall (x:a), ~ p x 
+:=  fun (a:Type) (p:a -> Prop) =>
+        fun (H1:Exists p) (H2:forall (x:a), ~ p x) =>
+            ExistsE a p False H2 H1.
