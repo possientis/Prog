82e57b69564333ae1dd913d7e411a15214c49b05
Paul Ossientis <paul_ossientis@riseup.net>
2020-04-25 22:00:14 +0100

    extend idris test

--- idris/test.sh
+++ idris/test.sh
@@ -8,14 +8,77 @@ echo
 echo "testing idris..."
 echo
 
-echo "Hello world..."
-idris Hello.idr --check
+echo "WordLength..."
+idris WordLength.idr --check
+
+echo "Vehicle..."
+idris Vehicle.idr --check
+
+echo "Vect..."
+idris Vect.idr --check
+
+echo "VecSort..."
+idris VecSort.idr --check
+
+echo "TupleVect..."
+idris TupleVect.idr --check
+
+echo "Tree..."
+idris Tree.idr --check
+
+echo "StringOrInt..."
+idris StringOrInt.idr --check
+
+echo "Reverse..."
+idris Reverse.idr --check
+
+echo "replWith..."
+idris replWith.idr --check
+
+echo "Printf..."
+idris Printf.idr --check
+
+echo "Polygon..."
+idris Polygon.idr --check
+
+echo "Overloading..."
+idris Overloading.idr --check
+
+echo "Mutual..."
+idris Mutual.idr --check
+
+echo "Matter..."
+idris Matter.idr --check
 
 echo "Matrix..."
 idris Matrix.idr --check
 
-echo "Tree..."
-idris Tree.idr --check
+echo "Map..."
+idris Map.idr --check
+
+echo "Length..."
+idris Length.idr --check
+
+echo "Hello world..."
+idris Hello.idr --check
+
+echo "Expr..."
+idris Expr.idr --check
+
+echo "ExactLength..."
+idris ExactLength.idr --check
+
+echo "EqNat..."
+idris EqNat.idr --check
+
+echo "DepPairs..."
+idris DepPairs.idr --check
+
+echo "DataStore..."
+idris DataStore.idr --check
+
+echo "Adder..."
+idris Adder.idr --check
 
 ./clean.sh
 
