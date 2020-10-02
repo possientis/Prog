4669146a147fa8b55a5016dff5ff789a26ed4e60
Paul Ossientis <paul_ossientis@riseup.net>
2020-10-02 18:13:58 +0100

    coq: set theory

--- coq/Logic/Lang1/Context.v
+++ coq/Logic/Lang1/Context.v
@@ -11,24 +11,50 @@ Require Import Logic.Set.Elem.
 
 Require Import Logic.Lang1.Syntax.
 
-Definition Context : Type := list (nat * set).
-
-Inductive Find : Context -> nat -> set -> Prop :=
-| FindZ : forall (G:Context) (n:nat) (x:set), Find (cons (n,x) G) n x
-| FindS : forall (G:Context) (n m:nat) (x y:set),
-    n <> m     -> 
-    Find G n x -> 
-    Find (cons (m,y) G) n x
+Inductive Context : Type :=
+| O        : Context
+| CtxSnoc  : Context -> (nat * set) -> Context
 .
+
+Notation "G ';' x" := (CtxSnoc G x)
+    (at level 50, left associativity) : Context_scope.
+
+Open Scope Context_scope.
+
+Inductive Find : Context -> (nat * set) -> Prop :=
+| FindZ : forall (G:Context) (n:nat) (x:set), Find (G ; (n,x)) (n,x)
+| FindS : forall (G:Context) (n m:nat) (x y:set),
+    n <> m       -> 
+    Find G (n,x) -> 
+    Find (G ; (m,y)) (n,x)
+.
+
+Notation "G ':>' x" := (Find G x)
+    (at level 60, no associativity) : Context_scope.
+
+Open Scope Context_scope.
+
+(* Just restating constructor FindZ with custom notations.                      *)
+Lemma findZ : forall (G:Context) (n:nat) (x:set), G ; (n,x) :> (n,x). 
+Proof.
+    intros G n x. constructor.
+Qed.
+
+(* Just restating constructor FindS with custom notations.                      *)
+Lemma findS : forall (G:Context) (n m:nat) (x y:set),
+    n <> m -> G :> (n,x) -> G ; (m,y) :> (n,x).
+Proof.
+    intros G n m x y H1 H2. constructor; assumption.
+Qed.
 
 Inductive Eval : Context -> Formula -> Prop -> Prop :=
 | EvalBot  : forall (G:Context), Eval G Bot False
 | EvalElem : forall (G:Context) (n m:nat) (x y:set),
-    Find G n x -> Find G m y -> Eval G (Elem n m) (x :: y)
+    G :> (n,x) -> G :> (m,y) -> Eval G (Elem n m) (x :: y)
 | EvalImp  : forall (G:Context) (p1 p2:Formula) (P1 P2:Prop), 
     Eval G p1 P1 -> Eval G p2 P2 -> Eval G (Imp p1 p2) (P1 -> P2)
 |EvalAll   : forall (G:Context) (n:nat) (p1:Formula) (P1:set -> Prop),
-    (forall (x:set), Eval (cons (n,x) G) p1 (P1 x)) ->
+    (forall (x:set), Eval (G ; (n,x)) p1 (P1 x)) ->
     Eval G (All n p1) (forall (x:set), P1 x)
 .
 
