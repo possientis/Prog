9fdb7441e4876554647506ea5e1572a9a4db4f0b
Paul Ossientis <paul_ossientis@riseup.net>
2020-10-22 18:29:05 +0100

    coq: cttwc

--- coq/Logic/Nat/Witness.v
+++ coq/Logic/Nat/Witness.v
@@ -1,3 +1,5 @@
+Require Import Le.
+
 Require Import Logic.Class.Ord.
 
 Require Import Logic.Nat.Leq.
@@ -53,10 +55,10 @@ Proof.
 Defined.
 
 (* Transform a predicate into another predicate which is satisfied when the     *)
-(* argument predicate is satisfied and it is the smallest value doing so.       *)
+(* argument predicate is satisfied and it is the smallest value doing so, i.e.  *)
+(* 'small p n' <-> 'n is the smallest value satisfying the predicate p'.        *)
 Definition small (p:nat -> Prop) (n:nat) : Prop :=
     p n /\ forall (m:nat), p m -> n <= m.
-
 
 (* Expresses the idea that a predicate represents a finite subset of N.         *)
 Definition Finite (p:nat -> Prop) : Prop :=
@@ -65,8 +67,12 @@ Definition Finite (p:nat -> Prop) : Prop :=
 Definition truncate (p:nat -> Prop) (n:nat) : nat -> Prop :=
     fun (m:nat) => p m /\ m <= n.
 
-(*
-Lemma technical : forall (p:nat -> Prop) (n m:nat), p n ->
+Lemma truncateFinite : forall (p:nat -> Prop) (n:nat), Finite (truncate p n). 
+Proof.
+    intros p n. unfold Finite, truncate. exists n. intros m [H1 H2]. assumption.
+Qed.
+
+Lemma truncateSmall : forall (p:nat -> Prop) (n m:nat), p n ->
     small p m <-> small (truncate p n) m.
 Proof.
     intros p n m H1. unfold small, truncate. split.
@@ -74,7 +80,21 @@ Proof.
         + split; try assumption. apply H3. assumption.
         + intros k [H4 H5]. apply H3. assumption.
     - intros [[H2 H3] H4]. split; try assumption. intros k H5.
-      apply leqTrans.
+      destruct (leqTotal k n) as [H6|H6].
+        + apply H4. split; assumption.
+        + apply le_trans with n; assumption.
+Qed.
+
+(*
+Lemma finiteNonEmptyHasSmallest : forall (p:nat -> Prop), Finite p -> 
+    (exists (k:nat), p k) -> (exists (k:nat), small p k).
+Proof.
+    unfold Finite. intros p [n H1]. revert n H1. 
+    induction n as [|n IH]; intros H1 [m H2]; unfold small.
+    - assert (m = 0) as H3.
+        { apply le_0, H1. assumption. }
+      subst. exists 0. split; try assumption. intros m H3. apply le_0_n.
+    -
 Show.
 *)
 
