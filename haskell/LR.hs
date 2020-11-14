a2eb86c0999b3db47e25022c1d54c645dbf68506
Paul Ossientis <paul_ossientis@riseup.net>
2020-11-14 18:01:38 +0000

    haskell: LR

--- haskell/LR.hs
+++ haskell/LR.hs
@@ -26,4 +26,75 @@ step a lrs
 equal :: Double -> Double -> Bool
 equal x y  = abs (x - y) < tolerance
 
+data Matrix = Matrix
+    { aMat :: Integer
+    , bMat :: Integer
+    , cMat :: Integer
+    , dMat :: Integer
+    }
 
+instance Show Matrix where
+    show m = "[(" ++ show (aMat m) ++ "," ++ show (bMat m) ++ "),(" 
+           ++ show (cMat m) ++ "," ++ show (dMat m) ++ ")]"
+prod :: Matrix -> Matrix -> Matrix
+prod m1 m2 = Matrix
+    { aMat = a1*a2 + b1*c2
+    , bMat = a1*b2 + b1*d2
+    , cMat = c1*a2 + d1*c2
+    , dMat = c1*b2 + d1*d2
+    }
+    where
+        a1 = aMat m1
+        b1 = bMat m1
+        c1 = cMat m1
+        d1 = dMat m1
+        a2 = aMat m2
+        b2 = bMat m2
+        c2 = cMat m2
+        d2 = dMat m2
+
+matrixI :: Matrix
+matrixI  = Matrix 1 0 0 1
+
+matrixL :: Matrix
+matrixL  = Matrix 1 0 1 1
+
+matrixR :: Matrix
+matrixR  = Matrix 1 1 0 1
+
+pow :: Integer -> Matrix -> Matrix
+pow 0 m = matrixI
+pow n m = prod m (pow (n - 1) m)
+
+data Rat = Rat
+    { numerator   :: Integer
+    , denominator :: Integer
+    } deriving (Eq)
+
+instance Show Rat where
+    show r = show (numerator r) ++ "/" ++ show (denominator r)
+
+toRat :: Matrix -> Rat
+toRat m = Rat (a + b) (c + d)
+    where
+        a = aMat m
+        b = bMat m
+        c = cMat m
+        d = dMat m
+
+toMatrix :: LR -> Matrix
+toMatrix L = matrixL
+toMatrix R = matrixR
+
+scan :: [Matrix] -> [Matrix]
+scan []     = []
+scan (m:ms) = m : [ prod m x | x <- scan ms ]
+
+approximate :: Integer -> Double -> [Rat]
+approximate n r = map toRat $ scan $ map toMatrix $ decision n r
+
+toRatio :: Rat -> Double
+toRatio (Rat n m) = fromIntegral n / fromIntegral m
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f xs = [f x | x <- xs] 
