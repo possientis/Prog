1c94eb639728f25da4fcad4ebe537749800e06d2
Paul Ossientis <paul_ossientis@riseup.net>
2023-05-27 13:11:21 +0100

    guile setup

--- scheme/guile/setup.sh
+++ scheme/guile/setup.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-root="${HOME}/Prog/scheme"
+DIR="${HOME}/Prog/scheme"
 
 cd /usr/share/guile/2.2/
 
 sudo mkdir -p foo
 cd foo
-sudo ln -nfs ${root}/guile/bar-module.scm bar-module.scm
+sudo ln -nfs ${DIR}/guile/bar-module.scm bar-module.scm
 cd ..
 
 sudo mkdir -p math
 cd math
-sudo ln -nfs ${root}/guile/bessel.scm bessel.scm
+sudo ln -nfs ${DIR}/guile/bessel.scm bessel.scm
 cd ..
 
 sudo mkdir -p bitcoin
 cd bitcoin
-sudo ln -nfs ${root}/bitcoin/secp256k1/secp256k1.scm secp256k1.scm
+sudo ln -nfs ${DIR}/bitcoin/secp256k1/secp256k1.scm secp256k1.scm
 
 cd /usr/lib/x86_64-linux-gnu/guile/2.2
 sudo mkdir -p extensions
 cd extensions
-sudo ln -nfs ${HOME}/Prog/scheme/guile/libguile-bessel.so libguile-bessel.so
+sudo ln -nfs ${DIR}/guile/libguile-bessel.so libguile-bessel.so
