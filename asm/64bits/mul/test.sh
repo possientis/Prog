b8edf4dd88c47907904fbd2be2fdea48c37b8987
Paul Ossientis <paul_ossientis@riseup.net>
2023-06-13 08:10:21 +0100

    asm test

--- asm/64bits/mul/test.sh
+++ asm/64bits/mul/test.sh
@@ -9,19 +9,19 @@ option=$(sh ../option.sh)
 
 echo "testing 8 bits unsigned multiplication..." 
 ../yasm.sh test_mul_8bits.asm 
-gcc test_mul_8bits.c test_mul_8bits.o
+gcc $option test_mul_8bits.c test_mul_8bits.o
 ./a.out; ./clean.sh
 echo
 
 echo "testing 16 bits unsigned multiplication..." 
 ../yasm.sh test_mul_16bits.asm 
-gcc test_mul_16bits.c test_mul_16bits.o
+gcc $option test_mul_16bits.c test_mul_16bits.o
 ./a.out; ./clean.sh
 echo
 
 echo "testing 32 bits unsigned multiplication..." 
 ../yasm.sh test_mul_32bits.asm 
-gcc test_mul_32bits.c test_mul_32bits.o
+gcc $option test_mul_32bits.c test_mul_32bits.o
 ./a.out; ./clean.sh
 echo
 
@@ -33,19 +33,19 @@ echo
 
 echo "testing 8 bits signed multiplication..." 
 ../yasm.sh test_imul_8bits.asm 
-gcc test_imul_8bits.c test_imul_8bits.o
+gcc $option test_imul_8bits.c test_imul_8bits.o
 ./a.out; ./clean.sh
 echo
 
 echo "testing 16 bits signed multiplication..." 
 ../yasm.sh test_imul_16bits.asm 
-gcc test_imul_16bits.c test_imul_16bits.o
+gcc $option test_imul_16bits.c test_imul_16bits.o
 ./a.out; ./clean.sh
 echo
 
 echo "testing 32 bits signed multiplication..." 
 ../yasm.sh test_imul_32bits.asm 
-gcc test_imul_32bits.c test_imul_32bits.o
+gcc $option test_imul_32bits.c test_imul_32bits.o
 ./a.out; ./clean.sh
 echo
 
