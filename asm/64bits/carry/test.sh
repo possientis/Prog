bb6e9bbf72902854bc870e6aae62d8016bd28471
Paul Ossientis <paul_ossientis@riseup.net>
2023-06-13 08:13:36 +0100

    asm test

--- asm/64bits/carry/test.sh
+++ asm/64bits/carry/test.sh
@@ -5,14 +5,16 @@ DIR=${HOME}/Prog/asm/64bits/carry
 cd ${DIR}
 echo
 
+option=$(sh ../option.sh)
+
 echo "testing carry for 8 bits add"
 ../yasm.sh add_carry_8bits.asm
-gcc test_add_carry_8bits.c add_carry_8bits.o
+gcc $option test_add_carry_8bits.c add_carry_8bits.o
 ./a.out; ./clean.sh 
 
 echo "testing carry for 8 bits sub"
 ../yasm.sh sub_carry_8bits.asm
-gcc test_sub_carry_8bits.c sub_carry_8bits.o
+gcc $option test_sub_carry_8bits.c sub_carry_8bits.o
 ./a.out; ./clean.sh 
 
 echo
