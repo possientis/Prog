6b07eb3731f6c7bbbe9e9bff933d551a0ce7c674
Paul Ossientis <paul_ossientis@riseup.net>
2023-08-11 08:36:23 +0100

    dot setup

---
+++ dot/setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+DIR=$(pwd)
+
+cd ${HOME}/Prog/dot
+sudo cp agda.vim /usr/share/vim/vim90/syntax
+sudo cp coq.vim /usr/share/vim/vim90/syntax
+sudo cp haskell.vim /usr/share/vim/vim90/syntax
+sudo cp idris.vim /usr/share/vim/vim90/syntax
+sudo cp lean.vim /usr/share/vim/vim90/syntax
+sudo cp scheme.vim /usr/share/vim/vim90/syntax
+sudo cp solarized.vim /usr/share/vim/vim90/colors
+
+cp .bash_aliases ${HOME}/
+cp .dircolors ${HOME}/
+cp .gdbinit ${HOME}/
+cp .guile ${HOME}/
+cp .vimrc ${HOME}/
+
+mkdir -p ${HOME}/.gradle
+cp gradle.properties ${HOME}/.gradle
+
+cd $DIR
