a56b0edbc4026eb5ae1fb8b5f3a60bfb503e70c3
Paul Ossientis <paul_ossientis@riseup.net>
2024-01-21 13:54:12 +0000

    haskell: histo

--- haskell/Recursive.hs
+++ haskell/Recursive.hs
@@ -10,11 +10,36 @@ module Recursive
 import Control.Monad (join)
 import Data.Tuple.Extra
 
+data Cofree f a = Cofree a (f (Cofree f a))
+
+data CofreeF f a r = CofreeF a (f r)
+
+instance (Functor f) => Functor (CofreeF f a) where
+  fmap f (CofreeF a fr) = CofreeF a (fmap f fr)
+
+instance (Foldable f) => Foldable (CofreeF f a) where
+  foldMap f (CofreeF _ fr) = foldMap f fr
+
+instance (Traversable f) => Traversable (CofreeF f a) where
+  traverse f (CofreeF a fr) = CofreeF a <$> traverse f fr
+
+instance (Functor f) => Corecursive (Cofree f a) (CofreeF f a) where
+  embed (CofreeF a fr) = Cofree a fr
+
+unfold :: (Functor f) => (b -> (a, f b)) -> b -> Cofree f a
+unfold = ana . (.) (uncurry CofreeF) 
+
 class (Functor b) => Recursive t b | t -> b where
   project :: t -> b t
 
 class (Functor b) => Corecursive t b | t -> b where
   embed :: b t -> t
+
+hylo :: (Functor f) => (f b -> b) -> (a -> f a) -> a -> b
+hylo alg coalg = alg . fmap (hylo alg coalg) . coalg
+
+hyloM :: (Monad m, Traversable f) => (f b -> m b) -> (a -> m (f a)) -> a -> m b
+hyloM algM coalgM = join . fmap (join.fmap algM.traverse (hyloM algM coalgM)) . coalgM
 
 cata :: (Recursive t b) => (b a -> a) -> t -> a 
 cata alg =  alg . fmap (cata alg) . project 
@@ -40,10 +65,5 @@ paraM algM t = join . fmap algM . fmap (fmap (t,)) . traverse (paraM algM) . pro
 apoM :: (Monad m, Traversable b, Corecursive t b) => (a -> m (b (Either t a))) -> a -> m t
 apoM coalgM = fmap embed . join . fmap (traverse (either return (apoM coalgM))) . coalgM
 
-hylo :: (Functor f) => (f b -> b) -> (a -> f a) -> a -> b
-hylo alg coalg = alg . fmap (hylo alg coalg) . coalg
-
-hyloM :: (Monad m, Traversable f) => (f b -> m b) -> (a -> m (f a)) -> a -> m b
-hyloM algM coalgM = join . fmap (join.fmap algM.traverse (hyloM algM coalgM)) . coalgM
-
-
+histo :: (Recursive t b) => (b (Cofree b a) -> a) -> t -> a
+histo alg = alg . fmap (unfold (histo alg &&& project)) . project
