cd0f2ca30990b8bba18a15a813ff78fb18e2aafc
Paul Ossientis <paul_ossientis@riseup.net>
2024-04-13 14:50:59 +0100

    haskell: Strong

---
+++ haskell/Optics2/Strong.hs
@@ -0,0 +1,28 @@
+module Strong
+  ( Strong
+  , first
+  , second
+  ) where
+
+import Data.Tuple (swap)
+import Data.Tuple.Extra ((***))
+import Profunctor
+
+
+-- Def: a Profunctor p is said to be Strong, if and only, there exists a map 
+-- 'first` which extends every transformation of type a ~> b to a transformation 
+-- of type (a,c) ~> (b,c)
+
+class (Profunctor p) => Strong p where
+  first :: p a b -> p (a,c) (b,c)
+
+-- Remark: If p is a strong Profunctor, then any transformation of type a ~> b can
+-- also be extended to an transformation of type (c,a) ~> (c,b). This motivates
+-- the following definition:
+
+second :: (Strong p) => p a b -> p (c,a) (c,b)
+second t = dimap swap swap (first t) 
+
+-- Lemma: the Profunctor (->) is Strong.
+instance Strong (->) where
+  first f = f *** id 
