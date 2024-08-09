cd157fdaf4d565cfd09b3e86a4f9ed1be07c5e9d
Paul Ossientis <paul_ossientis@riseup.net>
2024-08-09 21:41:16 +0100

    coq: Logic: Fol2: remove_stillV, remove_stillP

--- coq/Logic/Fol2/Remove.v
+++ coq/Logic/Fol2/Remove.v
@@ -2,6 +2,8 @@ Require Import List.
 
 Require Import Logic.Class.Eq.
 
+Require Import Logic.List.In.
+
 Require Import Logic.Fol.Free.
 Require Import Logic.Fol.Syntax.
 
@@ -23,6 +25,62 @@ Fixpoint removeFromScope (v:Type) (e:Eq v) (x:v) (G:Ctx v) : Ctx v :=
  end.
 
 Arguments removeFromScope {v} {e}.
+
+Lemma remove_stillV : forall (v:Type) (e:Eq v) (G:Ctx v) (x y:v),
+  x <> y -> Var y :: G -> Var y :: removeFromScope x G.
+Proof.
+  intros v e G x y. induction G as [|ent G' IH].
+  - intros _ HIn. inversion HIn.
+  - destruct ent as [z|p]; intros HNeq HIn; simpl.
+    + destruct (eqDec x z) as [Hxz|Hxz].
+      * subst. apply IH.
+        { apply HNeq. }
+        { destruct HIn as [HIn|HIn].
+          - inversion HIn. contradiction.
+          - apply HIn. }
+      * destruct HIn as [HIn|HIn].
+        { inversion HIn. subst. left. reflexivity. }
+        { right. apply IH.
+          - apply HNeq.
+          - apply HIn. }
+    + destruct (in_dec eqDec x (Fr p)) as [HFree|HFree].
+      * apply IH. apply HNeq. destruct HIn as [HIn|HIn].
+        { inversion HIn. }
+        { apply HIn. }
+      * right. apply IH. apply HNeq. destruct HIn as [HIn|HIn].
+        { inversion HIn. }
+        { apply HIn. }
+Qed.
+
+Lemma remove_stillP : forall (v:Type) (e:Eq v) (G:Ctx v) (x:v) (p:P v),
+  ~x :: Fr p -> Prp p :: G -> Prp p :: removeFromScope x G.
+Proof.
+  intros v e G x p. induction G as [|ent G' IH].
+  - intros _ HIn. inversion HIn.
+  - destruct ent as [y|q]; intros HFree HIn; simpl.
+    + destruct (eqDec x y) as [Hxy|Hxy].
+      * apply IH.
+        { apply HFree. }
+        { destruct HIn as [HIn|HIn].
+          - inversion HIn.
+          - apply HIn. }
+      * right. apply IH.
+        { apply HFree. }
+        { destruct HIn as [HIn|HIn].
+          - inversion HIn.
+          - apply HIn. }
+    + destruct (in_dec eqDec x (Fr q)) as [HFree'|Hfree'].
+      * apply IH.
+        { apply HFree. }
+        { destruct HIn as [HIn|HIn].
+          - inversion HIn. subst. contradiction.
+          - apply HIn. }
+      * destruct HIn as [HIn|HIn].
+        { inversion HIn. subst. left. reflexivity. }
+        { right. apply IH.
+          - apply HFree.
+          - apply HIn. }
+Qed.
 
 (*
 Lemma removeStillValid : forall (v:Type) (e:Eq v) (G:Ctx v) (x:v),
