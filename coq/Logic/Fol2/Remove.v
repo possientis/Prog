f14506858b485c8063f10e431644570980838c45
Paul Ossientis <paul_ossientis@riseup.net>
2024-08-10 17:36:03 +0100

    coq: Logic: Fol2: remove_characV, remove_characP

--- coq/Logic/Fol2/Remove.v
+++ coq/Logic/Fol2/Remove.v
@@ -185,6 +185,117 @@ Proof.
         { right. apply IH, Hu. }
 Qed.
 
+Arguments removeFromScope_incl {v} {e}.
+
+Lemma remove_characV : forall (v:Type) (e:Eq v) (G:Ctx v) (x:v),
+  forall (y:v), Var y :: removeFromScope x G <-> Var y :: G /\ x <> y.
+Proof.
+  intros v e G x. induction G as [|ent G' IH]; simpl; intros y.
+  - split.
+    + intros H. contradiction.
+    + intros [H1 H2]. contradiction.
+  - split.
+    + destruct ent as [z|p].
+      * destruct (eqDec x z) as [HEq|HNeq].
+        { subst. intros HIn. split.
+          - right. apply (removeFromScope_incl G' z), HIn.
+          - apply IH, HIn. }
+        { intros [HIn|HIn].
+          - inversion HIn. subst. split.
+            + left. reflexivity.
+            + apply HNeq.
+          - split.
+            + right. apply IH, HIn.
+            + apply IH, HIn. }
+      * destruct (in_dec eqDec x (Fr p)) as [HFree|HFree].
+        { intros HIn. split.
+          - right. apply IH, HIn.
+          - apply IH, HIn. }
+        { intros [HIn|HIn].
+          - inversion HIn.
+          - split.
+            + right. apply IH, HIn.
+            + apply IH, HIn. }
+    + destruct ent as [z|p].
+      * destruct (eqDec x z) as [HEq|HNeq].
+        { subst. intros [[HEq|HIn] HNeq].
+          - apply IH. split.
+            + inversion HEq. subst. contradiction.
+            + apply HNeq.
+          - apply IH. split.
+            + apply HIn.
+            + apply HNeq. }
+        { intros [[HEq|HIn] HNeq'].
+          - inversion HEq. subst. left. reflexivity.
+          - right. apply IH. split.
+            + apply HIn.
+            + apply HNeq'. }
+      * destruct (in_dec eqDec x (Fr p)) as [HFree|HFree].
+        { intros [[HEq|HIn] HNeq].
+          - inversion HEq.
+          - apply IH. split.
+            + apply HIn.
+            + apply HNeq. }
+        { intros [[HEq|HIn] HNeq].
+          - inversion HEq.
+          - right. apply IH. split.
+            + apply HIn.
+            + apply HNeq. }
+Qed.
+
+Lemma remove_characP : forall (v:Type) (e:Eq v) (G:Ctx v) (x:v),
+  forall (p:P v), Prp p :: removeFromScope x G <-> Prp p :: G /\ ~ x :: Fr p.
+Proof.
+  intros v e G x. induction G as [|ent G' IH]; simpl; intros p.
+  - split.
+    + intros H. contradiction.
+    + intros [H1 H2]. contradiction.
+  - split.
+    + destruct ent as [y|q].
+      * destruct (eqDec x y) as [HEq|HNeq].
+        { subst. intros HIn. split.
+          - right. apply IH, HIn.
+          - apply IH, HIn. }
+        { intros [HIn|HIn].
+          - inversion HIn.
+          - split.
+            + right. apply IH, HIn.
+            + apply IH, HIn. }
+      * destruct (in_dec eqDec x (Fr q)) as [HFree|HFree].
+        { intros HIn. split.
+          - right. apply IH, HIn.
+          - apply IH, HIn. }
+        { intros [HIn|HIn].
+          - inversion HIn. subst. split.
+            + left. reflexivity.
+            + apply HFree.
+          - split.
+            + right. apply IH, HIn.
+            + apply IH, HIn. }
+    + destruct ent as [y|q].
+      * destruct (eqDec x y) as [HEq|HNeq].
+        { subst. intros [[HEq|HIn] HFree].
+          - inversion HEq.
+          - apply IH. split.
+            + apply HIn.
+            + apply HFree. }
+        { intros [[HEq|HIn] HFree].
+          - inversion HEq.
+          - right. apply IH. split.
+            + apply HIn.
+            + apply HFree. }
+      * destruct (in_dec eqDec x (Fr q)) as [HFree|HFree].
+        { intros [[HEq|HIn] HFree'].
+          - inversion HEq. subst. contradiction.
+          - apply IH. split.
+            + apply HIn.
+            + apply HFree'. }
+        { intros [[HEq|HIn] HFree'].
+          - inversion HEq. subst. left. reflexivity.
+          - right. apply IH. split.
+            + apply HIn.
+            + apply HFree'. }
+Qed.
 (*
 Lemma removeStillValid : forall (v:Type) (e:Eq v) (G:Ctx v) (x:v),
   CtxVal G -> CtxVal (removeFromScope x G).
