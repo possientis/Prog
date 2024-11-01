351ebad7dbe5ea15857424024c50b22ad558e762
Paul Ossientis <paul_ossientis@riseup.net>
2024-11-01 21:44:37 +0000

    coq: Logic: ZF: Tuple

--- coq/Logic/ZF/Tuple.v
+++ coq/Logic/ZF/Tuple.v
@@ -11,20 +11,111 @@ Definition tuple3 (a1 a2 a3:U) : U := :{a1,a2}: :\/: :{a3}:.
 Notation ":{ a , b , c }:" := (tuple3 a b c)
   (at level 1, no associativity) : ZF_Tuple_scope.
 
+Definition tuple4 (a1 a2 a3 a4:U) : U := :{a1,a2,a3}: :\/: :{a4}:.
+
+Notation ":{ a , b , c , d }:" := (tuple4 a b c d)
+  (at level 1, no associativity) : ZF_Tuple_scope.
+
 Proposition Tuple3Charac : forall (a1 a2 a3:U),
   forall x, x :< :{a1,a2,a3}: <-> x = a1 \/ x = a2 \/ x = a3.
 Proof.
   intros a1 a2 a3 x. unfold tuple3. split.
   - intros H1. apply UnionABCharac in H1. destruct H1 as [H1|H1].
-    + apply PairCharac in H1. destruct H1 as [H1|H1].
-      * left. apply H1.
-      * right. left. apply H1.
-    + apply SingleCharac in H1. right. right. apply H1.
-  - intros [H1|H1]; apply UnionABCharac.
-    + left. apply PairCharac. left. apply H1.
-    + destruct H1 as [H1|H1].
-      * left. apply PairCharac. right. apply H1.
-      * right. apply SingleCharac, H1.
+    + apply PairCharac in H1. destruct H1 as [H1|H1]; auto.
+    + apply SingleCharac in H1. auto.
+  - intros [H1|[H1|H1]]; apply UnionABCharac.
+    + left. apply PairEqualIn1, H1.
+    + left. apply PairEqualIn2, H1.
+    + right. apply SingleCharac, H1.
 Qed.
 
+Proposition Tuple3EqualIn1 : forall (a1 a2 a3:U),
+  forall x, x = a1 -> x :< :{a1,a2,a3}:.
+Proof.
+  intros a1 a2 a3 x Hx. apply Tuple3Charac. left. apply Hx.
+Qed.
 
+Proposition Tuple3EqualIn2 : forall (a1 a2 a3:U),
+  forall x, x = a2 -> x :< :{a1,a2,a3}:.
+Proof.
+  intros a1 a2 a3 x Hx. apply Tuple3Charac. right. left. apply Hx.
+Qed.
+
+Proposition Tuple3EqualIn3 : forall (a1 a2 a3:U),
+  forall x, x = a3 -> x :< :{a1,a2,a3}:.
+Proof.
+  intros a1 a2 a3 x Hx. apply Tuple3Charac. right. right. apply Hx.
+Qed.
+
+Proposition Tuple3In1 : forall (a1 a2 a3:U), a1 :< :{a1,a2,a3}:.
+Proof.
+  intros a1 a2 a3. apply Tuple3EqualIn1. reflexivity.
+Qed.
+
+Proposition Tuple3In2 : forall (a1 a2 a3:U), a2 :< :{a1,a2,a3}:.
+Proof.
+  intros a1 a2 a3. apply Tuple3EqualIn2. reflexivity.
+Qed.
+
+Proposition Tuple3In3 : forall (a1 a2 a3:U), a3 :< :{a1,a2,a3}:.
+Proof.
+  intros a1 a2 a3. apply Tuple3EqualIn3. reflexivity.
+Qed.
+
+Proposition Tuple4Charac : forall (a1 a2 a3 a4:U),
+  forall x, x :< :{a1,a2,a3,a4}: <-> x = a1 \/ x = a2 \/ x = a3 \/ x = a4.
+Proof.
+  intros a1 a2 a3 a4 x. unfold tuple4. split.
+  - intros H1. apply UnionABCharac in H1. destruct H1 as [H1|H1].
+    + apply Tuple3Charac in H1. destruct H1 as [H1|[H1|H1]]; auto.
+    + apply SingleCharac in H1. auto.
+  - intros [H1|[H1|[H1|H1]]]; apply UnionABCharac.
+    + left. apply Tuple3EqualIn1, H1.
+    + left. apply Tuple3EqualIn2, H1.
+    + left. apply Tuple3EqualIn3, H1.
+    + right. apply SingleCharac, H1.
+Qed.
+
+Proposition Tuple4EqualIn1 : forall (a1 a2 a3 a4:U),
+  forall x, x = a1 -> x :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4 x Hx. apply Tuple4Charac. left. apply Hx.
+Qed.
+
+Proposition Tuple4EqualIn2 : forall (a1 a2 a3 a4:U),
+  forall x, x = a2 -> x :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4 x Hx. apply Tuple4Charac. right. left. apply Hx.
+Qed.
+
+Proposition Tuple4EqualIn3 : forall (a1 a2 a3 a4:U),
+  forall x, x = a3 -> x :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4 x Hx. apply Tuple4Charac. right. right. left. apply Hx.
+Qed.
+
+Proposition Tuple4EqualIn4 : forall (a1 a2 a3 a4:U),
+  forall x, x = a4 -> x :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4 x Hx. apply Tuple4Charac. right. right. right. apply Hx.
+Qed.
+
+Proposition Tuple4In1 : forall (a1 a2 a3 a4:U), a1 :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4. apply Tuple4EqualIn1. reflexivity.
+Qed.
+
+Proposition Tuple4In2 : forall (a1 a2 a3 a4:U), a2 :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4. apply Tuple4EqualIn2. reflexivity.
+Qed.
+
+Proposition Tuple4In3 : forall (a1 a2 a3 a4:U), a3 :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4. apply Tuple4EqualIn3. reflexivity.
+Qed.
+
+Proposition Tuple4In4 : forall (a1 a2 a3 a4:U), a4 :< :{a1,a2,a3,a4}:.
+Proof.
+  intros a1 a2 a3 a4. apply Tuple4EqualIn4. reflexivity.
+Qed.
