318293328f807f7db50fa2ec7d396995608502a1
Paul Ossientis <paul_ossientis@riseup.net>
2024-11-02 16:56:21 +0000

    coq: Logic: ZF: some properties of inclusion

--- coq/Logic/ZF/Include.v
+++ coq/Logic/ZF/Include.v
@@ -3,6 +3,8 @@ Open    Scope ZF_Include_scope.
 
 Require Import Logic.ZF.Core.
 Require Import Logic.ZF.Extensionality.
+Require Import Logic.ZF.Intersect.
+Require Import Logic.ZF.Union.
 
 (* Inclusion predicate between two sets.                                        *)
 Definition Incl (a b:U) : Prop := forall x, x :< a -> x :< b.
@@ -27,4 +29,64 @@ Proof.
   - intros [H1 H2]. apply Extensionality. intros x. split.
     + apply H1.
     + apply H2.
+Qed.
+
+(* The inclusion relation is reflexive.                                         *)
+Proposition InclRefl : forall (a:U), a :<=: a.
+Proof.
+  intros a x. auto.
+Qed.
+
+(* The inclusion relation is anti-symmetric.                                    *)
+Proposition InclAnti : forall (a b:U),
+  a :<=: b -> b :<=: a -> a = b.
+Proof.
+  intros a b H1 H2. apply DoubleInclusion. split; assumption.
+Qed.
+
+(* The inclusion relation is transitive.                                        *)
+Proposition InclTran : forall (a b c:U),
+  a :<=: b -> b :<=: c -> a :<=: c.
+Proof.
+  intros a b c H1 H2 x H3. apply H2, H1, H3.
+Qed.
+
+(* The inclusion relation is left compatible with intersection.                 *)
+Proposition InclCompatIntersectL : forall (a b c:U),
+  a :<=: b -> c:/\:a :<=: c:/\:b.
+Proof.
+  intros a b c H1 x H2. apply IntersectCharac in H2. destruct H2 as [H2 H3].
+  apply IntersectCharac. split.
+  - apply H2.
+  - apply H1, H3.
+Qed.
+
+(* The inclusion relation is right compatible with intersection.                 *)
+Proposition InclCompatIntersectR : forall (a b c:U),
+  a :<=: b -> a:/\:c :<=: b:/\:c.
+Proof.
+  intros a b c H1 x H2. apply IntersectCharac in H2. destruct H2 as [H2 H3].
+  apply IntersectCharac. split.
+  - apply H1, H2.
+  - apply H3.
+Qed.
+
+(* The inclusion relation is left compatible with union.                        *)
+Proposition InclCompatUnionL : forall (a b c:U),
+  a :<=: b -> c:\/:a :<=: c:\/:b.
+Proof.
+  intros a b c H1 x H2. apply UnionCharac in H2. destruct H2 as [H2|H2];
+  apply UnionCharac.
+  - left. apply H2.
+  - right. apply H1, H2.
+Qed.
+
+(* The inclusion relation is right compatible with union.                       *)
+Proposition InclCompatUnionR : forall (a b c:U),
+  a :<=: b -> a:\/:c :<=: b:\/:c.
+Proof.
+  intros a b c H1 x H2. apply UnionCharac in H2. destruct H2 as [H2|H2];
+  apply UnionCharac.
+  - left. apply H1, H2.
+  - right. apply H2.
 Qed.
