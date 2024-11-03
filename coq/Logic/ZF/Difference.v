9e9d11f5ebcc1d937831d72869061d3285f044c6
Paul Ossientis <paul_ossientis@riseup.net>
2024-11-03 14:59:24 +0000

    coq: Logic: ZF: Difference

---
+++ coq/Logic/ZF/Difference.v
@@ -0,0 +1,17 @@
+Declare Scope ZF_Difference_scope.
+Open    Scope ZF_Difference_scope.
+
+Require Import Logic.ZF.Core.
+Require Import Logic.ZF.Comprehension.
+
+(* The set a - b is made of those elements of a which do not belong to b.       *)
+Definition difference (a b:U) : U := :{a | fun x => ~ x :< b }:.
+
+Notation "a :\: b" := (difference a b)
+  (at level 3, no associativity) : ZF_Difference_scope.
+
+Proposition DiffCharac : forall (a b:U),
+  forall x, x :< a:\:b <-> x :< a /\ ~ x :< b.
+Proof.
+  intros a b x. unfold difference. apply CompCharac.
+Qed.
