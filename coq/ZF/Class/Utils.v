3c453ef61b822859dc48fc1e0debd11848a78cbc
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-18 14:42:05 +0000

    coq: ZF: refactor V

--- coq/ZF/Class/Utils.v
+++ coq/ZF/Class/Utils.v
@@ -1,23 +1,12 @@
 Require Import ZF.Class.
 Require Import ZF.Class.Bounded.
-Require Import ZF.Class.Include.
 Require Import ZF.Class.Intersect.
-Require Import ZF.Class.Product.
 Require Import ZF.Class.Relation.
 Require Import ZF.Class.Small.
 Require Import ZF.Class.Union.
-Require Import ZF.Class.V.
 Require Import ZF.Core.And.
-Require Import ZF.Core.Leq.
-Require Import ZF.Core.Lt.
 Require Import ZF.Core.Or.
-Require Import ZF.Core.Product.
-Require Import ZF.Core.Zero.
-Require Import ZF.Set.
-Require Import ZF.Set.Empty.
-Require Import ZF.Set.OrdPair.
 Require Import ZF.Set.Relation.
-Require Import ZF.Set.Utils.
 
 Definition CRelation := ZF.Class.Relation.Relation.
 Definition SRelation := ZF.Set.Relation.Relation.
@@ -35,26 +24,6 @@ Proposition SmallIntersectSmall2 : forall (P Q:Class),
 Proof.
   intros P Q [a Ha]. apply BoundedClassIsSmall. exists a.
   intros x [_ H1]. apply Ha, H1.
-Qed.
-
-(* The product of two classes is a subclass of V^2.                             *)
-Proposition ProdInclV2 : forall (P Q:Class),
-  P :x: Q :<=: V :x: V.
-Proof.
-  intros P Q x H1. destruct H1 as [y [z [H1 [H2 H3]]]].
-  exists y. exists z. split.
-  - apply H1.
-  - split; apply I.
-Qed.
-
-(* V^2 is a strict subclass of V.                                               *)
-Proposition V2InclStrictV : V :x: V :<: V.
-Proof.
-  apply InclStrictExists. split.
-  - intros x H1. apply I.
-  - exists :0:. split.
-    + apply I.
-    + intros [x [y [H1 _]]]. apply (EmptyNotOrdPair x y), H1.
 Qed.
 
 (* The union of two relation class is a relation class.                         *)
--- coq/ZF/Class/V.v
+++ coq/ZF/Class/V.v
@@ -1,4 +1,33 @@
 Require Import ZF.Class.
+Require Import ZF.Class.Include.
+Require Import ZF.Class.Product.
+Require Import ZF.Core.Leq.
+Require Import ZF.Core.Lt.
+Require Import ZF.Core.Product.
+Require Import ZF.Core.Zero.
+Require Import ZF.Set.Utils.
 
 (* The class satisfied by all sets.                                             *)
 Definition V : Class := fun _ => True.
+
+(* The product of two classes is a subclass of V^2.                             *)
+Proposition ProdInclV2 : forall (P Q:Class),
+  P :x: Q :<=: V :x: V.
+Proof.
+  intros P Q x H1. destruct H1 as [y [z [H1 [H2 H3]]]].
+  exists y. exists z. split.
+  - apply H1.
+  - split; apply I.
+Qed.
+
+(* V^2 is a strict subclass of V.                                               *)
+Proposition V2InclStrictV : V :x: V :<: V.
+Proof.
+  apply InclStrictExists. split.
+  - intros x H1. apply I.
+  - exists :0:. split.
+    + apply I.
+    + intros [x [y [H1 _]]]. apply (EmptyNotOrdPair x y), H1.
+Qed.
+
+
