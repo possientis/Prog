1a47ed65e5ac7619a93ceb58989b30cc02c56261
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-21 06:39:19 +0000

    coq: ZF: Class.Bijection.ComposeIsBijection

--- coq/ZF/Class/Bijection.v
+++ coq/ZF/Class/Bijection.v
@@ -1,8 +1,10 @@
 Require Import ZF.Class.
+Require Import ZF.Class.Compose.
 Require Import ZF.Class.Converse.
 Require Import ZF.Class.Function.
 Require Import ZF.Class.OneToOne.
 Require Import ZF.Class.Relation.
+Require Import ZF.Core.Dot.
 
 (* A class is a bijection if it is a relation and it is one-to-one.             *)
 Definition Bijection (P:Class) : Prop := Relation P /\ OneToOne P.
@@ -21,4 +23,10 @@ Proof.
   - apply H3.
 Qed.
 
-
+Proposition ConposeIsBijection : forall (P Q:Class),
+  OneToOne P -> OneToOne Q -> Bijection (P :.: Q).
+Proof.
+  intros P Q Hp Hq. split.
+  - apply ComposeIsRelation.
+  - apply ComposeIsOneToOne; assumption.
+Qed.
