a4683d9e4bf223f089d3ce1224bc02e8d60cf670
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-21 15:02:13 +0000

    coq: ZF: Set.Replace

--- coq/ZF/Set/Replace.v
+++ coq/ZF/Set/Replace.v
@@ -9,17 +9,6 @@ Require Import ZF.Core.Image.
 Require Import ZF.Set.
 Require Import ZF.Set.FromClass.
 
-(* It is useful to define the class underlying the replacement axiom.           *)
-Definition ReplacePred (F:Binary) (P:Class) : Class := fun y =>
-  exists x, P x /\ F x y.
-
-(* This class is exactly F[P], the direct image of P by F.                      *)
-Proposition ReplacePredImage : forall (F:Binary) (P:Class),
-    ReplacePred F P :~: F:[P]:.
-Proof.
-  intros F P y. split; auto.
-Qed.
-
 (* If F is functional and P is small, then F:[P]: is small                      *)
 Proposition ReplaceSmall : forall (F:Binary) (P:Class),
   Functional F -> Small P -> Small F:[P]:.
@@ -34,9 +23,30 @@ Proof.
   (* We assume that P is small. *)
   intros H2. assert (Small P) as A. { apply H2. } clear A.
 
+  (* In particular P is equivalent to some set a. *)
+  assert (exists a, toClass a :~: P) as H3. { apply (proj1 (SmallIsSet _)), H2. }
+
+  (* So let a be a set equivalent to the class P. *)
+  destruct H3 as [a H3].
+
   (* We need to show that the direct image of P by F is small. *)
   assert (Small F:[P]:) as A. 2: apply A.
-Admitted.
+
+  (* The property of being small being compatible with equivalences... *)
+  apply SmallEquivCompat with F:[toClass a]:.
+
+  (* We first need to show the equivalence between F[a] and F[P]. *)
+  - assert (F:[toClass a]: :~: F:[P]:) as A. 2: apply A.
+
+  (* Which follows from the equivalence between a and P. *)
+  apply ImageEquivCompatR, H3.
+
+  (* We next need to show that F[a] is small. *)
+  - assert (Small F:[toClass a]:) as A. 2: apply A.
+
+  (* Which follows from the replacement axiom and the fact F is functional. *)
+    apply Replacement, H1.
+Qed.
 
 (* The set defined by the class F[P] when F is functional and P is small.       *)
 Definition replaceSet (F:Binary) (P:Class) (p:Functional F) (q:Small P) : U
