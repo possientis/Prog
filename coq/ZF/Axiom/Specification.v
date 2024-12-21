afe767d3aced75dbd9c5abd2ee43675d3ede0196
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-21 09:31:29 +0000

    coq: ZF: refactor Axiom.Specification.

--- coq/ZF/Axiom/Specification.v
+++ coq/ZF/Axiom/Specification.v
@@ -20,46 +20,51 @@ Proof.
   (* Consider the binary class F x y := P x /\ x = y. *)
   remember (fun x y => P x /\ x = y) as F eqn:H1.
 
-  (* Consider the class associated with the set a. *)
-  remember (toClass a) as Q eqn:Ea.
+  (* Consider the class A associated with the set a. *)
+  remember (toClass a) as A eqn:Ea.
 
-  (* Note that the class associated with the set a is small. *)
-  assert (Small Q) as S. { rewrite Ea. apply SetIsSmall. }
+  (* Note that the class A is small. *)
+  assert (Small A) as Ha. { rewrite Ea. apply SetIsSmall. }
 
-  (* We claim that this binary class is functional. *)
-  assert (Functional F) as H2.
+  (* We claim the binary class F is functional. *)
+  assert (Functional F) as Hf.
     { intros x y z H3 H4. rewrite H1 in H3. rewrite H1 in H4.
       destruct H3 as [_ H3]. destruct H4 as [_ H4]. subst.
       reflexivity.
     }
 
-  (* We claim that the class F[a] (direct image of the set a by F) is the same  *)
-  (* as the class fun x => x :< a /\ P x.                                       *)
-  assert (forall x, F:[toClass a]: x <-> x :< a /\ P x) as H3.
+  (* We claim the class F[A] is the same as fun x => x :< a /\ P x. *)
+  assert (forall x, F:[A]: x <-> x :< a /\ P x) as H3.
     { rewrite H1. unfold image. intros x. split; intros H4.
       - destruct H4 as [y [H5 [H6 H7]]]. subst. auto.
       - exists x. split.
-        + destruct H4 as [H4 _]. apply H4.
+        + rewrite Ea. destruct H4 as [H4 _]. apply H4.
         + split. 2: reflexivity. destruct H4 as [_ H4]. apply H4.
     }
 
   (* We need to show the existence of b such that x :< b <-> x :< a /\ P x.*)
-  assert (exists b, forall x, x :< b <-> x :< a /\ P x) as A. 2: apply A.
+  assert (exists b, forall x, x :< b <-> x :< a /\ P x) as A'. 2: apply A'.
 
-  (* Let us take b to be the direct image of a by the functional relation F     *)
-  (* which is a small class and can be viewed as a set.                         *)
-  exists (replaceSet F Q H2 S).
+  (* Let us take b = { F x | A x }. Such a set exists since F is functional and *)
+  (* the class A is small.                                                      *)
+  remember (replaceSet F A Hf Ha) as b eqn:Eb.
+
+  (* We claim that b has the desired property. *)
+  exists b.
 
   (* We need to show x :< b <-> x :< a /\ P x *)
-  assert (forall x, x :< (replaceSet F Q H2 S) <-> x :< a /\ P x) as A. 2: apply A.
+  assert (forall x, x :< b <-> x :< a /\ P x) as A'. 2: apply A'.
 
   intros x. split.
 
   (* Proof of -> *)
-  - assert (x :< (replaceSet F Q H2 S) -> x :< a /\ P x) as A. 2: apply A.
-    intros H4. apply ReplaceCharac in H4. rewrite Ea in H4. apply H3, H4.
+  - assert (x :< b -> x :< a /\ P x) as A'. 2: apply A'.
+    intros H4. rewrite Eb in H4.
+    apply ReplaceCharac in H4. apply H3, H4.
 
   (* Proof of <- *)
-  - assert (x :< a /\ P x -> x :< (replaceSet F Q H2 S)) as A. 2: apply A.
-    intros H4. apply ReplaceCharac. rewrite Ea. apply H3, H4.
+  - assert (x :< a /\ P x -> x :< b) as A'. 2: apply A'.
+    intros H4. rewrite Eb.
+    apply ReplaceCharac. apply H3, H4.
 Qed.
+
