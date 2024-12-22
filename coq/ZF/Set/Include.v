736d6d4cf6a0a05668abb672fe82f5bf38fb4733
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-22 17:38:05 +0000

    coq: ZF: refactor Set.Union.

--- coq/ZF/Set/Include.v
+++ coq/ZF/Set/Include.v
@@ -2,11 +2,7 @@ Require Import ZF.Axiom.Classic.
 Require Import ZF.Axiom.Extensionality.
 Require Import ZF.Core.Leq.
 Require Import ZF.Core.Lt.
-Require Import ZF.Core.Or.
-Require Import ZF.Core.Zero.
 Require Import ZF.Set.
-Require Import ZF.Set.Empty.
-Require Import ZF.Set.Union.
 
 (* Inclusion predicate between two sets.                                        *)
 Definition Incl (a b:U) : Prop := forall x, x :< a -> x :< b.
@@ -51,48 +47,6 @@ Proposition InclTran : forall (a b c:U),
   a :<=: b -> b :<=: c -> a :<=: c.
 Proof.
   intros a b c H1 H2 x H3. apply H2, H1, H3.
-Qed.
-
-(* The inclusion relation is left compatible with union.                        *)
-Proposition InclCompatUnionL : forall (a b c:U),
-  a :<=: b -> c:\/:a :<=: c:\/:b.
-Proof.
-  intros a b c H1 x H2. apply UnionCharac in H2. destruct H2 as [H2|H2];
-  apply UnionCharac.
-  - left. apply H2.
-  - right. apply H1, H2.
-Qed.
-
-(* The inclusion relation is right compatible with union.                       *)
-Proposition InclCompatUnionR : forall (a b c:U),
-  a :<=: b -> a:\/:c :<=: b:\/:c.
-Proof.
-  intros a b c H1 x H2. apply UnionCharac in H2. destruct H2 as [H2|H2];
-  apply UnionCharac.
-  - left. apply H1, H2.
-  - right. apply H2.
-Qed.
-
-Proposition InclEqualUnion : forall (a b:U),
-  a :<=: b <-> b = a :\/: b.
-Proof.
-  intros a b. split; intros H1.
-  - apply Extensionality. intros x. split; intros H2.
-    + apply UnionCharac. right. apply H2.
-    + apply UnionCharac in H2. destruct H2 as [H2|H2].
-      * apply H1, H2.
-      * apply H2.
-  - intros x H2. rewrite H1. apply UnionCharac. left. apply H2.
-Qed.
-
-Proposition InclUnion1 : forall (a b:U), a :<=: a:\/:b.
-Proof.
-  intros a b x H1. apply UnionCharac. left. apply H1.
-Qed.
-
-Proposition InclUnion2 : forall (a b:U), b :<=: a:\/:b.
-Proof.
-  intros a b x H1. apply UnionCharac. right. apply H1.
 Qed.
 
 Proposition InclStrictExists : forall (a b:U),
--- coq/ZF/Set/Union.v
+++ coq/ZF/Set/Union.v
@@ -4,9 +4,11 @@ Open    Scope ZF_Set_Union_scope.
 Require Import ZF.Axiom.Extensionality.
 Require Import ZF.Axiom.Union.
 Require Import ZF.Class.Small.
+Require Import ZF.Core.Leq.
 Require Import ZF.Core.Or.
 Require Import ZF.Set.
 Require Import ZF.Set.FromClass.
+Require Import ZF.Set.Include.
 Require Import ZF.Set.Pair.
 
 (* It is useful to define the predicate underlying the union axiom.             *)
@@ -79,4 +81,46 @@ Proof.
   - apply UnionCharac in H1. destruct H1 as [H1|H1].
     + left. apply UnionCharac. right. apply H1.
     + right. apply H1.
+Qed.
+
+(* The inclusion relation is left compatible with union.                        *)
+Proposition InclCompatUnionL : forall (a b c:U),
+  a :<=: b -> c:\/:a :<=: c:\/:b.
+Proof.
+  intros a b c H1 x H2. apply UnionCharac in H2. destruct H2 as [H2|H2];
+  apply UnionCharac.
+  - left. apply H2.
+  - right. apply H1, H2.
+Qed.
+
+(* The inclusion relation is right compatible with union.                       *)
+Proposition InclCompatUnionR : forall (a b c:U),
+  a :<=: b -> a:\/:c :<=: b:\/:c.
+Proof.
+  intros a b c H1 x H2. apply UnionCharac in H2. destruct H2 as [H2|H2];
+  apply UnionCharac.
+  - left. apply H1, H2.
+  - right. apply H2.
+Qed.
+
+Proposition InclEqualUnion : forall (a b:U),
+  a :<=: b <-> b = a :\/: b.
+Proof.
+  intros a b. split; intros H1.
+  - apply Extensionality. intros x. split; intros H2.
+    + apply UnionCharac. right. apply H2.
+    + apply UnionCharac in H2. destruct H2 as [H2|H2].
+      * apply H1, H2.
+      * apply H2.
+  - intros x H2. rewrite H1. apply UnionCharac. left. apply H2.
+Qed.
+
+Proposition InclUnion1 : forall (a b:U), a :<=: a:\/:b.
+Proof.
+  intros a b x H1. apply UnionCharac. left. apply H1.
+Qed.
+
+Proposition InclUnion2 : forall (a b:U), b :<=: a:\/:b.
+Proof.
+  intros a b x H1. apply UnionCharac. right. apply H1.
 Qed.
