804b1bdf24b06c34a8a80561b877db1983ac33dc
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-23 15:18:14 +0000

    coq: ZF: refactor Set.Relation

--- coq/ZF/Set/Relation.v
+++ coq/ZF/Set/Relation.v
@@ -1,6 +1,14 @@
+Require Import ZF.Class.
+Require Import ZF.Class.Relation.
 Require Import ZF.Set.
 Require Import ZF.Set.OrdPair.
 
 (* A relation is a set of ordered pairs.                                        *)
-Definition Relation (a:U) : Prop :=
-  forall x, x :< a -> exists y z, x = :(y,z):.
+Definition Relation (a:U) : Prop
+  := Class.Relation.Relation (toClass a).
+
+Proposition RelationCharac : forall (a:U),
+  Relation a <-> forall x, x :< a -> exists y z, x = :(y,z):.
+Proof.
+  intro a. unfold Relation, Class.Relation.Relation. split; auto.
+Qed.
