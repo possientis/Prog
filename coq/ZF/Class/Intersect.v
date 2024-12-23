51d42fa720b39c0a29e16f5e8be294c0812599c7
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-23 18:36:47 +0000

    coq: ZF: remove Class.Utils

--- coq/ZF/Class/Intersect.v
+++ coq/ZF/Class/Intersect.v
@@ -1,4 +1,6 @@
 Require Import ZF.Class.
+Require Import ZF.Class.Bounded.
+Require Import ZF.Class.Small.
 Require Import ZF.Core.And.
 
 (* The intersection of two classes P and Q.                                     *)
@@ -6,3 +8,18 @@ Definition intersect (P Q:Class) : Class := fun x => P x /\ Q x.
 
 (* Notation "P :/\: Q" := (intersect P Q)                                       *)
 Global Instance ClassAnd : And Class := { and := intersect }.
+
+Proposition SmallIntersectSmallL : forall (P Q:Class),
+  Small P -> Small (P:/\:Q).
+Proof.
+  intros P Q [a Ha].
+  apply BoundedClassIsSmall. exists a.
+  intros x [H1 _]. apply Ha, H1.
+Qed.
+
+Proposition SmallIntersectSmallR : forall (P Q:Class),
+  Small Q -> Small (P:/\:Q).
+Proof.
+  intros P Q [a Ha]. apply BoundedClassIsSmall. exists a.
+  intros x [_ H1]. apply Ha, H1.
+Qed.
--- coq/ZF/Set/Relation.v
+++ coq/ZF/Set/Relation.v
@@ -1,5 +1,6 @@
 Require Import ZF.Class.
 Require Import ZF.Class.Relation.
+Require Import ZF.Class.Union.
 Require Import ZF.Set.
 Require Import ZF.Set.OrdPair.
 
@@ -11,4 +12,12 @@ Proposition RelationCharac : forall (a:U),
   Relation a <-> forall x, x :< a -> exists y z, x = :(y,z):.
 Proof.
   intro a. unfold Relation, Class.Relation.Relation. split; auto.
+Qed.
+
+(* The union of a class of relations is a relation class.                       *)
+Proposition UnionClassOfRelsIsRel : forall (P:Class),
+  (forall x, P x -> Relation x) -> Class.Relation.Relation :U(P).
+Proof.
+  intros P H1 x H2. unfold unionClass in H2. destruct H2 as [y [H2 H3]].
+  apply H1 in H3. apply H3, H2.
 Qed.
