d7e2a70dc891b0f255be3bbb5eb9532e5337bad9
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-24 10:27:23 +0000

    coq: ZF: refactor Axiom.Specification

--- coq/ZF/Axiom/Specification.v
+++ coq/ZF/Axiom/Specification.v
@@ -1,71 +1,76 @@
-Require Import ZF.Axiom.Replacement.
-Require Import ZF.Binary.
 Require Import ZF.Binary.Functional.
 Require Import ZF.Binary.Image.
 Require Import ZF.Class.
 Require Import ZF.Class.Small.
+Require Import ZF.Core.Equiv.
 Require Import ZF.Core.Image.
 Require Import ZF.Set.
 Require Import ZF.Set.FromClass.
-Require Import ZF.Set.Replace.
 
-(* Given a set theoretic predicate P and a set a, there exists a set b whose    *)
-(* elements are the elements of the set a which satisfy P.                      *)
 (* This is a theorem and not an axiom as it can be derived from replacement.    *)
-Theorem Specification : forall (P:U -> Prop),
+(* Given a class P and a set a, there exists a set b whose elements are the     *)
+(* elements of the set a which are in P.                                        *)
+Theorem Specification : forall (P:Class),
   forall a, exists b, forall x, x :< b <-> x :< a /\ P x.
 Proof.
-  (* Let P be a predicate and a be a set. *)
+  (* Let P be a class and a be a set. *)
   intros P a.
 
+  (* We need to show the existence of a set b with the desired property. *)
+  assert (exists b, forall x, x :< b <-> x :< a /\ P x) as A. 2: apply A.
+
+  (* Consider the class Q ={ x :< a | P x }. *)
+  remember (fun x => x :< a /\ P x) as Q eqn:Eq.
+
+  (* Let us assume that Q is small. *)
+  assert (Small Q) as Hq. 2: {
+
+  (* Then Q is actually a set b. *)
+  remember (fromClass Q Hq) as b eqn:Eb.
+
+  (* And we claim this set has the required property. *)
+  exists b.
+
+  (* Which is true *)
+  intros x. rewrite Eb. split; intros H1.
+  - apply FromClassCharac in H1. rewrite Eq in H1. assumption.
+  - apply FromClassCharac. rewrite Eq. assumption.
+  }
+
+  (* So it remains to show that Q is small. *)
+  assert (Small Q) as A. 2: apply A.
+
   (* Consider the binary class F x y := P x /\ x = y. *)
-  remember (fun x y => P x /\ x = y) as F eqn:H1.
+  remember (fun x y => P x /\ x = y) as F eqn:Ef.
 
-  (* Consider the class A associated with the set a. *)
-  remember (toClass a) as A eqn:Ea.
+  (* We claim that the direct image F[a] is Q *)
+  assert (F:[toClass a]: :~: Q) as H2. {
+    intros y. split; intros H3.
+    - apply (proj1 (ImageCharac _ _ _)) in H3. destruct H3 as [x [H3 H4]].
+      rewrite Ef in H4. destruct H4 as [H4 H5]. subst. split; assumption.
+    - rewrite Eq in H3. destruct H3 as [H3 H4]. apply ImageCharac.
+      exists y. rewrite Ef. split.
+      + assumption.
+      + split. { assumption. } { reflexivity. }
+  }
 
-  (* Note that the class A is small. *)
-  assert (Small A) as Ha. { rewrite Ea. apply SetIsSmall. }
+  (* Using this equivalence... *)
+  apply SmallEquivCompat with F:[toClass a]:. 1: apply H2.
 
-  (* We claim the binary class F is functional. *)
+  (* It is sufficient to show that F[a] is small *)
+  assert (Small F:[toClass a]:) as A. 2: apply A.
+
+  (* Which we know is true since a set is small ... *)
+  apply ImageSmall. 2: apply SetIsSmall.
+
+  (* Provided we show that F is functional *)
+  assert (Functional F) as A. 2: apply A.
+
   assert (Functional F) as Hf.
-    { intros x y z H3 H4. rewrite H1 in H3. rewrite H1 in H4.
+    { intros x y z H3 H4. rewrite Ef in H3. rewrite Ef in H4.
       destruct H3 as [_ H3]. destruct H4 as [_ H4]. subst.
       reflexivity.
     }
 
-  (* We claim the class F[A] is the same as fun x => x :< a /\ P x. *)
-  assert (forall x, F:[A]: x <-> x :< a /\ P x) as H3.
-    { rewrite H1. unfold image. intros x. split; intros H4.
-      - destruct H4 as [y [H5 [H6 H7]]]. subst. auto.
-      - exists x. split.
-        + rewrite Ea. destruct H4 as [H4 _]. apply H4.
-        + split. 2: reflexivity. destruct H4 as [_ H4]. apply H4.
-    }
-
-  (* We need to show the existence of b such that x :< b <-> x :< a /\ P x.*)
-  assert (exists b, forall x, x :< b <-> x :< a /\ P x) as A'. 2: apply A'.
-
-  (* Let us take b = { F x | A x }. Such a set exists since F is functional and *)
-  (* the class A is small.                                                      *)
-  remember (replaceSet F A Hf Ha) as b eqn:Eb.
-
-  (* We claim that b has the desired property. *)
-  exists b.
-
-  (* We need to show x :< b <-> x :< a /\ P x *)
-  assert (forall x, x :< b <-> x :< a /\ P x) as A'. 2: apply A'.
-
-  intros x. split.
-
-  (* Proof of -> *)
-  - assert (x :< b -> x :< a /\ P x) as A'. 2: apply A'.
-    intros H4. rewrite Eb in H4.
-    apply ReplaceCharac in H4. apply H3, H4.
-
-  (* Proof of <- *)
-  - assert (x :< a /\ P x -> x :< b) as A'. 2: apply A'.
-    intros H4. rewrite Eb.
-    apply ReplaceCharac. apply H3, H4.
+  apply Hf.
 Qed.
-
--- coq/ZF/Class/Intersect.v
+++ coq/ZF/Class/Intersect.v
@@ -2,12 +2,19 @@ Require Import ZF.Class.
 Require Import ZF.Class.Bounded.
 Require Import ZF.Class.Small.
 Require Import ZF.Core.And.
+Require Import ZF.Set.
 
 (* The intersection of two classes P and Q.                                     *)
 Definition intersect (P Q:Class) : Class := fun x => P x /\ Q x.
 
 (* Notation "P :/\: Q" := (intersect P Q)                                       *)
 Global Instance ClassAnd : And Class := { and := intersect }.
+
+Proposition IntersectCharac : forall (P Q:Class) (x:U),
+  (P:/\:Q) x <-> P x /\ Q x.
+Proof.
+  intros P Q x. split; intros H1; apply H1.
+Qed.
 
 Proposition SmallIntersectSmallL : forall (P Q:Class),
   Small P -> Small (P:/\:Q).
@@ -23,3 +30,4 @@ Proof.
   intros P Q [a Ha]. apply BoundedClassIsSmall. exists a.
   intros x [_ H1]. apply Ha, H1.
 Qed.
+
