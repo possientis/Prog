e4f8fde9a2fe9bf5e86c34c1a60428d980f65c06
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-28 12:43:57 +0000

    coq: ZF: Refactor :0:

---
+++ coq/ZF/Class/Empty.v
@@ -0,0 +1,47 @@
+Require Import ZF.Axiom.NonEmptyUniverse.
+Require Import ZF.Class.
+Require Import ZF.Class.Small.
+Require Import ZF.Core.Zero.
+Require Import ZF.Set.
+Require Import ZF.Set.Specify.
+
+(* The class which is satisfied by no set.                                      *)
+Definition empty : Class := fun _ => False.
+
+(* Notation ":0:" := empty                                                      *)
+Global Instance ClassZero : Zero Class := { zero := empty }.
+
+Proposition EmptyCharac : forall (x:U), :0: x <-> False.
+Proof.
+  intros x. unfold zero, ClassZero, empty. split; auto.
+Qed.
+
+(* The empty class is small.                                                    *)
+Proposition EmptyIsSmall : Small :0:.
+Proof.
+  (* We know there is at least one set in the universe *)
+  remember NonEmptyUniverse as H1 eqn:A. clear A.
+
+  (* Let a be such a set *)
+  destruct H1 as [a _].
+
+  (* We need to show the existence of a set b with no elements *)
+  assert (exists b, forall x, x :< b <-> False) as A. 2: apply A.
+
+  (* Consider the set b = { x | x :< a /\ ~ x :< a } *)
+  remember :{a | fun x => ~ x :< a }: as b eqn:H2.
+
+  (* We claim this set b has the required property *)
+  exists b.
+
+  (* We need to show that x :< b <-> False for all x *)
+  assert (forall x, x :< b <-> False) as A. 2: apply A.
+
+  (* So let x be an arbitrary set *)
+  intros x.
+
+  split.
+  - intros H3. rewrite H2 in H3. apply SpecCharac in H3.
+    destruct H3 as [H3 H4]. contradiction.
+  - intros H3. contradiction.
+Qed.
--- coq/ZF/Set/Empty.v
+++ coq/ZF/Set/Empty.v
@@ -1,6 +1,6 @@
 Require Import ZF.Axiom.Classic.
 Require Import ZF.Axiom.Extensionality.
-Require Import ZF.Axiom.NonEmptyUniverse.
+Require Import ZF.Class.Empty.
 Require Import ZF.Class.Small.
 Require Import ZF.Core.Leq.
 Require Import ZF.Core.Zero.
@@ -10,55 +10,17 @@ Require Import ZF.Set.Incl.
 Require Import ZF.Set.OrdPair.
 Require Import ZF.Set.Pair.
 Require Import ZF.Set.Singleton.
-Require Import ZF.Set.Specify.
-
-(* The class which is satisfied by no set.                                      *)
-Definition Empty : U -> Prop := fun _ => False.
-
-(* The empty class is small.                                                    *)
-Proposition EmptySmall : Small Empty.
-Proof.
-  unfold Small, Empty.
-
-  (* We know there is at least one set in the universe *)
-  remember NonEmptyUniverse as H1 eqn:A. clear A.
-
-  (* Let a be such a set *)
-  destruct H1 as [a _].
-
-  (* We need to show the existence of a set b with no elements *)
-  assert (exists b, forall x, x :< b <-> False) as A. 2: apply A.
-
-  (* Consider the set b = { x | x :< a /\ ~ x :< a } *)
-  remember :{a | fun x => ~ x :< a }: as b eqn:H2.
-
-  (* We claim this set b has the required property *)
-  exists b.
-
-  (* We need to show that x :< b <-> False for all x *)
-  assert (forall x, x :< b <-> False) as A. 2: apply A.
-
-  (* So let x be an arbitrary set *)
-  intros x. split.
-
-  (* Proof of -> *)
-  - intros H3. rewrite H2 in H3. apply SpecCharac in H3.
-    destruct H3 as [H3 H4]. contradiction.
-
-  (* Proof of <- *)
-  - intros H3. contradiction.
-Qed.
 
 (* We consider the set defined by the small class Empty                         *)
-Definition emptySet : U := fromClass Empty EmptySmall.
+Definition empty : U := fromClass :0: EmptyIsSmall.
 
 (* Notation ":0:" := emptySet                                                   *)
-Global Instance SetZero : Zero U := { zero := emptySet }.
+Global Instance SetZero : Zero U := { zero := empty }.
 
 (* Characterisation of the elements of the empty set.                           *)
 Proposition EmptyCharac : forall x, x :< :0: <-> False.
 Proof.
-  unfold emptySet. apply FromClassCharac.
+  apply FromClassCharac.
 Qed.
 
 (* The empty set has no element.                                                *)
