92d89bc2e110a2c3e4877afc5677d021163a193e
Paul Ossientis <paul_ossientis@riseup.net>
2024-12-28 14:02:13 +0000

    coq: ZF: refactor Set.Power

---
+++ coq/ZF/Class/Power.v
@@ -0,0 +1,14 @@
+Require Import ZF.Axiom.Power.
+Require Import ZF.Class.
+Require Import ZF.Class.Small.
+Require Import ZF.Core.Leq.
+Require Import ZF.Set.
+Require Import ZF.Set.Incl.
+
+Definition power (a:U) : Class := fun x => x :<=: a.
+
+(* The power class of the set a is small.                                       *)
+Proposition PowerIsSmall : forall (a:U), Small (power a).
+Proof.
+  apply Power.
+Qed.
--- coq/ZF/Set/Power.v
+++ coq/ZF/Set/Power.v
@@ -1,33 +1,21 @@
 Declare Scope ZF_Set_Power_scope.
 Open    Scope ZF_Set_Power_scope.
 
-Require Import ZF.Axiom.Power.
-Require Import ZF.Class.Small.
+Require Import ZF.Class.Power.
 Require Import ZF.Core.Leq.
 Require Import ZF.Set.
 Require Import ZF.Set.FromClass.
 Require Import ZF.Set.Incl.
 
-(* It is useful to define the predicate underlying the power axiom.             *)
-Definition PowerPred (a:U) : U -> Prop := fun x => x :<=: a.
+Definition power (a:U) : U := fromClass (Class.Power.power a)
+  (PowerIsSmall a).
 
-(* The power predicate of the set a is small.                                   *)
-Proposition PowerSmall : forall (a:U),
-  Small (PowerPred a).
-Proof.
-  apply Power.
-Qed.
-
-(* We consider the set defined by the power predicate of the set a.             *)
-Definition powerSet (a:U) : U
-  := fromClass (PowerPred a) (PowerSmall a).
-
-Notation ":P( a )" := (powerSet a)
+Notation ":P( a )" := (power a)
   (at level 0, no associativity) : ZF_Set_Power_scope.
 
 (* Characterisation of the elements of the power set of a.                      *)
 Proposition PowerCharac : forall (a:U),
   forall x, x :< :P(a) <-> x :<=: a.
 Proof.
-  unfold powerSet. intros a. apply FromClassCharac.
+  intros a. apply FromClassCharac.
 Qed.
