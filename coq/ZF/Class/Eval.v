7ff6b088d4d9b7fc21ec9508b04017acd3ada510
Paul Ossientis <paul_ossientis@riseup.net>
2025-01-03 16:14:45 +0000

    coq: ZF: refactor Class.Eval

--- coq/ZF/Class/Eval.v
+++ coq/ZF/Class/Eval.v
@@ -3,6 +3,7 @@ Require Import ZF.Class.
 Require Import ZF.Class.Domain.
 Require Import ZF.Class.Empty.
 Require Import ZF.Class.Functional.
+Require Import ZF.Class.FunctionalAt.
 Require Import ZF.Class.Small.
 Require Import ZF.Core.Equiv.
 Require Import ZF.Core.Zero.
@@ -10,15 +11,14 @@ Require Import ZF.Set.
 Require Import ZF.Set.OrdPair.
 
 (* Predicate expressing the fact that class F has value y when evaluated at a.  *)
-Definition IsValueAt (F:Class) (a:U) : Class := fun y =>
-  F :(a,y): /\ (forall y1 y2, F :(a,y1): -> F :(a,y2): -> y1 = y2).
+Definition IsValueAt (F:Class) (a y:U) : Prop := F :(a,y): /\ FunctionalAt F a.
 
 Proposition IsValueAtWhenFunctional : forall (F:Class) (a y:U),
   Functional F -> IsValueAt F a y <-> F :(a,y):.
 Proof.
   intros F a y H1. split; intros H2.
   - destruct H2 as [H2 _]. assumption.
-  - split. 1: { assumption. } apply H1.
+  - split . 1: { assumption. } apply FunctionalIsFunctionalAt. assumption.
 Qed.
 
 (* Predicate expressing the fact that class F has a value at a.                 *)
@@ -38,56 +38,65 @@ Qed.
 Definition eval (F:Class) (a:U) : Class := fun x =>
   exists y, x :< y /\ IsValueAt F a y.
 
-(* If P has a value at a and y corresponds to a, then eval P a = y.             *)
-Proposition WhenHasValue : forall (F:Class) (a y:U),
-  HasValueAt F a -> F :(a,y): -> eval F a :~: toClass y.
+(* If F has a value at a, then y corresponds to a iff eval F a = y.             *)
+Proposition WhenHasValueAt : forall (F:Class) (a y:U),
+  HasValueAt F a -> F :(a,y): <-> eval F a :~: toClass y.
 Proof.
-  intros F a y H1 H2 x. split; intros H3.
-  - unfold eval in H3. destruct H3 as [y' [H3 H4]]. unfold IsValueAt in H4.
-    destruct H4 as [H4 H5].
-    assert (y = y') as H6. { apply H5; assumption. } subst. assumption.
-  - unfold HasValueAt in H1. destruct H1 as [y' [H1 H4]].
-    unfold eval. exists y. split. 1: assumption. unfold IsValueAt.
-    split; assumption.
+  intros F a y H1. split; intros H2.
+  - intros x. split; intros H3.
+    + unfold eval in H3. destruct H3 as [y' [H3 H4]]. unfold IsValueAt in H4.
+      destruct H4 as [H4 H5].
+      assert (y = y') as H6. { apply H5; assumption. } subst. assumption.
+    + unfold HasValueAt in H1. destruct H1 as [y' [H1 H4]].
+      unfold eval. exists y. split. 1: assumption. unfold IsValueAt.
+      split; assumption.
+  - destruct H1 as [y' [H1 H3]]. assert (y = y') as H4. 2: { subst. assumption. }
+    apply SameClassEqual. apply ClassEquivTran with (eval F a).
+    + apply ClassEquivSym. assumption.
+    + clear H2 y. intros x. split; intros H4.
+      * unfold eval in H4. destruct H4 as [y [H4 H5]]. unfold IsValueAt in H5.
+        destruct H5 as [H5 H6]. assert (y' = y) as H7. { apply H6; assumption. }
+        subst. assumption.
+      * exists y'. split. 1: assumption. unfold IsValueAt. split; assumption.
 Qed.
 
-(* If P has no value at a then eval P a is the empty class.                     *)
-Proposition WhenNotHasValue : forall (P:Class) (a:U),
-  ~ HasValueAt P a -> eval P a :~: :0:.
+(* If F has no value at a then eval F a is the empty class.                     *)
+Proposition WhenNotHasValueAt : forall (F:Class) (a:U),
+  ~ HasValueAt F a -> eval F a :~: :0:.
 Proof.
-  intros P a H1 x. split; intros H2.
+  intros F a H1 x. split; intros H2.
   - apply H1. unfold eval in H2. destruct H2 as [y [H2 H3]].
     destruct H3 as [H3 H4]. exists y. split; assumption.
   - apply EmptyCharac in H2. contradiction.
 Qed.
 
-Proposition EvalIsSmall : forall (P:Class) (a:U), Small (eval P a).
+Proposition EvalIsSmall : forall (F:Class) (a:U), Small (eval F a).
 Proof.
-  (* Let P be an arbitrary class. *)
-  intros P.
+  (* Let F be an arbitrary class. *)
+  intros F.
 
   (* Let a be an arbitrary set. *)
   intros a.
 
-  (* Either P has a value at a or it has not. *)
-  assert (HasValueAt P a \/ ~ HasValueAt P a) as H1. { apply LawExcludedMiddle. }
+  (* Either F has a value at a or it has not. *)
+  assert (HasValueAt F a \/ ~ HasValueAt F a) as H1. { apply LawExcludedMiddle. }
 
   (* We consider these two cases separately. *)
   destruct H1 as [H1|H1].
 
-  (* So we assume that P as a value at a. *)
-  - assert (HasValueAt P a) as A. { apply H1. } clear A.
+  (* So we assume that F as a value at a. *)
+  - assert (HasValueAt F a) as A. { apply H1. } clear A.
 
-  (* Let y be a value of P at a. *)
+  (* Let y be a value of F at a. *)
     remember H1 as H2 eqn:E. clear E. destruct H2 as [y H2].
-    assert (IsValueAt P a y) as A. { apply H2. } clear A.
+    assert (IsValueAt F a y) as A. { apply H2. } clear A.
 
-  (* In particular we have P (a,y) *)
+  (* In particular we have F (a,y) *)
     destruct H2 as [H2 _].
-    assert (P :(a,y):) as A. { apply H2. } clear A.
+    assert (F :(a,y):) as A. { apply H2. } clear A.
 
-  (* And it follows that eval P a = y. *)
-    assert (eval P a :~: toClass y) as H3. { apply WhenHasValue; assumption. }
+  (* And it follows that eval F a = y. *)
+    assert (eval F a :~: toClass y) as H3. { apply WhenHasValueAt; assumption. }
 
   (* Using this equivalence ... *)
     apply SmallEquivCompat with (toClass y). 1: { apply ClassEquivSym, H3. }
@@ -98,11 +107,11 @@ Proof.
   (* Which is clear. *)
     apply SetIsSmall.
 
-  (* We now assume that P as no value at a. *)
-  - assert (~ HasValueAt P a) as A. { apply H1. } clear A.
+  (* We now assume that F as no value at a. *)
+  - assert (~ HasValueAt F a) as A. { apply H1. } clear A.
 
-  (* Then eval P a is the empty class. *)
-    assert (eval P a :~: :0:) as H2. { apply WhenNotHasValue, H1. }
+  (* Then eval F a is the empty class. *)
+    assert (eval F a :~: :0:) as H2. { apply WhenNotHasValueAt, H1. }
 
   (* Using this equivalence ... *)
     apply SmallEquivCompat with :0:. 1: { apply ClassEquivSym, H2. }
