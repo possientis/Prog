c6cb50a148de7be5211c3e18e7c24d058ad295f5
Paul Ossientis <paul_ossientis@riseup.net>
2025-01-26 18:32:17 +0000

    coq: ZF: Set.InvImageEvalCharac

--- coq/ZF/Set/Eval.v
+++ coq/ZF/Set/Eval.v
@@ -3,6 +3,7 @@ Open    Scope ZF_Set_Eval_scope.
 
 Require Import ZF.Class.
 Require Import ZF.Class.Compose.
+Require Import ZF.Class.Converse.
 Require Import ZF.Class.Domain.
 Require Import ZF.Class.Eval.
 Require Import ZF.Class.Function.
@@ -14,6 +15,7 @@ Require Import ZF.Class.Rel.
 Require Import ZF.Core.Dot.
 Require Import ZF.Core.Equiv.
 Require Import ZF.Core.Image.
+Require Import ZF.Core.Inverse.
 Require Import ZF.Core.Zero.
 Require Import ZF.Set.
 Require Import ZF.Set.Empty.
@@ -263,3 +265,21 @@ Proof.
     apply ImageCharac. exists x. split. 1: assumption.
     apply EvalWhenFunctional; assumption.
 Qed.
+
+
+(* Characterisation of the inverse image F^(-1)[A] in terms of evaluations of F.*)
+Proposition InvImageEvalCharac : forall (F B:Class), Functional F ->
+  forall x, F^:-1: :[B]: x <-> domain F x /\ B F!x.
+Proof.
+  intros F B H1 x. split; intros H2.
+  - apply (proj1 (ImageCharac _ _ _)) in H2. destruct H2 as [y [H2 H3]].
+    apply (proj1 (ConverseCharac2 _ _ _)) in H3.
+    assert (domain F x) as H4. { apply DomainCharac. exists y. assumption. }
+    split. 1: assumption.
+    assert (F!x = y) as H5. { apply EvalWhenFunctional; assumption. }
+    rewrite H5. assumption.
+  - destruct H2 as [H2 H3]. apply ImageCharac. exists (F!x). split. 1: assumption.
+    apply ConverseCharac2. apply EvalWhenFunctionalSatisfies; assumption.
+Qed.
+
+
