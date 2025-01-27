d2d6f3de8b7a0f49811a92dcdd2d4aabdedbcd04
Paul Ossientis <paul_ossientis@riseup.net>
2025-01-27 12:03:32 +0000

    coq: ZF: Class.Minimal

--- coq/ZF/Class/InitSegment.v
+++ coq/ZF/Class/InitSegment.v
@@ -1,9 +1,14 @@
 Require Import ZF.Class.
 Require Import ZF.Class.Converse.
+Require Import ZF.Class.Empty.
 Require Import ZF.Class.Image.
+Require Import ZF.Class.Inter.
 Require Import ZF.Class.InvImage.
+Require Import ZF.Core.And.
+Require Import ZF.Core.Equiv.
 Require Import ZF.Core.Image.
 Require Import ZF.Core.Inverse.
+Require Import ZF.Core.Zero.
 Require Import ZF.Set.
 Require Import ZF.Set.OrdPair.
 Require Import ZF.Set.Singleton.
@@ -18,4 +23,17 @@ Proof.
   - apply InvImageCharac in H1. destruct H1 as [y [H1 H2]].
     apply SingleCharac in H1. subst. assumption.
   - apply InvImageCharac. exists a. split. 2: assumption. apply SingleIn.
+Qed.
+
+Proposition InitSegmentEmptyInter : forall (R A:Class) (a:U),
+  A :/\: initSegment R a :~: :0: <-> forall x, A x -> ~ R :(x,a):.
+Proof.
+  intros R A a. split; intros H1.
+  - intros x H2 H3. apply EmptyCharac with x. apply H1, InterCharac.
+    split. 1: assumption. apply InitSegmentCharac. assumption.
+  - intros x. split; intros H2.
+    + apply EmptyCharac.
+      apply (proj1 (InterCharac _ _ _)) in H2. destruct H2 as [H2 H3].
+      apply InitSegmentCharac in H3. apply (H1 x); assumption.
+    + apply EmptyCharac in H2. contradiction.
 Qed.
---
+++ coq/ZF/Class/Minimal.v
@@ -0,0 +1,41 @@
+Require Import ZF.Class.
+Require Import ZF.Class.Empty.
+Require Import ZF.Class.InitSegment.
+Require Import ZF.Class.Inter.
+Require Import ZF.Core.And.
+Require Import ZF.Core.Equiv.
+Require Import ZF.Core.Zero.
+Require Import ZF.Set.
+Require Import ZF.Set.OrdPair.
+
+(* Predicate expressing the fact that a is an R-minimal element of A.           *)
+Definition Minimal (R A:Class) (a:U) : Prop
+  := A a /\ A :/\: initSegment R a :~: :0:.
+
+Proposition MinimalIn : forall (R A:Class) (a:U),
+  Minimal R A a -> A a.
+Proof.
+  intros R A a [H1 _]. assumption.
+Qed.
+
+Proposition MinimalEmptyInter : forall (R A:Class) (a:U),
+  Minimal R A a -> A :/\: initSegment R a :~: :0:.
+Proof.
+  intros R A a [_ H2]. assumption.
+Qed.
+
+Proposition MinimalHasNoPredecessor : forall (R A:Class) (a x:U),
+  Minimal R A a -> A x -> ~ R :(x,a):.
+Proof.
+  intros R A a x H1 H2. apply MinimalEmptyInter in H1.
+  remember (proj1 (InitSegmentEmptyInter R A a) H1) as H3 eqn:E. clear E H1.
+  apply H3. assumption.
+Qed.
+
+Proposition MinimalSuffice : forall (R A:Class) (a:U),
+  A a -> (forall x, A x -> ~ R :(x,a):) -> Minimal R A a.
+Proof.
+  intros R A a H1 H2. split. 1: assumption.
+  apply InitSegmentEmptyInter. assumption.
+Qed.
+
