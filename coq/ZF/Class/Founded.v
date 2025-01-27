40f145ca957c4883b4e9daee153c1bcdcae50364
Paul Ossientis <paul_ossientis@riseup.net>
2025-01-27 12:34:53 +0000

    coq: ZF: Class.Founded

---
+++ coq/ZF/Class/Founded.v
@@ -0,0 +1,13 @@
+Require Import ZF.Class.
+Require Import ZF.Class.Incl.
+Require Import ZF.Class.Minimal.
+Require Import ZF.Core.Leq.
+Require Import ZF.Core.Zero.
+Require Import ZF.Set.
+
+(* Predicate expressing the fact that R is a founded class on A.                *)
+(* R is founded on A iff every non-empty subset of A has an R-minimal element.  *)
+(* We wish this predicate to be expressed in the language of set theory, so     *)
+(* while Coq allows us to quantify over all subclasses of A, we do not do so.   *)
+Definition Founded (R A:Class) : Prop :=
+  forall a, toClass a :<=: A /\ ~(a = :0:) -> exists x, Minimal R (toClass a) x.
