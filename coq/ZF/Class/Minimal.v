0953c91c15713088e3e09c19d7f8d2f8a198a0e0
Paul Ossientis <paul_ossientis@riseup.net>
2025-02-02 15:03:00 +0000

    coq: ZF: refactor

--- coq/ZF/Class/Minimal.v
+++ coq/ZF/Class/Minimal.v
@@ -9,29 +9,10 @@ Require Import ZF.Set.OrdPair.
 Definition Minimal (R A:Class) (a:U) : Prop
   := A a /\ initSegment R A a :~: :0:.
 
-Proposition MinimalIn : forall (R A:Class) (a:U),
-  Minimal R A a -> A a.
-Proof.
-  intros R A a [H1 _]. assumption.
-Qed.
-
-Proposition MinimalEmptySegment : forall (R A:Class) (a:U),
-  Minimal R A a -> initSegment R A a :~: :0:.
-Proof.
-  intros R A a [_ H2]. assumption.
-Qed.
-
-Proposition MinimalHasNoPred : forall (R A:Class) (a x:U),
-  Minimal R A a -> A x -> ~ R :(x,a):.
-Proof.
-  intros R A a x H1 H2. apply MinimalEmptySegment in H1.
-  remember (proj1 (InitSegmentWhenEmpty R A a) H1) as H3 eqn:E. clear E H1.
-  apply H3. assumption.
-Qed.
-
 Proposition MinimalSuffice : forall (R A:Class) (a:U),
   A a -> (forall x, A x -> ~ R :(x,a):) -> Minimal R A a.
 Proof.
   intros R A a H1 H2. split. 1: assumption.
   apply InitSegmentWhenEmpty. assumption.
 Qed.
+
