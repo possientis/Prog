9e1d9bf661f15f40c5f601b9a0803683669d75c0
Paul Ossientis <paul_ossientis@riseup.net>
2025-04-22 08:18:22 +0300

    coq: ZF: refactor

--- coq/ZF/Set/Ordinal/Core.v
+++ coq/ZF/Set/Ordinal/Core.v
@@ -170,7 +170,7 @@ Proof.
 Qed.
 
 (* The union of a set of ordinals is an ordinal.                                *)
-Proposition UnionOfSetOfOrdinals : forall (a:U),
+Proposition UnionIsOrdinal : forall (a:U),
   toClass a :<=: On -> Ordinal :U(a).
 Proof.
   intros a H1. apply Class.Ordinal.EquivCompat with :U(toClass a).
@@ -179,31 +179,52 @@ Proof.
 Qed.
 
 (* The union of an ordinal is an ordinal.                                       *)
-Proposition UnionIsOrdinal : forall (a:U), Ordinal a ->
+Proposition UnionOfOrdinalIsOrdinal : forall (a:U), Ordinal a ->
   Ordinal :U(a).
 Proof.
-  intros a H1. apply UnionOfSetOfOrdinals.
+  intros a H1. apply UnionIsOrdinal.
   apply OrdinalIsStrictSubclass. assumption.
 Qed.
 
-(* The union of an ordinal is an upper-bound ot its elements.                   *)
-Proposition UnionIsUpperBound : forall (a b:U), Ordinal a -> 
-  b :< a -> b :<=: :U(a). 
+(* The union of a set of ordinals is an 'upper-bound' of the set.               *)
+Proposition UnionIsUpperBound : forall (a b:U),
+  toClass a :<=: On -> 
+  b :< a            -> 
+  b :<=: :U(a).
 Proof.
   intros a b H1 H2. apply Incl.EquivCompatR with :U(toClass a).
   - apply UnionOfToClass.
-  - apply Class.Ordinal.UnionIsUpperBound. 2: assumption.
-    apply OrdinalIsStrictSubclass. assumption.
+  - apply Class.Ordinal.UnionIsUpperBound; assumption.
 Qed.
 
-(* The union of an ordinal is the smallest upper-bound.                         *)
-Proposition UnionIsSmallestUpperBound : forall (a b:U), Ordinal a -> Ordinal b ->
-  (forall c, c :< a -> c :<=: b) -> :U(a) :<=: b.
+(* The union of an ordinal is an upper-bound ot its elements.                   *)
+Proposition UnionOfOrdinalIsUpperBound : forall (a b:U), Ordinal a -> 
+  b :< a -> b :<=: :U(a). 
+Proof.
+  intros a b H1 H2. apply UnionIsUpperBound. 2: assumption.
+  apply OrdinalIsStrictSubclass. assumption.
+Qed.
+
+Proposition UnionIsSmallestUpperBound : forall (a b:U),
+  toClass a :<=: On               ->
+  Ordinal b                       ->
+  (forall c, c :< a -> c :<=: b)  ->
+  :U(a) :<=: b.
 Proof.
   intros a b H1 H2 H3. apply Incl.EquivCompatL with :U(toClass a).
   - apply UnionOfToClass.
-  - apply Class.Ordinal.UnionIsSmallestUpperBound; try assumption.
-    apply OrdinalIsStrictSubclass. assumption. 
+  - apply Class.Ordinal.UnionIsSmallestUpperBound; assumption.
+Qed.
+
+(* The union of an ordinal is the smallest upper-bound.                         *)
+Proposition UnionOfOrdinalIsSmallestUpperBound : forall (a b:U), 
+  Ordinal a                      -> 
+  Ordinal b                      ->
+  (forall c, c :< a -> c :<=: b) -> 
+  :U(a) :<=: b.
+Proof.
+  intros a b H1 H2 H3. apply UnionIsSmallestUpperBound; try assumption.
+  apply OrdinalIsStrictSubclass. assumption. 
 Qed.
 
 Proposition UnionNotElemIsUnionEqual : forall (a:U), Ordinal a ->
@@ -211,12 +232,12 @@ Proposition UnionNotElemIsUnionEqual : forall (a:U), Ordinal a ->
 Proof.
   intros a H1. split; intros H2.
   - apply DoubleInclusion. split.
-    + apply UnionIsSmallestUpperBound; try assumption.
+    + apply UnionOfOrdinalIsSmallestUpperBound; try assumption.
       intros c H3. apply ElemIsIncl; try assumption.
       apply ElemIsOrdinal with a; assumption.
     + assert (:U(a) :< a \/ a :<=: :U(a)) as H3. {
-        apply ElemOrIncl. 2: assumption. apply UnionIsOrdinal. assumption. }
+        apply ElemOrIncl. 2: assumption. 
+        apply UnionOfOrdinalIsOrdinal. assumption. }
       destruct H3 as [H3|H3]. 1: contradiction. assumption.
   - intros H3. rewrite H2 in H3. apply NoElemLoop1 with a. assumption.
 Qed.
-
