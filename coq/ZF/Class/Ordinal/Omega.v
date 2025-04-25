0c991917a6eb80f0317aa101a43bbb2937e3380b
Paul Ossientis <paul_ossientis@riseup.net>
2025-04-25 08:11:28 +0300

    coq: ZF: Notation :N for omega

--- coq/ZF/Class/Ordinal/Omega.v
+++ coq/ZF/Class/Ordinal/Omega.v
@@ -1,9 +1,26 @@
 Require Import ZF.Class.Core.
+Require Import ZF.Class.Empty.
 Require Import ZF.Class.Incl.
 Require Import ZF.Class.Ordinal.Core.
 Require Import ZF.Class.Ordinal.NonLimit.
+Require Import ZF.Set.Core.
+Require Import ZF.Set.Ordinal.Natural.
 Require Import ZF.Set.Ordinal.Succ.
+Require Import ZF.Set.Union2.
+
+Require Import ZF.Notation.N.
+Export ZF.Notation.N.
 
 (* The class natural numbers.                                                   *)
 Definition omega : Class := fun a =>
   On a /\ toClass (succ a) :<=: NonLimit.
+
+(* Notation ":N" := omega                                                       *)
+Global Instance ClassN : N Class := { omega := omega }.
+
+Proposition NotEmpty : :N :<>: :0:.
+Proof.
+  intros H1. apply Class.Empty.Charac with :0:. apply H1. split.
+  - apply ZeroIsOrdinal.
+  - intros a H2.
+Admitted.
---
+++ coq/ZF/Notation/N.v
@@ -0,0 +1,8 @@
+Declare Scope ZF_Core_N_scope.
+Open    Scope ZF_Core_N_scope.
+
+
+Class N (v:Type) := { omega : v }.
+
+Notation ":N" := omega
+  (at level 0, no associativity) : ZF_Core_N_scope.
