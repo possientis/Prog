95aafea1bd0d6836b719b05e3c09601e71ea46d3
Paul Ossientis <paul_ossientis@riseup.net>
2025-04-25 08:50:05 +0300

    coq: ZF: 0 belongs to N

--- coq/ZF/Class/Ordinal/Omega.v
+++ coq/ZF/Class/Ordinal/Omega.v
@@ -6,6 +6,7 @@ Require Import ZF.Class.Ordinal.NonLimit.
 Require Import ZF.Set.Core.
 Require Import ZF.Set.Ordinal.Natural.
 Require Import ZF.Set.Ordinal.Succ.
+Require Import ZF.Set.Single.
 Require Import ZF.Set.Union2.
 
 Require Import ZF.Notation.N.
@@ -18,9 +19,23 @@ Definition omega : Class := fun a =>
 (* Notation ":N" := omega                                                       *)
 Global Instance ClassN : N Class := { omega := omega }.
 
+(* 0 belongs to the class omega. The notation ':N' can't be used here.          *)
+Proposition HasZero : omega :0:.
+Proof.
+  split.
+  - apply ZeroIsOrdinal.
+  - intros a H1. apply Union2.Charac in H1. destruct H1 as [H1|H1].
+    + apply Empty.Charac in H1. contradiction.
+    + apply Single.Charac in H1. subst. left. reflexivity.
+Qed.
+
+
+(*
 Proposition NotEmpty : :N :<>: :0:.
 Proof.
   intros H1. apply Class.Empty.Charac with :0:. apply H1. split.
   - apply ZeroIsOrdinal.
-  - intros a H2.
-Admitted.
+  - intros a H2. apply Union2.Charac in H2.
+
+Show.
+*)
