06481349c1bed8468ad8989ab43b1799496520ad
Paul Ossientis <paul_ossientis@riseup.net>
2025-05-03 19:21:02 +0300

    coq: ZF: Class.Ordinal.Sup

--- coq/ZF/Class/Ordinal/Sup.v
+++ coq/ZF/Class/Ordinal/Sup.v
@@ -5,6 +5,7 @@ Require Import ZF.Class.Ordinal.Core.
 Require Import ZF.Class.Ordinal.Union.
 Require Import ZF.Class.Union.
 Require Import ZF.Class.Inter2.
+Require Import ZF.Class.Small.
 Require Import ZF.Set.Core.
 
 Require Import ZF.Notation.SupBelow.
@@ -42,11 +43,37 @@ Proof.
   intros A. apply Ordinal.Union.IsOrdinal, Class.Inter2.InclR.
 Qed.
 
+Proposition IsOrdinalBelow : forall (A:Class) (b:U),
+  Ordinal (sup(:< b) A).
+Proof.
+  intros A b. apply Ordinal.Union.IsOrdinal. intros x H1.
+  destruct H1 as [H1 [H2 H3]]. assumption.
+Qed.
+
+Proposition IsSmallBelow : forall (A:Class) (b:U),
+  Small (sup(:< b) A).
+Proof.
+  intros A b. apply Union.IsSmall, Inter2.IsSmallR, Inter2.IsSmallR.
+  apply SetIsSmall.
+Qed.
+
+Proposition IsLessBelow : forall (A:Class) (b:U),
+  sup(:< b) A :<: On.
+Proof.
+  intros A b.
+  assert (sup(:< b) A :~: On \/ sup(:< b) A :<: On) as H1. {
+    apply Core.IsEquivOrLess, IsOrdinalBelow. }
+  destruct H1 as [H1|H1]. 2: assumption. exfalso.
+  apply OnIsProper. apply Small.EquivCompat with (sup(:< b) A).
+  1: assumption. apply IsSmallBelow.
+Qed.
+
+(*
 Proposition IsequivOrLess : forall (A:Class),
   sup A :~: On \/ sup A :<: On.
 Proof.
   intros A. apply Core.IsEquivOrLess, IsOrdinal.
 Qed.
-
+*)
 
 
