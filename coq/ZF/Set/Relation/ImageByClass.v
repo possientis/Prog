2c569d823017d566005a47ccc767f38f73b8e645
Paul Ossientis <paul_ossientis@riseup.net>
2025-06-15 17:11:04 +0100

    coq: ZF: Set.Relation.ImageByClass

--- coq/ZF/Set/Relation/ImageByClass.v
+++ coq/ZF/Set/Relation/ImageByClass.v
@@ -1,2 +1,45 @@
-Proposition TODO : True.
+Require Import ZF.Class.Core.
+Require Import ZF.Class.Relation.Functional.
+Require Import ZF.Class.Relation.Image.
+Require Import ZF.Class.Small.
+Require Import ZF.Set.Core.
+Require Import ZF.Set.Incl.
+Require Import ZF.Set.OrdPair.
+Require Import ZF.Set.Truncate.
+
+Export ZF.Notation.Image.
+
+(* Need to use 'truncate' as the image F[a] may not be small.                   *)
+Definition image (F:Class) (a:U) : U := truncate F:[toClass a]:.
+
+(* Notation "F :[ a ]:" := (image F a)                                          *)
+Global Instance SetImageByClass : Image Class U := { image := image }.
+
+Proposition ToClass : forall (F:Class) (a:U),
+  Functional F -> toClass F:[a]: :~: F:[toClass a]:.
+Proof.
+  intros F a H1. apply Truncate.WhenSmall. apply Image.IsSmall.
+  1: assumption. apply SetIsSmall.
+Qed.
+
+Proposition Charac : forall (F:Class) (a y:U),
+  Functional F ->  y :< F:[a]: -> exists x, x :< a /\ F :(x,y):.
+Proof.
+  intros F a y H1 H2. apply ToClass in H2. 2: assumption.
+  destruct H2 as [x [H2 H3]]. exists x. split; assumption.
+Qed.
+
+Proposition CharacRev : forall (F:Class) (a x y:U),
+  Functional F -> x :< a -> F :(x,y): -> y :< F:[a]:.
+Proof.
+  intros F a x y H1 H2 H3. apply ToClass. 1: assumption.
+  exists x. split; assumption.
+Qed.
+
+Proposition EquivCompat : forall (F G:Class) (a:U),
+  F :~: G -> F:[a]: = G:[a]:.
+Proof.
+  intros F G a H1.
 Admitted.
+
+
