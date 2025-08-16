d11b2ae30ced39cbd447f7f6399d535e57b9c96e
Paul Ossientis <paul_ossientis@riseup.net>
2025-08-16 13:08:40 +0300

    coq: ZF: Set.Ordinal.RecursionOfClass

--- coq/ZF/Set/Ordinal/RecursionOfClass.v
+++ coq/ZF/Set/Ordinal/RecursionOfClass.v
@@ -1,4 +1,5 @@
 Require Import ZF.Class.Equiv.
+Require Import ZF.Class.Ordinal.Induction.
 Require Import ZF.Class.Ordinal.Recursion.
 Require Import ZF.Set.Core.
 Require Import ZF.Set.Incl.
@@ -59,17 +60,54 @@ Proof.
     split; assumption.
 Qed.
 
-Lemma RestrictRecursive : forall (F:Class) (f a b:U),
-  Ordinal a                                           ->
-  b :<=: a                                            ->
-  FunctionOn f a                                      ->
-  (forall c, c :< a -> f!c = F!(f:|:c))               ->
-  (forall c, c :< b -> (f:|:b)!c = F!((f:|:b) :|: c)).
+(* The recursion set of F and a is a function defined on a, when a ordinal.     *)
+Proposition IsFunctionOn : forall (F:Class) (a:U), Ordinal a ->
+  FunctionOn (recursion F a) a.
 Proof.
-  intros F f a b H1 H2 H3 H4 c H5.
-  assert ((f:|:b)!c = f!c) as H6. {
-    apply Restrict.Eval. 2: assumption. apply H3. }
-  assert ((f:|:b) :|: c = f:|:c) as H7. {
-    apply FunctionOn.RestrictEqual with b a. 2: assumption.
-    - apply FunctionOn.Restrict with a; assumption.
-Admitted.
+  intros F a H1. apply RestrictIsFunctionOn. assumption.
+Qed.
+
+(* The recursion set of F and a is F-recursive, when a is an ordinal.           *)
+Proposition IsRecursive : forall (F:Class) (a:U), Ordinal a ->
+  forall b, b :< a -> (recursion F a)!b = F!((recursion F a) :|: b).
+Proof.
+  intros F a H1 b H2.
+  assert (b :<=: a) as H3. { apply Core.ElemIsIncl; assumption. }
+  assert ((recursion F a)!b = (Recursion F)!b) as H4. {
+    apply RestrictOfClass.Eval. 2: assumption. apply Recursion.IsFunctionOn. }
+  assert ((recursion F a) :|: b = (Recursion F) :|: b) as H5. {
+    apply FunctionOn.EqualCharac with b b. 3: reflexivity.
+    - apply FunctionOn.Restrict with a. 2: assumption.
+      apply IsFunctionOn. assumption.
+    - apply RestrictIsFunctionOn. apply Core.IsOrdinal with a; assumption.
+    - intros x H5. unfold recursion. rewrite RestrictOfClass.TowerProperty.
+      + reflexivity.
+      + apply Recursion.IsFunctionOn.
+      + assumption. }
+  rewrite H4, H5. apply Recursion.IsRecursive.
+  apply Core.IsOrdinal with a; assumption.
+Qed.
+
+(* The recursion set is the unique F-recursive function defined on a.           *)
+Proposition IsUnique : forall (F:Class) (a f:U),
+  Ordinal a                             ->
+  FunctionOn f a                        ->
+  (forall b, b :< a -> f!b = F!(f:|:b)) ->
+  f = recursion F a.
+Proof.
+  intros F a f H1 H2 H3. apply FunctionOn.EqualCharac with a a.
+  1: assumption. 2: reflexivity.
+  - apply IsFunctionOn. assumption.
+  - remember (fun x => x :< a -> f!x = (recursion F a)!x) as A eqn:H4.
+    assert (forall x, Ordinal x -> A x) as H5. {
+      apply Induction'. intros b H5 H6. rewrite H4 in H6. rewrite H4.
+      intros H7. rewrite H3. 2: assumption. rewrite IsRecursive; try assumption.
+    assert (b :<=: a) as H8. { apply Core.ElemIsIncl; assumption. }
+    assert (f:|:b = (recursion F a) :|: b) as H9. {
+      apply FunctionOn.RestrictEqual with a a; try assumption.
+      - apply IsFunctionOn. assumption.
+      - intros x H9. apply H6. 1: assumption. apply H8. assumption. }
+      rewrite H9. reflexivity. }
+    intros x H6. rewrite H4 in H5. apply H5. 2: assumption.
+    apply Core.IsOrdinal with a; assumption.
+Qed.
