4bee8075a45ab1ae270b2fdb3e959044f8e162dc
Paul Ossientis <paul_ossientis@riseup.net>
2025-08-30 15:10:07 +0100

    coq: ZF: refactor

---
+++ coq/ZF/Class/Order/Induction.v
@@ -0,0 +1,66 @@
+Require Import ZF.Axiom.Classic.
+Require Import ZF.Class.Incl.
+Require Import ZF.Class.Diff.
+Require Import ZF.Class.Empty.
+Require Import ZF.Class.Equiv.
+Require Import ZF.Class.Order.InitSegment.
+Require Import ZF.Class.Order.Minimal.
+Require Import ZF.Class.Order.WellFoundedWellOrd.
+
+
+Proposition Induction : forall (R A B:Class),
+  WellFoundedWellOrd R A                      ->
+  B :<=: A                                    ->
+  (forall x, initSegment R A x :<=: B -> B x) ->
+  A :~: B.
+Proof.
+
+  (* Let R A B be arbitrary classes. *)
+  intros R A B.
+
+  (* We assume that R is a well-founded well-ordering on A. *)
+  intros H1. assert (WellFoundedWellOrd R A) as X. apply H1. clear X.
+
+  (* We assume that B is a subclass of A. *)
+  intros H2. assert (B :<=: A) as X. apply H2. clear X.
+
+  (* We assume the inductive property. *)
+  intros H3. assert (forall x, initSegment R A x :<=: B -> B x) as X. apply H3. clear X.
+
+  (* We need to show that A = B. *)
+  assert (A :~: B) as X. 2: apply X.
+
+  (* It is sufficient to show that A <= B. *)
+  apply DoubleInclusion. split. 2: assumption. assert (A :<=: B) as X. 2: apply X.
+
+  (* Or equivalently that A\B = 0. *)
+  apply Diff.WhenEmpty. assert (A:\:B :~: :0:) as X. 2: apply X.
+
+  (* Suppose to the contrary that A\B <> 0. *)
+  apply DoubleNegation. intros H5. assert (~ A:\:B :~: :0:) as X. apply H5. clear X.
+
+  (* Then A\B has an R-minimal element. *)
+  assert (exists a, Minimal R (A:\:B) a) as H6. {
+    apply HasMinimal with A.
+    - assumption.
+    - apply Class.Inter2.IsInclL.
+    - assumption.
+  }
+
+  (* So let a be such an R-minimal element. *)
+  destruct H6 as [a H6]. assert (Minimal R (A:\:B) a) as X. apply H6. clear X.
+
+  (* So the initial segment in A at a must be inside B. *)
+  assert (initSegment R A a :<=: B) as H7. {
+    intros x H7. apply InitSegment.Charac in H7. destruct H7 as [H7 H8].
+    apply DoubleNegation. intros H9. revert H8.
+    apply (Minimal.HasNoLesser R (A:\:B)). 2: assumption.
+    split; assumption.
+  }
+
+  (* From the inductive property, it follows that a lies in B. *)
+  assert (B a) as H8. { apply H3. assumption. }
+
+  (* This contradicts the fact that a lies in A\B. *)
+  apply Minimal.IsIn in H6. destruct H6 as [_ H6]. contradiction.
+Qed.
--- coq/ZF/Class/Order/Recursion.v
+++ coq/ZF/Class/Order/Recursion.v
@@ -1,5 +1,8 @@
 Require Import ZF.Class.Equiv.
-Require Import ZF.Class.Order.WellOrdering.
+Require Import ZF.Class.Incl.
+Require Import ZF.Class.Order.Induction.
+Require Import ZF.Class.Order.InitSegment.
+Require Import ZF.Class.Order.WellFoundedWellOrd.
 Require Import ZF.Set.Core.
 Require Import ZF.Set.Order.InitSegment.
 Require Import ZF.Set.OrdPair.
@@ -8,6 +11,9 @@ Require Import ZF.Set.Relation.Eval.
 Require Import ZF.Set.Relation.EvalOfClass.
 Require Import ZF.Set.Relation.FunctionOn.
 Require Import ZF.Set.Relation.Restrict.
+
+Module COI := ZF.Class.Order.InitSegment.
+Module SOI := ZF.Set.Order.InitSegment.
 
 Definition Recursion (R A F:Class) : Class := fun x => exists f a,
   x :< f                                                                  /\
@@ -28,7 +34,7 @@ Proof.
 Qed.
 
 Lemma Coincide : forall (R A F:Class) (f g a b:U),
-  WellOrdering R A                                                        ->
+  WellFoundedWellOrd R A                                                  ->
   A a                                                                     ->
   A b                                                                     ->
   R :(a,b):                                                               ->
@@ -38,4 +44,14 @@ Lemma Coincide : forall (R A F:Class) (f g a b:U),
   (forall x, x :< initSegment R A b -> g!x = F!(g:|:initSegment R A x))   ->
   (forall x, x :< initSegment R A a -> f!x = g!x).
 Proof.
+  intros R A F f g a b H1 H2 H3 H4 H5 H6 H7 H8.
+  assert (forall x c, A c -> x :< initSegment R A c -> A x) as H9. {
+    intros x c H9 H10. apply (SOI.IsIncl R A A c) in H10; try assumption.
+    - apply H1.
+    - apply Class.Incl.Refl. }
+  remember (fun x => A x /\ (x :< initSegment R A a -> f!x = g!x)) as B eqn:H10.
+  assert (A :~: B) as H11. {
+    apply Induction with R. 1: assumption.
+    - intros x H11. rewrite H10 in H11. destruct H11 as [H11 _]. assumption.
+    - intros c H11. rewrite H10.
 Admitted.
