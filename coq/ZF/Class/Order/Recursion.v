d78e41be4e5a85a81911d7134b60e05dc37a8da3
Paul Ossientis <paul_ossientis@riseup.net>
2025-08-30 10:58:41 +0100

    coq: ZF: Class.Order.Recursion.

--- coq/ZF/Class/Order/Recursion.v
+++ coq/ZF/Class/Order/Recursion.v
@@ -1,26 +1,24 @@
 Require Import ZF.Class.Equiv.
-Require Import ZF.Class.Order.InitSegment.
 Require Import ZF.Class.Order.WellOrdering.
 Require Import ZF.Set.Core.
+Require Import ZF.Set.Order.InitSegment.
 Require Import ZF.Set.OrdPair.
 Require Import ZF.Set.Relation.Domain.
 Require Import ZF.Set.Relation.Eval.
 Require Import ZF.Set.Relation.EvalOfClass.
-Require Import ZF.Set.Relation.Function.
-Require Import ZF.Set.Relation.RestrictByClass.
+Require Import ZF.Set.Relation.FunctionOn.
+Require Import ZF.Set.Relation.Restrict.
 
 Definition Recursion (R A F:Class) : Class := fun x => exists f a,
-  x :< f                                                              /\
-  A a                                                                 /\
-  Function f                                                          /\
-  toClass (domain f) :~: initSegment R A a                            /\
-  (forall b, initSegment R A a b -> f!b = F!(f:|:initSegment R A b)).
+  x :< f                                                                  /\
+  A a                                                                     /\
+  FunctionOn f (initSegment R A a)                                        /\
+  (forall b, b :< initSegment R A a -> f!b = F!(f:|:initSegment R A b)).
 
 Definition K (R A F:Class) : U -> U -> Prop := fun f a =>
-  A a                                                                 /\
-  Function f                                                          /\
-  toClass (domain f) :~: initSegment R A a                            /\
-  (forall b, initSegment R A a b -> f!b = F!(f:|:initSegment R A b)).
+  A a                                                                     /\
+  FunctionOn f (initSegment R A a)                                        /\
+  (forall b, b :< initSegment R A a -> f!b = F!(f:|:initSegment R A b)).
 
 Lemma Charac2 : forall (R A F:Class) (x y:U),
   Recursion R A F :(x,y): <-> exists f a, :(x,y): :< f /\ K R A F f a.
@@ -30,16 +28,14 @@ Proof.
 Qed.
 
 Lemma Coincide : forall (R A F:Class) (f g a b:U),
-  WellOrdering R A                                                      ->
-  A a                                                                   ->
-  A b                                                                   ->
-  R :(a,b):                                                             ->
-  Function f                                                            ->
-  Function g                                                            ->
-  toClass (domain f) :~: initSegment R A a                              ->
-  toClass (domain g) :~: initSegment R A b                              ->
-  (forall x, initSegment R A a x  -> f!x = F!(f:|:initSegment R A x))   ->
-  (forall x, initSegment R A b x  -> g!x = F!(g:|:initSegment R A x))   ->
-  (forall x, initSegment R A a x  -> f!x = g!x).
+  WellOrdering R A                                                        ->
+  A a                                                                     ->
+  A b                                                                     ->
+  R :(a,b):                                                               ->
+  FunctionOn f (initSegment R A a)                                        ->
+  FunctionOn g (initSegment R A b)                                        ->
+  (forall x, x :< initSegment R A a -> f!x = F!(f:|:initSegment R A x))   ->
+  (forall x, x :< initSegment R A b -> g!x = F!(g:|:initSegment R A x))   ->
+  (forall x, x :< initSegment R A a -> f!x = g!x).
 Proof.
 Admitted.
