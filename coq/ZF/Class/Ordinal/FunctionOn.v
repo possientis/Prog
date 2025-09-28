b8ff01b30df22a6cd6f4eb61c10965c9957a5e3c
Paul Ossientis <paul_ossientis@riseup.net>
2025-09-28 20:22:02 +0100

    coq: ZF: Class.Ordinal.FunctionOn

--- coq/ZF/Class/Ordinal/FunctionOn.v
+++ coq/ZF/Class/Ordinal/FunctionOn.v
@@ -17,18 +17,25 @@ Require Import ZF.Class.Small.
 Require Import ZF.Set.Core.
 Require Import ZF.Set.Ordinal.Core.
 Require Import ZF.Set.OrdPair.
+Require Import ZF.Set.Relation.Function.
+Require Import ZF.Set.Relation.FunctionOn.
 Require Import ZF.Set.Relation.EvalOfClass.
 Require Import ZF.Set.Relation.ImageByClass.
 Require Import ZF.Set.Relation.OneToOne.
 Require Import ZF.Set.Relation.RestrictOfClass.
+Require Import ZF.Set.Specify.
 
 Module COC := ZF.Class.Ordinal.Core.
+Module CRF := ZF.Class.Relation.Function.
+Module CFO := ZF.Class.Relation.FunctionOn.
 Module CRO := ZF.Class.Relation.OneToOne.
 Module SOC := ZF.Set.Ordinal.Core.
+Module SRF := ZF.Class.Relation.Function.
+Module SFO := ZF.Set.Relation.FunctionOn.
 Module SRO := ZF.Set.Relation.OneToOne.
 
 Proposition WhenFreshValue : forall (F A:Class),
-  FunctionOn F On                                 ->
+  CFO.FunctionOn F On                                 ->
   (forall a, On a -> (A :\: toClass F:[a]:) F!a)  ->
   range F :<=: A                                  /\
   CRO.OneToOne F                                  /\
@@ -36,10 +43,10 @@ Proposition WhenFreshValue : forall (F A:Class),
 Proof.
   intros F A H1 H2.
   assert (range F :<=: A) as H3. {
-    intros b H3. apply (FunctionOn.RangeCharac F On) in H3. 2: assumption.
+    intros b H3. apply (CFO.RangeCharac F On) in H3. 2: assumption.
     destruct H3 as [a [H3 H4]]. subst. apply H2. assumption. }
   assert (CRO.OneToOne F) as H4. {
-    apply FunctionOn.IsOneToOne with On. 1: assumption.
+    apply CFO.IsOneToOne with On. 1: assumption.
     intros a b H4 H5 H6.
     assert (a = b \/ a :< b \/ b :< a) as H7. {
       apply SOC.OrdinalTotal; assumption. }
@@ -56,13 +63,13 @@ Proof.
       apply Bounded.WhenSmaller with A; assumption. }
     assert (Small On) as H7. {
       apply Small.EquivCompat with (domain F). 1: apply H1.
-      apply Function.DomainIsSmall; assumption. }
+      apply CRF.DomainIsSmall; assumption. }
     revert H7. apply COC.OnIsProper. }
   split. 1: assumption. split; assumption.
 Qed.
 
 Proposition WhenFreshAndSmall : forall (F A:Class),
-  FunctionOn F On                                                         ->
+  CFO.FunctionOn F On                                                     ->
   Small A                                                                 ->
   (forall a,
     On a                            ->
@@ -101,12 +108,37 @@ Proof.
     intros y H14. apply ImageByClass.Charac in H14. 2: apply H1.
     destruct H14 as [b [H14 H15]].
     assert (On b) as H16. { apply SOC.IsOrdinal with a; assumption. }
-    assert (F!b = y) as H17. { apply (FunctionOn.EvalCharac F On); assumption. }
+    assert (F!b = y) as H17. { apply (CFO.EvalCharac F On); assumption. }
     assert ((A :\: toClass F:[b]:) :<>: :0:) as H18. { apply H13. assumption. }
     assert ((A :\: toClass F:[b]:) F!b) as H19. { apply H3; assumption. }
     rewrite H17 in H19. apply H19. }
   assert (A :<=: toClass F:[a]:) as H15. { apply Diff.WhenEmpty. assumption. }
-  assert (A :~: toClass F:[a]:) as H16. {
+  assert (toClass F:[a]: :~: A) as H16. {
     apply DoubleInclusion. split; assumption. }
-  assert (SRO.OneToOne (F:|:a)) as H17. {
-Admitted.
+  assert (SFO.FunctionOn (F:|:a) a) as H17. {
+    split.
+    - apply RestrictOfClass.IsFunction, H1.
+    - rewrite RestrictOfClass.DomainOf. 2: apply H1.
+      apply Specify.IsA. intros b H17. apply H1.
+      apply SOC.IsOrdinal with a; assumption. }
+  assert (SRO.OneToOne (F:|:a)) as H18. {
+    apply SFO.IsOneToOne with a. 1: assumption.
+    intros b c H18 H19 H20.
+    assert (On b) as H21. { apply SOC.IsOrdinal with a; assumption. }
+    assert (On c) as H22. { apply SOC.IsOrdinal with a; assumption. }
+    assert ((F:|:a)!b = F!b) as H23. {
+      apply RestrictOfClass.Eval. 1: apply H1. assumption. }
+    assert ((F:|:a)!c = F!c) as H24. {
+      apply RestrictOfClass.Eval. 1: apply H1. assumption. }
+    rewrite H23 in H20. rewrite H24 in H20.
+    assert (b = c \/ b :< c \/ c :< b) as H25. {
+      apply SOC.OrdinalTotal; assumption. }
+    destruct H25 as [H25|[H25|H25]]. 1: assumption.
+    - exfalso. specialize (H13 c H19). specialize (H3 c H22 H13).
+      rewrite <- H20 in H3. destruct H3 as [_ H3]. apply H3.
+      apply ImageByClass.IsIn; try apply H1; assumption.
+    - exfalso. specialize (H13 b H18). specialize (H3 b H21 H13).
+      rewrite H20 in H3. destruct H3 as [_ H3]. apply H3.
+      apply ImageByClass.IsIn; try apply H1; assumption. }
+  exists a. split. 1: assumption. split. 1: assumption. split; assumption.
+Qed.
