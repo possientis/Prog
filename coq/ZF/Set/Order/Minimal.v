2e1b2a65f9a2df6670bb91857c45e8b70c83b429
Paul Ossientis <paul_ossientis@riseup.net>
2025-10-19 11:52:37 +0100

    coq: ZF: Set.Order.Minimal

---
+++ coq/ZF/Set/Order/Minimal.v
@@ -0,0 +1,17 @@
+Require Import ZF.Class.Equiv.
+Require Import ZF.Class.Order.Minimal.
+Require Import ZF.Set.Core.
+Require Import ZF.Set.OrdPair.
+
+
+Module COM := Class.Order.Minimal.
+
+(* Predicate expressing the fact that b is an r-minimal element of a.           *)
+Definition Minimal (r a b:U) : Prop
+  := b :< a /\ (forall x, x :< a  -> ~ :(x,b): :< r).
+
+Proposition ToClass : forall (r a b:U),
+  Minimal r a b <-> COM.Minimal (toClass r) (toClass a) b.
+Proof.
+  intros r a b. split; intros [H1 H2]; split; assumption.
+Qed.
