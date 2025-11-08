8fb2e78558d4b1d067939ba0e05210003ba0a9ad
Paul Ossientis <paul_ossientis@riseup.net>
2025-11-08 10:28:42 +0000

    coq: ZF: Notation + left assoc

--- coq/ZF/Notation/Plus.v
+++ coq/ZF/Notation/Plus.v
@@ -4,4 +4,4 @@ Open    Scope ZF_Notation_Plus_scope.
 Class Plus (v:Type) := { plus : v -> v -> v }.
 
 Notation "a :+: b" := (plus a b)
-  (at level 15, right associativity) : ZF_Notation_Plus_scope.
+  (at level 15, left associativity) : ZF_Notation_Plus_scope.
