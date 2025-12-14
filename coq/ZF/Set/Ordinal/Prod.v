8ddf5be6d9a1a55d68071253b8a03b420b1af262
Paul Ossientis <paul_ossientis@riseup.net>
2025-12-14 12:04:59 +0000

    coq: ZF: Set.Ordinal.Prod

--- coq/ZF/Set/Ordinal/Prod.v
+++ coq/ZF/Set/Ordinal/Prod.v
@@ -1,8 +1,11 @@
 Require Import ZF.Class.Equiv.
 Require Import ZF.Class.Ordinal.Order.Le.
 Require Import ZF.Set.Core.
+Require Import ZF.Set.Empty.
+Require Import ZF.Set.Foundation.
 Require Import ZF.Set.Incl.
 Require Import ZF.Set.Order.E.
+Require Import ZF.Set.Order.Isom.
 Require Import ZF.Set.Order.RestrictOfClass.
 Require Import ZF.Set.Ordinal.Core.
 Require Import ZF.Set.Ordinal.Mult.
@@ -10,16 +13,20 @@ Require Import ZF.Set.Ordinal.Plus.
 Require Import ZF.Set.Ordinal.Succ.
 Require Import ZF.Set.OrdPair.
 Require Import ZF.Set.Prod.
+Require Import ZF.Set.Relation.Bij.
 Require Import ZF.Set.Relation.Bijection.
 Require Import ZF.Set.Relation.BijectionOn.
 Require Import ZF.Set.Relation.Converse.
 Require Import ZF.Set.Relation.Domain.
+Require Import ZF.Set.Relation.Eval.
 Require Import ZF.Set.Relation.Functional.
 Require Import ZF.Set.Relation.OneToOne.
 Require Import ZF.Set.Relation.Functional.
 Require Import ZF.Set.Relation.Range.
 Require Import ZF.Set.Relation.Relation.
 Require Import ZF.Set.Specify.
+
+Require Import ZF.Notation.Eval. (* TODO: already exported by Eval so why ? *)
 
 Module SOR := ZF.Set.Order.RestrictOfClass.
 Module SPR := ZF.Set.Prod.
@@ -75,8 +82,8 @@ Qed.
 Lemma Inequality : forall (a b c d:U),
   Ordinal a                   ->
   Ordinal b                   ->
-  d :< a                      ->
   c :< b                      ->
+  d :< a                      ->
   a :*: c :+: d :< a :*: b.
 Proof.
   intros a b c d H1 H2 H3 H4.
@@ -180,9 +187,112 @@ Qed.
 Proposition RangeOf : forall (a b:U), Ordinal a -> Ordinal b ->
   range (f a b) = a :*: b.
 Proof.
-  intros a b H1 H2. apply DoubleInclusion. split; intros y H3.
+  intros a b H1 H2. apply DoubleInclusion.
+  assert (Ordinal (a :*: b)) as G1. { apply Mult.IsOrdinal; assumption. }
+  split; intros y H3.
   - apply Range.Charac in H3. destruct H3 as [x H3].
     apply Charac2 in H3; try assumption.
     destruct H3 as [c [d [H3 [H4 [H5 H6]]]]]. subst.
     apply Inequality; assumption.
-  - Admitted.
+  - assert (Ordinal y) as G2. {
+      apply Core.IsOrdinal with (a :*: b); assumption. }
+    assert (a = :0: \/ :0: :< a) as H4. { apply ZeroOrElem. assumption. }
+    destruct H4 as [H4|H4].
+    + subst. rewrite Mult.WhenZeroL in H3. 2: assumption.
+      apply Empty.Charac in H3. contradiction.
+    + assert (exists c d,
+        Ordinal c /\ Ordinal d /\ y = a :*: c :+: d /\ d :< a) as H5. {
+          apply Mult.Euclid; assumption. }
+      destruct H5 as [c [d [H5 [H6 [H7 H8]]]]].
+      assert (Ordinal (a :*: c)) as G3. { apply Mult.IsOrdinal; assumption. }
+      assert (c :< b \/ b :<=: c) as H9. { apply Core.ElemOrIncl; assumption. }
+      destruct H9 as [H9|H9].
+      * apply Range.Charac. exists :(c,d):. apply Charac2; try assumption.
+        exists c. exists d. split. 1: assumption. split. 1: assumption.
+        split. 2: assumption. reflexivity.
+      * exfalso.
+        assert (a :*: b :<=: y) as H10. {
+          apply Incl.Tran with (a :*: c).
+          - apply Mult.InclCompatR; assumption.
+          - rewrite H7. apply Plus.IsInclPlusR; assumption. }
+        assert (y :< y) as H11. { apply H10. assumption. }
+        revert H11. apply NoElemLoop1.
+Qed.
+
+
+(* f a b is a bijection from bxa to a*b.                                        *)
+Proposition IsBij : forall (a b:U), Ordinal a -> Ordinal b ->
+  Bij (f a b) (b :x: a) (a :*: b).
+Proof.
+  intros a b H1 H2. split.
+  - apply IsBijectionOn; assumption.
+  - apply RangeOf; assumption.
+Qed.
+
+Proposition Eval : forall (a b c d:U), Ordinal a -> Ordinal b ->
+  c :< b -> d :< a -> (f a b)!(:(c,d):) = a :*: c :+: d.
+Proof.
+  intros a b c d H1 H2 H3 H4.
+  apply Eval.Charac.
+  - apply IsFunctional; assumption.
+  - rewrite DomainOf; try assumption. apply SPR.Charac2. split; assumption.
+  - apply Charac2; try assumption. exists c. exists d.
+    split. 1: assumption. split. 1: assumption. split; reflexivity.
+Qed.
+
+(* f a b is an order isomorphism from bxa to a*b.                               *)
+Proposition IsIsom : forall (a b:U), Ordinal a -> Ordinal b ->
+  Isom (f a b) (le a b) (r a b) (b :x: a) (a :*: b).
+Proof.
+  intros a b H1 H2. split. 1: { apply IsBij; assumption. }
+  intros x y H3 H4.
+  apply SPR.Charac in H3. apply SPR.Charac in H4.
+  destruct H3 as [c [d [H3 [H5 H6]]]]. destruct H4 as [c' [d' [H4 [H7 H8]]]].
+  assert (Ordinal c)  as G1. { apply Core.IsOrdinal with b; assumption. }
+  assert (Ordinal d)  as G2. { apply Core.IsOrdinal with a; assumption. }
+  assert (Ordinal c') as G3. { apply Core.IsOrdinal with b; assumption. }
+  assert (Ordinal d') as G4. { apply Core.IsOrdinal with a; assumption. }
+  assert (Ordinal (a :*: c))  as G5. { apply Mult.IsOrdinal; assumption. }
+  assert (Ordinal (a :*: c')) as G6. { apply Mult.IsOrdinal; assumption. }
+  assert (Ordinal (a :*: c :+: d))   as G7. { apply Plus.IsOrdinal; assumption. }
+  assert (Ordinal (a :*: c' :+: d')) as G8. { apply Plus.IsOrdinal; assumption. }
+  assert (Ordinal (succ c))   as G9.  { apply Succ.IsOrdinal. assumption. }
+  assert (Ordinal (succ c'))  as G10. { apply Succ.IsOrdinal. assumption. }
+  assert (Ordinal (a :*: succ c))  as G11. { apply Mult.IsOrdinal; assumption. }
+  subst. split; intros H9.
+  - apply leCharac2 in H9. destruct H9 as [H9 [H10 [H11 [H12 H13]]]].
+    rewrite Eval, Eval; try assumption. apply E.Charac2. split.
+    + apply Inequality; assumption.
+    + split . 1: { apply Inequality; assumption. }
+      destruct H13 as [H13|[H13 H14]].
+      * apply Core.ElemInclTran with (a :*: succ c); try assumption.
+        { rewrite Mult.WhenSuccR. 2: assumption.
+          apply Plus.ElemCompatR; assumption. }
+        { apply Incl.Tran with (a :*: c').
+          - apply Mult.InclCompatR; try assumption.
+            apply Succ.ElemIsIncl; assumption.
+          - apply Plus.IsInclPlusR; assumption. }
+      * subst. apply Plus.ElemCompatR; assumption.
+  - rewrite Eval, Eval in H9; try assumption. apply E.Charac2 in H9.
+    destruct H9 as [H9 [H10 H11]].
+    apply leCharac2. split. 1: assumption. split. 1: assumption.
+    split. 1: assumption. split. 1: assumption.
+    assert (c = c' \/ c :< c' \/ c' :< c) as H12. {
+      apply Core.IsTotal; assumption. }
+    destruct H12 as [H12|[H12|H12]].
+    + right. split. 1: assumption. subst.
+      apply Plus.ElemCompatRevR with (a :*: c'); assumption.
+    + left. assumption.
+    + exfalso.
+      assert (a :*: c' :+: d' :<=: a :*: c :+: d) as H13. {
+        apply Incl.Tran with (a :*: succ c').
+        - rewrite Mult.WhenSuccR. 2: assumption.
+          apply Plus.InclCompatR; try assumption.
+          apply Core.ElemIsIncl; assumption.
+        - apply Incl.Tran with (a :*: c).
+          + apply Mult.InclCompatR; try assumption.
+            apply Succ.ElemIsIncl; assumption.
+          + apply Plus.IsInclPlusR; assumption. }
+      assert (a :*: c :+: d :< a :*: c :+: d) as H14. { apply H13. assumption. }
+      revert H14. apply NoElemLoop1.
+Qed.
