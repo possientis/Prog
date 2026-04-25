d77da8edae09b582ea343b773e4f56f582662b6e
Paul Ossientis <paul_ossientis@riseup.net>
2026-04-25 10:48:49 +0100

    coq: ZF: tweak

--- coq/ZF/Set/Relation/Fun/From2.v
+++ coq/ZF/Set/Relation/Fun/From2.v
@@ -18,6 +18,8 @@ Definition from2 (a b:U) (f:U*U -> U) : U := (CF2.from2 f) :|: (a :x: b).
 Proposition Charac : forall (f:U*U -> U) (a b x:U),
   x :< from2 a b f <-> exists u v, x = :(:(u,v):,f (u,v)): /\ u :< a /\ v :< b.
 Proof.
+  (* An element of from2 a b f is precisely a pair ((u,v), f(u,v)) with u in a  *)
+  (* and v in b, by definition of from2.                                        *)
   intros f a b x. split; intros H1.
   - apply SRR.Charac in H1. 2: apply CF2.IsFunctional.
     destruct H1 as [y [z [H1 [H2 H3]]]].
@@ -35,6 +37,8 @@ Qed.
 Proposition Charac2 : forall (f:U*U -> U) (a b u v w:U),
   :(:(u,v):,w): :< from2 a b f <-> u :< a /\ v :< b /\ w = f(u,v).
 Proof.
+  (* Proof by Claude.                                                           *)
+  (* Follows from the membership characterization and ordered-pair equality.    *)
   intros f a b u v w. split; intros H1.
   - apply Charac in H1. destruct H1 as [u' [v' [H1 [H2 H3]]]].
     apply OrdPair.WhenEqual in H1. destruct H1 as [H1 H4].
@@ -47,6 +51,8 @@ Qed.
 Proposition Satisfies : forall (f:U*U -> U) (a b u v:U),
   u :< a -> v :< b -> :(:(u,v):, f(u,v)): :< from2 a b f.
 Proof.
+  (* Proof by Claude.                                                           *)
+  (* Follows directly from the membership characterization.                     *)
   intros f a b u v H1 H2. apply Charac2.
   split. 1: assumption. split. 1: assumption. reflexivity.
 Qed.
@@ -54,6 +60,9 @@ Qed.
 Proposition IsFunctionOn : forall (f:U*U -> U) (a b:U),
   FunctionOn (from2 a b f) (a :x: b).
 Proof.
+  (* Proof by Claude.                                                           *)
+  (* The relation from2 a b f is functional on a x b: for each (u,v) in a x b,  *)
+  (* the pair ((u,v), f(u,v)) is the unique element with first component (u,v). *)
   intros f a b. unfold from2.
   apply SRR.IsFunctionOn. 1: apply CF2.IsFunctional.
   intros p H1. apply Prod.Charac in H1.
@@ -64,6 +73,9 @@ Qed.
 Proposition Eval : forall (f:U*U -> U) (a b u v:U),
   u :< a -> v :< b -> (from2 a b f)!(:(u,v):) = f(u,v).
 Proof.
+  (* Proof by Claude.                                                           *)
+  (* Since ((u,v), f(u,v)) is in from2 a b f and the relation is functional,    *)
+  (* the value of from2 a b f at (u,v) is f(u,v).                               *)
   intros f a b u v H1 H2.
   apply (FunctionOn.Eval (from2 a b f) (a :x: b)).
   - apply IsFunctionOn.
