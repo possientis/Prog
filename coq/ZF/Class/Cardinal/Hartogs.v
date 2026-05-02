169b79476107e6ca0a8f03a2ffb5fee95d1fb016
Paul Ossientis <paul_ossientis@riseup.net>
2026-05-02 14:34:44 +0100

    coq: ZF: Class.Cardinal.Hartogs: WIP

--- coq/ZF/Class/Cardinal/Hartogs.v
+++ coq/ZF/Class/Cardinal/Hartogs.v
@@ -15,12 +15,14 @@ Require Import ZF.Set.Prod.
 Require Import ZF.Set.Relation.Bij.
 Require Import ZF.Set.Relation.BijectionOn.
 Require Import ZF.Set.Relation.Converse.
+Require Import ZF.Set.Relation.Eval.
 Require Import ZF.Set.Relation.FunctionOn.
 Require Import ZF.Set.Relation.Image.
 Require Import ZF.Set.Relation.Inj.
 Require Import ZF.Set.Relation.EvalOfClass.
 Require Import ZF.Set.Relation.Fun.IfThenElse2.
 Require Import ZF.Set.Relation.Range.
+Require Import ZF.Set.Specify.
 
 Require Import ZF.Notation.Eval.
 Require Import ZF.Notation.Image.
@@ -69,5 +71,48 @@ Proof.
         assert (y = :0:) as H10. { rewrite <- H5, H9. reflexivity. }
         unfold hartogs. rewrite H10. split.
         * apply Core.ZeroIsOrdinal.
-        *
+        * exists :0:. apply Inj.WhenEmpty. reflexivity.
+    - destruct H4 as [H4 [i H5]].
+      remember (range i) as x eqn:H6.
+      assert (x :<=: a) as H7. { rewrite H6. apply H5. }
+      assert (Bij i y x) as H8. {
+        split.
+        - apply H5.
+        - symmetry. assumption. }
+(*
+      remember (i^:-1:) as j eqn:H9.
+      assert (Bij j x y) as H10. { rewrite H9. apply Bij.Converse. assumption. }
+      remember {{x :< x :x: x
+                | fun z => exists u v, z = :(u,v): /\ j!u :< j!v}} as r eqn:H12.
+      assert (forall u v,
+        :(u,v): :< r <-> u :< x /\ v :< x /\ j!u :< j!v) as H13. {
+          intros u v. split; intros H13.
+          - rewrite H12 in H13. apply Specify.Charac in H13.
+            destruct H13 as [H13 H14].
+            apply Prod.Charac2 in H13. destruct H13 as [H13 H15].
+            destruct H14 as [u' [v' [H14 H16]]].
+            apply OrdPair.WhenEqual in H14. destruct H14 as [H14 H17].
+            split. 1: assumption. split. 1: assumption.
+            rewrite H14, H17. assumption.
+          - destruct H13 as [H13 [H14 H15]]. rewrite H12.
+            apply Specify.Charac. split.
+            + apply Prod.Charac2. split; assumption.
+            + exists u, v. split. 2: assumption. reflexivity. }
+      assert (FunctionOn f (:P(a :x: a) :x: :P(a))) as H14. {
+        rewrite H3. apply IfThenElse2.IsFunctionOn. }
+      apply (FunctionOn.RangeCharac f (:P(a :x: a) :x: :P(a))). 1: assumption.
+      exists :(r,x):.
+      assert (r :< :P(a :x: a)) as H15. {
+        apply Power.Charac. intros z H15. rewrite H12 in H15.
+        apply Specify.Charac in H15. destruct H15 as [H15 _].
+        apply Prod.Charac in H15. destruct H15 as [u [v [H15 [H16 H17]]]].
+        apply Prod.Charac. exists u, v. split. 1: assumption.
+        split; apply H7; assumption. }
+      assert (x :< :P(a)) as H16. { apply Power.Charac. assumption. }
+      assert (:(r,x): :< :P(a :x: a) :x: :P(a)) as H17. {
+        apply Prod.Charac2. split; assumption. }
+      split. 1: assumption.
+      assert (WO :(r,x):) as H18. {
+        apply H2.
+*)
 Admitted.
