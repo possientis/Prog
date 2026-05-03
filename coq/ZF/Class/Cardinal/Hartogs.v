24b141c1ea6470dadf0c6014db860c1723905df2
Paul Ossientis <paul_ossientis@riseup.net>
2026-05-03 10:03:31 +0100

    coq: ZF: Class.Cardinal.Hartogs: WIP

--- coq/ZF/Class/Cardinal/Hartogs.v
+++ coq/ZF/Class/Cardinal/Hartogs.v
@@ -7,6 +7,7 @@ Require Import ZF.Set.Incl.
 Require Import ZF.Set.Empty.
 Require Import ZF.Set.Order.E.
 Require Import ZF.Set.Order.Isom.
+Require Import ZF.Set.Order.Transport.
 Require Import ZF.Set.Order.WellOrdering.
 Require Import ZF.Set.Ordinal.Core.
 Require Import ZF.Set.OrdPair.
@@ -27,6 +28,8 @@ Require Import ZF.Set.Specify.
 Require Import ZF.Notation.Eval.
 Require Import ZF.Notation.Image.
 
+Module CCI := ZF.Class.Cardinal.Isom.
+
 
 Definition hartogs (a:U) : Class := fun b =>
   Ordinal b /\ exists f, Inj f b a.
@@ -41,7 +44,7 @@ Proof.
       apply OrdPair.WhenEqual in H2. destruct H2 as [H2 H4]. subst. assumption.
     - rewrite H1. exists r, x. split. 2: assumption. reflexivity. }
   remember (ifThenElse2 :P(a :x: a) :P(a) WO
-      (fun r x => (isom!:(r,x):) :[x]:) (fun _ _ => :0:)) as f eqn:H3.
+      (fun r x => (CCI.isom!:(r,x):) :[x]:) (fun _ _ => :0:)) as f eqn:H3.
   assert (forall y, y :< range f <-> hartogs a y) as H4. {
     intros y. split; intros H4.
     - apply (FunctionOn.RangeCharac f (:P(a :x: a) :x: :P(a))) in H4.
@@ -49,12 +52,12 @@ Proof.
       destruct H4 as [u [H4 H5]]. apply Prod.Charac in H4.
       destruct H4 as [r [x [H4 [H6 H7]]]]. rewrite H4 in H5. clear H4.
       assert (WO :(r,x): \/ ~ WO :(r,x):) as [H8|H8]. { apply LawExcludedMiddle. }
-      + assert ((f!:(r,x):) = (isom!:(r,x):) :[x]:) as H9. {
+      + assert ((f!:(r,x):) = (CCI.isom!:(r,x):) :[x]:) as H9. {
           rewrite H3. rewrite IfThenElse2.Eval1 by assumption. reflexivity. }
         unfold hartogs.
         assert (Ordinal y) as H10. {
           rewrite <- H5, H9. apply Isom.IsOrdinal, H2. assumption. }
-        remember (isom!:(r,x):) as g eqn:H11.
+        remember (CCI.isom!:(r,x):) as g eqn:H11.
         assert (Isom g r (E y) x y) as H12. {
           rewrite H11. apply Isom.IsIsom.
           - apply H2. assumption.
@@ -79,40 +82,7 @@ Proof.
         split.
         - apply H5.
         - symmetry. assumption. }
-(*
-      remember (i^:-1:) as j eqn:H9.
-      assert (Bij j x y) as H10. { rewrite H9. apply Bij.Converse. assumption. }
-      remember {{x :< x :x: x
-                | fun z => exists u v, z = :(u,v): /\ j!u :< j!v}} as r eqn:H12.
-      assert (forall u v,
-        :(u,v): :< r <-> u :< x /\ v :< x /\ j!u :< j!v) as H13. {
-          intros u v. split; intros H13.
-          - rewrite H12 in H13. apply Specify.Charac in H13.
-            destruct H13 as [H13 H14].
-            apply Prod.Charac2 in H13. destruct H13 as [H13 H15].
-            destruct H14 as [u' [v' [H14 H16]]].
-            apply OrdPair.WhenEqual in H14. destruct H14 as [H14 H17].
-            split. 1: assumption. split. 1: assumption.
-            rewrite H14, H17. assumption.
-          - destruct H13 as [H13 [H14 H15]]. rewrite H12.
-            apply Specify.Charac. split.
-            + apply Prod.Charac2. split; assumption.
-            + exists u, v. split. 2: assumption. reflexivity. }
-      assert (FunctionOn f (:P(a :x: a) :x: :P(a))) as H14. {
-        rewrite H3. apply IfThenElse2.IsFunctionOn. }
-      apply (FunctionOn.RangeCharac f (:P(a :x: a) :x: :P(a))). 1: assumption.
-      exists :(r,x):.
-      assert (r :< :P(a :x: a)) as H15. {
-        apply Power.Charac. intros z H15. rewrite H12 in H15.
-        apply Specify.Charac in H15. destruct H15 as [H15 _].
-        apply Prod.Charac in H15. destruct H15 as [u [v [H15 [H16 H17]]]].
-        apply Prod.Charac. exists u, v. split. 1: assumption.
-        split; apply H7; assumption. }
-      assert (x :< :P(a)) as H16. { apply Power.Charac. assumption. }
-      assert (:(r,x): :< :P(a :x: a) :x: :P(a)) as H17. {
-        apply Prod.Charac2. split; assumption. }
-      split. 1: assumption.
-      assert (WO :(r,x):) as H18. {
-        apply H2.
-*)
+      remember (transport i (E y) y) as r eqn:H9.
+      assert (Isom i (E y) r y x) as H10. { apply Isom.Transport; assumption. }
+      assert (WellOrdering (E y) y) as H11. {
 Admitted.
