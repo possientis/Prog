c453326a7077fd4e524301f956ee08a531330a21
Paul Ossientis <paul_ossientis@riseup.net>
2026-05-21 20:07:33 +0100

    coq: ZF: Class.Relation.Eval refactor

--- coq/ZF/Class/Relation/Eval.v
+++ coq/ZF/Class/Relation/Eval.v
@@ -4,38 +4,11 @@ Require Import ZF.Class.Relation.Domain.
 Require Import ZF.Class.Empty.
 Require Import ZF.Class.Relation.Functional.
 Require Import ZF.Class.Relation.FunctionalAt.
+Require Export ZF.Class.Relation.IsValueAt.
 Require Import ZF.Class.Inter2.
 Require Import ZF.Class.Small.
 Require Import ZF.Set.Core.
 Require Import ZF.Set.OrdPair.
-
-(* Predicate expressing the fact that y is the value of F at a.                 *)
-Definition IsValueAt (F:Class) (a y:U) : Prop := F :(a,y): /\ FunctionalAt F a.
-
-Proposition IsValueAtCompat : forall (F G:Class) (a y:U),
-  F :~: G -> IsValueAt F a y -> IsValueAt G a y.
-Proof.
-  intros F G a y H1 [H2 H3]. split.
-  - apply H1. assumption.
-  - apply FunctionalAt.EquivCompat with F; assumption.
-Qed.
-
-(* When F is functional at a, y being a value of F at a reduces to F (a,y).     *)
-Proposition IsValueAtWhenFunctionalAt : forall (F:Class) (a y:U),
-  FunctionalAt F a -> IsValueAt F a y <-> F :(a,y):.
-Proof.
-  intros F a y H1. split; intros H2.
-  - destruct H2 as [H2 _]. assumption.
-  - split; assumption.
-Qed.
-
-(* Whem F is functional, y being a value of F at a reduces to F (a,y).          *)
-Proposition IsValueAtWhenFunctional : forall (F:Class) (a y:U),
-  Functional F -> IsValueAt F a y <-> F :(a,y):.
-Proof.
-  intros F a y H1. apply IsValueAtWhenFunctionalAt.
-  apply IsFunctionalAt. assumption.
-Qed.
 
 (* Predicate expressing the fact that the class F has a value at a.             *)
 Definition HasValueAt (F:Class) (a:U) : Prop := exists y, IsValueAt F a y.
@@ -48,7 +21,7 @@ Proof.
   - destruct H1 as [y [H1 H2]]. split. 1: assumption.
     exists y. assumption.
   - destruct H1 as [H1 H2]. destruct H2 as [y H2]. exists y.
-    apply IsValueAtWhenFunctionalAt; assumption.
+    apply IsValueAt.WhenFunctionalAt; assumption.
 Qed.
 
 Proposition HasValueAtWhenFunctionalAt : forall (F:Class) (a:U),
@@ -76,8 +49,8 @@ Proposition EquivCompat : forall (F G:Class) (a:U),
 Proof.
   intros F G a H1 x. split; intros H2; destruct H2 as [y [H2 H3]]; exists y;
   split; try assumption.
-  - apply IsValueAtCompat with F; assumption.
-  - apply IsValueAtCompat with G. 2: assumption. apply Equiv.Sym. assumption.
+  - apply IsValueAt.EquivCompat with F; assumption.
+  - apply IsValueAt.EquivCompat with G. 2: assumption. apply Equiv.Sym. assumption.
 Qed.
 
 (* If F has a value at a, then y corresponds to a iff eval F a = y.             *)
@@ -147,55 +120,56 @@ Qed.
 (* The value of F at a is a always a small class.                               *)
 Proposition IsSmall : forall (F:Class) (a:U), Small (eval F a).
 Proof.
-  (* Let F be an arbitrary class. *)
+  (* Let F be an arbitrary class.                                               *)
   intros F.
 
-  (* Let a be an arbitrary set. *)
+  (* Let a be an arbitrary set.                                                 *)
   intros a.
 
-  (* Either F has a value at a or it has not. *)
+  (* Either F has a value at a or it has not.                                   *)
   assert (HasValueAt F a \/ ~ HasValueAt F a) as H1. { apply LawExcludedMiddle. }
 
-  (* We consider these two cases separately. *)
+  (* We consider these two cases separately.                                    *)
   destruct H1 as [H1|H1].
 
-  (* So we assume that F as a value at a. *)
+  (* So we assume that F as a value at a.                                       *)
   - assert (HasValueAt F a) as A. { apply H1. } clear A.
 
-  (* Let y be a value of F at a. *)
+  (* Let y be a value of F at a.                                                *)
     assert (H2 := H1). destruct H2 as [y H2].
     assert (IsValueAt F a y) as A. { apply H2. } clear A.
 
-  (* In particular we have F (a,y) *)
+  (* In particular we have F (a,y)                                              *)
     destruct H2 as [H2 _].
     assert (F :(a,y):) as A. { apply H2. } clear A.
 
-  (* And it follows that eval F a = y. *)
+  (* And it follows that eval F a = y.                                          *)
     assert (eval F a :~: toClass y) as H3.
        { apply WhenHasValueAt; assumption. }
 
-  (* Using this equivalence ... *)
+  (* Using this equivalence ...                                                 *)
     apply Small.EquivCompat with (toClass y). 1: { apply Equiv.Sym, H3. }
 
-  (* We simply need to show that the set y is small *)
+  (* We simply need to show that the set y is small                             *)
     assert (Small (toClass y)) as A. 2: apply A.
 
-  (* Which is clear. *)
+  (* Which is clear.                                                            *)
     apply SetIsSmall.
 
-  (* We now assume that F as no value at a. *)
+  (* We now assume that F as no value at a.                                     *)
   - assert (~ HasValueAt F a) as A. { apply H1. } clear A.
 
-  (* Then eval F a is the empty class. *)
+  (* Then eval F a is the empty class.                                          *)
     assert (eval F a :~: :0:) as H2. { apply WhenNotHasValueAt, H1. }
 
-  (* Using this equivalence ... *)
+  (* Using this equivalence ...                                                 *)
     apply Small.EquivCompat with :0:. 1: { apply Equiv.Sym, H2. }
 
-  (* We simply need to show that the empty class is small. *)
+  (* We simply need to show that the empty class is small.                      *)
     assert (Small :0:) as A. 2: apply A.
 
-  (* Which we know is true. *)
+  (* Which we know is true.                                                     *)
     apply Empty.IsSmall.
 Qed.
+
 
