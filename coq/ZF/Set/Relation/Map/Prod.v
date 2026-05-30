e23e3a4342c6fd0f144b541fe147ba13f4187e6c
Paul Ossientis <paul_ossientis@riseup.net>
2026-05-30 14:45:01 +0100

    coq: ZF: Set.Relation.Map.Prod.ComposeL

--- coq/ZF/Set/Relation/Map/Prod.v
+++ coq/ZF/Set/Relation/Map/Prod.v
@@ -1,11 +1,12 @@
 Require Import ZF.Set.Core.
 Require Import ZF.Set.OrdPair.
 Require Import ZF.Set.Prod.
+Require Import ZF.Set.Relation.Compose.
+Require Import ZF.Set.Relation.Eval.
 Require Import ZF.Set.Relation.Fun.
 Require Import ZF.Set.Relation.Fun.From.
 Require Import ZF.Set.Relation.Fun.From2.
 Require Import ZF.Set.Relation.Map.
-Require Import ZF.Notation.Eval.
 
 (* The fork of f and g sends x to the ordered pair (f(x),g(x)).                 *)
 Definition fork (c f g:U) : U := from c (fun x => :(f!x,g!x):).
@@ -23,6 +24,28 @@ Proof.
   intros x H3. apply Prod.Charac2. split.
   - apply Fun.IsInRange with c; assumption.
   - apply Fun.IsInRange with c; assumption.
+Qed.
+
+(* Composing the fork with the left projection gives the left component.        *)
+Proposition ComposeL : forall (c a b f g:U),
+  Fun f c a -> Fun g c b -> (outL a b) :.: (fork c f g) = f.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros c a b f g H1 H2. apply Fun.Equal with c a c a.
+  2: assumption. 2: reflexivity.
+  - apply Fun.Compose with (a :x: b).
+    + apply IsFun; assumption.
+    + apply Prod.IsFunL.
+  - intros x H3.
+    rewrite (Fun.ComposeEval (fork c f g) (outL a b) c (a :x: b) a x).
+    4: assumption.
+    + unfold fork. rewrite From.Eval. 2: assumption.
+      rewrite Prod.EvalL.
+      * reflexivity.
+      * apply Fun.IsInRange with c; assumption.
+      * apply Fun.IsInRange with c; assumption.
+    + apply IsFun; assumption.
+    + apply Prod.IsFunL.
 Qed.
 
 (* The fork map is a map from map(c,a) x map(c,b) to map(c,a x b).              *)
