7022580b49519d3f1cc4989f7511c7f0911a082f
Paul Ossientis <paul_ossientis@riseup.net>
2026-06-07 09:15:53 +0100

    Set.Ordinal.Max.ElemCompat

--- coq/ZF/Set/Ordinal/Max.v
+++ coq/ZF/Set/Ordinal/Max.v
@@ -22,3 +22,28 @@ Proof.
     apply IsLeftOrRight; assumption. }
   destruct H3 as [H3|H3]; rewrite H3; assumption.
 Qed.
+
+(* If two ordinals belong to a set, then so does their maximum.                 *)
+Proposition ElemCompat : forall (a b c:U), Ordinal a -> Ordinal b ->
+  a :< c -> b :< c -> a :\/: b :< c.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros a b c H1 H2 H3 H4.
+  (* The maximum of two ordinals is one of the two ordinals.                    *)
+  assert (a :\/: b = a \/ a :\/: b = b) as H5. {
+    apply IsLeftOrRight; assumption. }
+  (* Hence membership of the maximum follows from the corresponding member.     *)
+  destruct H5 as [H5|H5]; rewrite H5; assumption.
+Qed.
+
+Proposition IsInclL : forall (a b:U),
+  a :<=: a :\/: b.
+Proof.
+  apply Union2.IsInclL.
+Qed.
+
+Proposition IsInclR : forall (a b:U),
+  b :<=: a :\/: b.
+Proof.
+  apply Union2.IsInclR.
+Qed.
