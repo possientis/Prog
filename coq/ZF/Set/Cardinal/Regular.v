d70aa635fc715e09fcb866ecb5aba09c9e28aa19
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-01 12:19:38 +0100

    Set.Cardinal.Regular.WhenOmega

--- coq/ZF/Set/Cardinal/Regular.v
+++ coq/ZF/Set/Cardinal/Regular.v
@@ -2,6 +2,16 @@ Require Import ZF.Class.Equiv.
 Require Import ZF.Class.Cardinal.InfiniteCard.
 Require Import ZF.Set.Core.
 Require Import ZF.Set.Ordinal.Character.
+Require Import ZF.Set.Ordinal.Omega.
 
 (* The set a is a regular cardinal.                                             *)
 Definition Regular (a:U) : Prop := InfiniteCard a /\ charac a = a.
+
+(* Omega is a regular cardinal.                                                 *)
+Proposition WhenOmega : Regular :N.
+Proof.
+(* Proof by Hermes + gpt 5.5                                                    *)
+  split.
+  - apply InfiniteCard.HasOmega.
+  - apply Character.WhenOmega.
+Qed.
