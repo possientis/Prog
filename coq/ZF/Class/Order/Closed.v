868e8bd1de345a203455ee51cbb5d3659eeb79ff
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-21 13:44:37 +0300

    coq: ZF: Class.Order.Closed refactor

--- coq/ZF/Class/Order/Closed.v
+++ coq/ZF/Class/Order/Closed.v
@@ -35,8 +35,73 @@ Definition Closed (R A:Class) : Prop := R:[A]: :<=: A.
 (* R is closed on A^2.                                                          *)
 Definition Closed2 (R A:Class) : Prop := R:[A:x:A]: :<=: A.
 
-(* A subset of A has a superset in A closed under finitely many operations.     *)
-Proposition Exists : forall (R S A:Class) (p q a:U),
+(* The i-th unary image used in the finite closure construction.                *)
+Definition unaryImage (R:Class) (i x:U) : U := truncate R$i:[toClass x]:.
+
+(* The i-th binary image used in the finite closure construction.               *)
+Definition binaryImage (S:Class) (i x:U) : U := truncate S$i:[toClass (x:x:x)]:.
+
+(* The union of the finitely many unary images at one stage.                    *)
+Definition unaryUnion (R:Class) (p x:U) : U :=
+  :\/:_{p} (From.from (fun i => unaryImage R i x)).
+
+(* The union of the finitely many binary images at one stage.                   *)
+Definition binaryUnion (S:Class) (q x:U) : U :=
+  :\/:_{q} (From.from (fun i => binaryImage S i x)).
+
+(* One stage of closing under all listed operations.                            *)
+Definition step (R S:Class) (p q x:U) : U :=
+  x :\/: unaryUnion R p x :\/: binaryUnion S q x.
+
+(* The omega-sequence obtained by iterating the closure step.                   *)
+Definition seq (R S:Class) (p q a:U) : U :=
+  recursion (From.from (step R S p q)) a.
+
+(* The closure hull obtained by taking the union of all finite stages.          *)
+Definition hull (R S:Class) (p q a:U) : U := :\/:_{:N} (seq R S p q a).
+
+(* The closure sequence starts at the original set.                             *)
+Proposition WhenZero : forall (R S:Class) (p q a:U),
+  (seq R S p q a)!:0: = a.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros R S p q a. unfold seq. rewrite RecursionNOfClass.WhenZero. reflexivity.
+Qed.
+
+(* The closure sequence advances by one closure step.                           *)
+Proposition WhenSucc : forall (R S:Class) (p q a n:U), n :< :N ->
+  (seq R S p q a)!(succ n) = step R S p q ((seq R S p q a)!n).
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros R S p q a n H1. unfold seq at 1.
+  rewrite RecursionNOfClass.WhenSucc. 2: assumption.
+  rewrite From.Eval. reflexivity.
+Qed.
+
+(* Every finite stage is included in the closure hull.                          *)
+Proposition IsIncl : forall (R S:Class) (p q a n:U), n :< :N ->
+  (seq R S p q a)!n :<=: hull R S p q a.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros R S p q a n H1 x H2.
+  apply UnionGen.Charac. exists n. split; assumption.
+Qed.
+
+(* The closure sequence is increasing.                                          *)
+Proposition IsMono : forall (R S:Class) (p q a n m:U), n :< :N -> m :< :N ->
+  n :<=: m -> (seq R S p q a)!n :<=: (seq R S p q a)!m.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros R S p q a n m H1. revert m. apply Omega.Induction'. 1: assumption.
+  - apply Incl.Refl.
+  - intros m H2 H3 H4.
+    apply Incl.Tran with ((seq R S p q a)!m). 1: assumption.
+    rewrite WhenSucc. 2: assumption.
+    apply Union2.IsInclL.
+Qed.
+
+(* The explicit closure hull has the expected closure properties.               *)
+Proposition Hull : forall (R S A:Class) (p q a:U),
   p :< :N                                                                   ->
   q :< :N                                                                   ->
   toClass a :<=: A                                                          ->
@@ -44,41 +109,37 @@ Proposition Exists : forall (R S A:Class) (p q a:U),
   (forall i, i :< q -> Closed2 S$i A)                                       ->
   (forall i x, i :< p -> toClass x :<=: A -> Small R$i:[toClass x      ]:)  ->
   (forall i x, i :< q -> toClass x :<=: A -> Small S$i:[toClass (x:x:x)]:)  ->
-  exists b,
-    a :<=: b                                        /\
-    toClass b :<=: A                                /\
-    (forall i, i :< p -> Closed  R$i (toClass b))   /\
-    (forall i, i :< q -> Closed2 S$i (toClass b)).
+    a :<=: hull R S p q a                                                   /\
+    toClass (hull R S p q a) :<=: A                                         /\
+    (forall i, i :< p -> Closed  R$i (toClass (hull R S p q a)))            /\
+    (forall i, i :< q -> Closed2 S$i (toClass (hull R S p q a))).
 Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
   intros R S A p q a H1 H2 H3 H4 H5 H6 H7.
-  remember (fun i x => truncate (R$i:[toClass x]:)) as R_ eqn:H8.
-  remember (fun i x => truncate (S$i:[toClass (x:x:x)]:)) as S_ eqn:H9.
-  remember (fun x => :\/:_{p} (From.from (fun i => R_ i x))) as R' eqn:H10.
-  remember (fun x => :\/:_{q} (From.from (fun i => S_ i x))) as S' eqn:H11.
-  remember (fun x => x :\/: R' x :\/: S' x) as G eqn:H12.
-  remember (recursion (From.from G) a) as f eqn:H13.
-  remember (:\/:_{:N} f) as b eqn:H14.
-  exists b.
+  remember (seq R S p q a) as f eqn:H8.
+  remember (hull R S p q a) as b eqn:H9.
   assert (:0: :< :N) as G0. { apply Omega.HasZero. }
-  assert (domain f = :N) as G1. { rewrite H13. apply RecursionNOfClass.IsFunctionOn. }
-  assert (Functional f) as G2. { rewrite H13. apply RecursionNOfClass.IsFunctionOn. }
+  assert (domain f = :N) as G1. {
+    rewrite H8. apply RecursionNOfClass.IsFunctionOn. }
+  assert (Functional f) as G2. {
+    rewrite H8. apply RecursionNOfClass.IsFunctionOn. }
   assert (forall x, x :< b <-> exists n, n :< :N /\ x :< f!n) as G3. {
-    rewrite H14. apply UnionGen.Charac. }
+    rewrite H9, H8. apply UnionGen.Charac. }
   assert (forall n, n :< :N -> f!n :<=: b) as G4. {
     intros n G4 x G5. apply G3. exists n. split; assumption. }
-  assert (a = f!:0:) as G5. { rewrite H13, RecursionNOfClass.WhenZero. reflexivity. }
-  assert (forall n, n :< :N -> f!(succ n) = f!n :\/: R' f!n :\/: S' f!n) as G6. {
-    intros n G6. rewrite H13, RecursionNOfClass.WhenSucc, <- H13, From.Eval, H12.
-    2: assumption. reflexivity. }
-  assert (forall x, toClass x :<=: A -> toClass (R' x) :<=: A) as G7. {
-    intros c G7. rewrite H10. apply UnionGenOfClass.WhenClassBounded.
-    intros i G8. rewrite From.Eval, H8.
+  assert (a = f!:0:) as G5. { rewrite H8. symmetry. apply WhenZero. }
+  assert (forall n, n :< :N ->
+    f!(succ n) = f!n :\/: unaryUnion R p f!n :\/: binaryUnion S q f!n) as G6. {
+    intros n G6. rewrite H8. rewrite WhenSucc. 2: assumption. reflexivity. }
+  assert (forall x, toClass x :<=: A -> toClass (unaryUnion R p x) :<=: A) as G7. {
+    intros c G7. apply UnionGenOfClass.WhenClassBounded.
+    intros i G8. rewrite From.Eval.
     apply Truncate.IsIncl. apply CIN.Tran with R$i:[A]:.
     - apply CRI.InclCompatR. assumption.
     - apply H4. assumption. }
-  assert (forall x, toClass x :<=: A -> toClass (S' x) :<=: A) as G8. {
-    intros x G8. rewrite H11. apply UnionGenOfClass.WhenClassBounded.
-    intros i G9. rewrite From.Eval, H9.
+  assert (forall x, toClass x :<=: A -> toClass (binaryUnion S q x) :<=: A) as G8. {
+    intros x G8. apply UnionGenOfClass.WhenClassBounded.
+    intros i G9. rewrite From.Eval.
     apply Truncate.IsIncl. apply CIN.Tran with S$i:[A:x:A]:.
     - apply CRI.InclCompatR.
       apply CIN.EquivCompatL with (toClass x :x: toClass x).
@@ -86,23 +147,23 @@ Proof.
       + apply CPR.InclCompat; assumption.
     - apply H5. assumption. }
   assert (forall n, n :< :N -> f!n :<=: f!(succ n)) as G9. {
-    intros n G9. rewrite G6. 2: assumption.  apply Union2.IsInclL. }
+    intros n G9. rewrite G6. 2: assumption. apply Union2.IsInclL. }
   assert (forall n m, n :< :N -> m :< :N -> n :<=: m -> f!n :<=: f!m) as G10. {
     intros n m G10. revert m. apply Omega.Induction'. 1: assumption.
     - apply Incl.Refl.
     - intros m G11 G12 G13. apply Incl.Tran with f!m. 1: assumption.
       apply G9. assumption. }
-
-  assert (forall n, n :< :N -> R' f!n :\/: S' f!n :<=: f!(succ n)) as H16. {
-    intros n H16. rewrite G6. 2: assumption.  apply Union2.IsInclR. }
-  assert (forall n, n :< :N -> R' f!n :<=: f!(succ n)) as H17. {
+  assert (forall n, n :< :N ->
+    unaryUnion R p f!n :\/: binaryUnion S q f!n :<=: f!(succ n)) as H16. {
+    intros n H16. rewrite G6. 2: assumption. apply Union2.IsInclR. }
+  assert (forall n, n :< :N -> unaryUnion R p f!n :<=: f!(succ n)) as H17. {
     intros n H17.
-    apply Incl.Tran with (R' f!n :\/: S' f!n).
+    apply Incl.Tran with (unaryUnion R p f!n :\/: binaryUnion S q f!n).
     - apply Union2.IsInclL.
     - apply H16. assumption. }
-  assert (forall n, n :< :N -> S' f!n :<=: f!(succ n)) as H18. {
+  assert (forall n, n :< :N -> binaryUnion S q f!n :<=: f!(succ n)) as H18. {
     intros n H18.
-    apply Incl.Tran with (R' f!n :\/: S' f!n).
+    apply Incl.Tran with (unaryUnion R p f!n :\/: binaryUnion S q f!n).
     - apply Union2.IsInclR.
     - apply H16. assumption. }
   assert (a :<=: b) as H19. { rewrite G5. apply G4. assumption. }
@@ -122,9 +183,9 @@ Proof.
     apply G3 in H23. destruct H23 as [n [H23 H25]].
     assert (succ n :< :N) as K1. { apply Omega.HasSucc. assumption. }
     apply G4 with (succ n). 1: assumption. rewrite G6. 2: assumption.
-    apply Union2.Charac3. right. left. rewrite H10.
+    apply Union2.Charac3. right. left.
     apply UnionGenOfClass.Charac. exists i. split. 1: assumption.
-    rewrite From.Eval, H8. apply Truncate.Charac. split.
+    rewrite From.Eval. apply Truncate.Charac. split.
     - apply H6. 1: assumption. apply H20. assumption.
     - exists x. split; assumption. }
   assert (forall i, i :< q -> Closed2 S$i (toClass b)) as H23. {
@@ -140,12 +201,34 @@ Proof.
     assert (u :< f!r) as H33. { apply (G10 n r); assumption. }
     assert (v :< f!r) as H34. { apply (G10 m r); assumption. }
     apply G4 with (succ r). 1: assumption. rewrite G6. 2: assumption.
-    apply Union2.Charac3. right. right. rewrite H11.
+    apply Union2.Charac3. right. right.
     apply UnionGenOfClass.Charac. exists i. split. 1: assumption.
-    rewrite From.Eval, H9. apply Truncate.Charac. split.
+    rewrite From.Eval. apply Truncate.Charac. split.
     - apply H7. 1: assumption. apply H20. assumption.
     - exists x. split. 2: assumption. rewrite H24.
       apply Prod.Charac2. split; assumption. }
-  split. 1: assumption. split. 1: assumption. split; assumption.
+  split. 1: apply H19. split. 1: apply H21.
+  split.
+  - apply H22.
+  - apply H23.
 Qed.
 
+(* A subset of A has a superset in A closed under finitely many operations.     *)
+Proposition Exists : forall (R S A:Class) (p q a:U),
+  p :< :N                                                                   ->
+  q :< :N                                                                   ->
+  toClass a :<=: A                                                          ->
+  (forall i, i :< p -> Closed  R$i A)                                       ->
+  (forall i, i :< q -> Closed2 S$i A)                                       ->
+  (forall i x, i :< p -> toClass x :<=: A -> Small R$i:[toClass x      ]:)  ->
+  (forall i x, i :< q -> toClass x :<=: A -> Small S$i:[toClass (x:x:x)]:)  ->
+  exists b,
+    a :<=: b                                        /\
+    toClass b :<=: A                                /\
+    (forall i, i :< p -> Closed  R$i (toClass b))   /\
+    (forall i, i :< q -> Closed2 S$i (toClass b)).
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros R S A p q a H1 H2 H3 H4 H5 H6 H7. exists (hull R S p q a).
+  apply Hull; assumption.
+Qed.
