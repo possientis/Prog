cd59f3325fa7e9133bc90b10efe3ebccf13c0898
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-24 17:08:14 +0300

    Meta.Test.Axiom.NonEmptyUniverse

---
+++ coq/ZF/Meta/Test/Axiom/NonEmptyUniverse.v
@@ -0,0 +1,15 @@
+Require Import ZF.Meta.Ctx.
+Require Import ZF.Meta.Sig.
+Require Import ZF.Meta.Term.
+Require Import ZF.Meta.Ty.
+
+(* Source: exists x, True.                                                      *)
+Definition NonEmptyUniverse : Term :=
+  Ex VarTySet Top.
+
+(* The non-empty-universe example is a proposition in the empty environment.    *)
+Proposition HasTy : HasTy Sig.empty Ctx.empty NonEmptyUniverse TyProp.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  apply HasTyEx, HasTyTop.
+Qed.
