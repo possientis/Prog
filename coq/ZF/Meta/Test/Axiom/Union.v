e7857fb3eb5bde21339d7269a7ace1d1f1b1474b
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-24 17:05:06 +0300

    Meta.Test.Axiom.Union

---
+++ coq/ZF/Meta/Test/Axiom/Union.v
@@ -0,0 +1,27 @@
+Require Import ZF.Meta.Ctx.
+Require Import ZF.Meta.Sig.
+Require Import ZF.Meta.Term.
+Require Import ZF.Meta.Ty.
+
+(* Source: forall a, exists b, forall x, x :< b <-> exists y, x :< y /\ y :< a. *)
+Definition Union : Term :=
+  All VarTySet
+    (Ex VarTySet
+      (All VarTySet
+        (Iff
+          (Elem (Var 0) (Var 1))
+          (Ex VarTySet
+            (And
+              (Elem (Var 1) (Var 0))
+              (Elem (Var 0) (Var 3))))))).
+
+(* The union example is a proposition in the empty environment.                 *)
+Proposition HasTy : HasTy Sig.empty Ctx.empty Union TyProp.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  apply HasTyAll, HasTyEx, HasTyAll, HasTyIff.
+  - apply HasTyElem; apply (HasTyVar _ _ _ VarTySet); reflexivity.
+  - apply HasTyEx, HasTyAnd.
+    + apply HasTyElem; apply (HasTyVar _ _ _ VarTySet); reflexivity.
+    + apply HasTyElem; apply (HasTyVar _ _ _ VarTySet); reflexivity.
+Qed.
