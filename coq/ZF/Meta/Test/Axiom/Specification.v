2f3d143f9206e729715aff5a21239cadfb1de876
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-25 14:39:11 +0300

    Meta.Test.Axiom.Specification

---
+++ coq/ZF/Meta/Test/Axiom/Specification.v
@@ -0,0 +1,29 @@
+Require Import ZF.Meta.Ctx.
+Require Import ZF.Meta.Sig.
+Require Import ZF.Meta.Term.
+Require Import ZF.Meta.Ty.
+
+(* forall P, forall a, exists b, forall x, x :< b <-> x :< a /\ P x             *)
+Definition Specification : Term :=
+  All VarTyClass
+    (All VarTySet
+      (Ex VarTySet
+        (All VarTySet
+          (Iff
+            (Elem (Var 0) (Var 1))
+            (And
+              (Elem (Var 0) (Var 2))
+              (App (Var 3) (Var 0))))))).
+
+(* The specification example is a proposition in the empty signature.           *)
+Proposition HasTy : HasTy Sig.empty Ctx.empty Specification TyProp.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  apply HasTyAll, HasTyAll, HasTyEx, HasTyAll, HasTyIff.
+  - apply HasTyElem; apply (HasTyVar _ _ _ VarTySet); reflexivity.
+  - apply HasTyAnd.
+    + apply HasTyElem; apply (HasTyVar _ _ _ VarTySet); reflexivity.
+    + apply HasTyApp.
+      * apply (HasTyVar _ _ _ VarTyClass). reflexivity.
+      * apply (HasTyVar _ _ _ VarTySet). reflexivity.
+Qed.
