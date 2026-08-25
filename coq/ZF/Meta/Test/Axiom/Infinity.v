7bbd2a800200d3c125283c71497bf28bcb637364
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-25 12:37:35 +0300

    Meta.Test.Axiom.Infinity

---
+++ coq/ZF/Meta/Test/Axiom/Infinity.v
@@ -0,0 +1,51 @@
+Require Import Coq.Lists.List.
+Require Import Coq.Strings.String.
+
+Require Import ZF.Meta.Ctx.
+Require Import ZF.Meta.Sig.
+Require Import ZF.Meta.Term.
+Require Import ZF.Meta.Ty.
+
+Import ListNotations.
+
+Definition sig : Sig := fun name =>
+  if String.eqb name "empty"  then Some ([]              , TySet) else
+  if String.eqb name "single" then Some ([TySet]         , TySet) else
+  if String.eqb name "union2" then Some ([TySet; TySet]  , TySet) else
+  None.
+
+(* exists a, empty :< a /\ forall x, x :< a -> union2 x (single x) :< a         *)
+Definition Infinity : Term :=
+  Ex VarTySet
+    (And
+      (Elem (Ident "empty" []) (Var 0))
+      (All VarTySet
+        (Imp
+          (Elem (Var 0) (Var 1))
+          (Elem
+            (Ident "union2" [Var 0; Ident "single" [Var 0]])
+            (Var 1))))).
+
+(* The infinity example is a proposition in the local test signature.           *)
+Proposition HasTy : HasTy sig Ctx.empty Infinity TyProp.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  apply HasTyEx, HasTyAnd.
+  - apply HasTyElem.
+    + apply HasTyIdent with (argTys := []). 1: reflexivity.
+      apply HasTysNil.
+    + apply (HasTyVar _ _ _ VarTySet). reflexivity.
+  - apply HasTyAll, HasTyImp.
+    + apply HasTyElem; apply (HasTyVar _ _ _ VarTySet); reflexivity.
+    + apply HasTyElem.
+      * apply HasTyIdent with (argTys := [TySet; TySet]). 1: reflexivity.
+        apply HasTysCons.
+        -- apply (HasTyVar _ _ _ VarTySet). reflexivity.
+        -- apply HasTysCons.
+           ++ apply HasTyIdent with (argTys := [TySet]). 1: reflexivity.
+              apply HasTysCons.
+              ** apply (HasTyVar _ _ _ VarTySet). reflexivity.
+              ** apply HasTysNil.
+           ++ apply HasTysNil.
+      * apply (HasTyVar _ _ _ VarTySet). reflexivity.
+Qed.
