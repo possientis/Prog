9afff6334a4606bbc2f7e426869addb10932226e
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-26 15:25:54 +0300

    Meta.Test.Class.Equiv.equiv

--- coq/ZF/Meta/Test/Class/Equiv.v
+++ coq/ZF/Meta/Test/Class/Equiv.v
@@ -12,14 +12,32 @@ Require Import ZF.Meta.Ty.
 
 Import ListNotations.
 
+(* Declarations.                                                                *)
+
+(* Definition toClass (a:U) : Class := fun x => x :< a.                         *)
 Definition toClass : Decl :=
   {| para := [TySet];
      res  := TyClass;
      body := Some (Lam (Elem (Var 0) (Var 1))) |}.
 
+(* Definition equiv (P Q:Class) : Prop := forall x, P x <-> Q x.                *)
+Definition equiv : Decl :=
+  {| para := [TyClass; TyClass];
+     res  := TyProp;
+     body := Some
+       (All VarTySet
+         (Iff
+           (App (Var 2) (Var 0))
+           (App (Var 1) (Var 0)))) |}.
+
+(* Environment.                                                                 *)
+
 Definition env : Env := fun name =>
   if String.eqb name "toClass" then Some toClass else
+  if String.eqb name "equiv"   then Some equiv else
   None.
+
+(* Declaration typing.                                                          *)
 
 (* The declaration body for toClass maps a set to its membership class.         *)
 Proposition toClassHasTyDecl : HasTyDecl Env.empty toClass.
@@ -27,6 +45,21 @@ Proof.
   (* Proof by Hermes + gpt 5.5                                                  *)
   apply HasTyLam, HasTyElem; apply (HasTyVar _ _ _ TySet); reflexivity.
 Qed.
+
+(* The declaration body for equivalence compares two classes pointwise.         *)
+Proposition equivHasTyDecl : HasTyDecl Env.empty equiv.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  apply HasTyAll, HasTyIff.
+  - apply HasTyApp.
+    + apply (HasTyVar _ _ _ TyClass). reflexivity.
+    + apply (HasTyVar _ _ _ TySet). reflexivity.
+  - apply HasTyApp.
+    + apply (HasTyVar _ _ _ TyClass). reflexivity.
+    + apply (HasTyVar _ _ _ TySet). reflexivity.
+Qed.
+
+(* Identifier typing.                                                           *)
 
 (* The identifier toClass sends a set argument to a class.                      *)
 Proposition toClassHasTy :
@@ -37,4 +70,18 @@ Proof.
   apply HasTysCons.
   - apply (HasTyVar _ _ _ TySet). reflexivity.
   - apply HasTysNil.
+Qed.
+
+(* The identifier equiv sends two class arguments to a proposition.             *)
+Proposition equivHasTy :
+  HasTyIn env [TyClass; TyClass]
+    (Ident "equiv" [Var 1; Var 0]) TyProp.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  apply HasTyIdent with (argTys := [TyClass; TyClass]). 1: reflexivity.
+  apply HasTysCons.
+  - apply (HasTyVar _ _ _ TyClass). reflexivity.
+  - apply HasTysCons.
+    + apply (HasTyVar _ _ _ TyClass). reflexivity.
+    + apply HasTysNil.
 Qed.
