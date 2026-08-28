c3bdaf14727f90254c00b9b43661b3ba9499b789
Paul Ossientis <paul_ossientis@riseup.net>
2026-08-28 11:51:48 +0300

    Meta.Proof

---
+++ coq/ZF/Meta/Proof/Decl.v
@@ -0,0 +1,18 @@
+Require Import Coq.Lists.List.
+
+Require Import ZF.Meta.Ctx.
+Require Import ZF.Meta.Proof.Syntax.
+Require Import ZF.Meta.Term.Syntax.
+Require Import ZF.Meta.Ty.
+
+Import ListNotations.
+
+(* A proof declaration records parameters, conclusion and optional proof body.  *)
+Record Decl : Type := mkDecl
+  { para  : list Ty
+  ; concl : Term
+  ; body  : option Proof
+  }.
+
+(* A proof declaration conclusion is checked in de Bruijn context order.        *)
+Definition ctx (d:Decl) : Ctx := rev (para d).
