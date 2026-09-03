f3a5cda9ee050b334587dad9bd6a3a3dc6fb79f5
Paul Ossientis <paul_ossientis@riseup.net>
2026-09-03 19:45:55 +0100

    Meta.Ctx: more

--- rocq/ZF/Meta/Ctx.v
+++ rocq/ZF/Meta/Ctx.v
@@ -14,3 +14,14 @@ Fixpoint typeOf (G:Ctx) (n:nat) : option Ty :=
   | ty  :: _  , 0   => Some ty
   | _   :: H  , S n => typeOf H n
   end.
+
+(* A lookup remains valid after adding newer variables in front.                *)
+Proposition TypeOfAppR :
+  forall (G D:Ctx) (n:nat) (ty:Ty),
+    typeOf D n = Some ty ->
+    typeOf (G ++ D) (length G + n) = Some ty.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros G D n ty H1.
+  induction G as [|ty' G IH]; assumption.
+Qed.
