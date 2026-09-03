56d9f49268769988a12fb45ce135ead7998ec649
Paul Ossientis <paul_ossientis@riseup.net>
2026-09-03 19:56:53 +0100

    Meta.Ctx: more

--- rocq/ZF/Meta/Ctx.v
+++ rocq/ZF/Meta/Ctx.v
@@ -18,10 +18,26 @@ Fixpoint typeOf (G:Ctx) (n:nat) : option Ty :=
 (* A lookup remains valid after adding newer variables in front.                *)
 Proposition TypeOfAppR :
   forall (G D:Ctx) (n:nat) (ty:Ty),
-    typeOf D n = Some ty ->
+    typeOf D n = Some ty                      ->
     typeOf (G ++ D) (length G + n) = Some ty.
 Proof.
   (* Proof by Hermes + gpt 5.5                                                  *)
   intros G D n ty H1.
   induction G as [|ty' G IH]; assumption.
+Qed.
+
+(* A lookup in the front context is unchanged by adding older variables.        *)
+Proposition TypeOfAppL :
+  forall (G D:Ctx) (n:nat) (ty:Ty),
+    typeOf G n = Some ty                      ->
+    typeOf (G ++ D) n = Some ty.
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros G D n ty H1.
+  generalize dependent n.
+  induction G as [|ty' G IH]; intros n H1.
+  - discriminate.
+  - destruct n as [|n].
+    + rewrite <- H1. reflexivity.
+    + apply IH. assumption.
 Qed.
