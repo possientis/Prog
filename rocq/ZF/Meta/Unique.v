fd6403c1912257234c3b27899ad02e9863e070d2
Paul Ossientis <paul_ossientis@riseup.net>
2026-09-06 16:18:50 +0100

    Meta.Unique.FromT

--- rocq/ZF/Meta/Unique.v
+++ rocq/ZF/Meta/Unique.v
@@ -1,3 +1,5 @@
+Require Import Coq.Arith.PeanoNat.
+
 Require Import ZF.Meta.Shift.
 Require Import ZF.Meta.Syntax.
 
@@ -9,3 +11,14 @@ Definition Unique (A:Term) : Term :=
         (Imp
           (App (shiftT 2 A) (Var 0))
           (Equal (Var 1) (Var 0))))).
+
+(* Lifting commutes with unique class membership.                               *)
+Proposition FromT : forall (A:Term) (i j:nat),
+  fromT i j (Unique A) = Unique (fromT i j A).
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros A i j. unfold Unique, shiftT. simpl.
+  assert (i + 2 = S (S i)) as H1. {
+    rewrite Nat.add_succ_r. rewrite Nat.add_1_r. reflexivity. }
+  rewrite <- H1, CommT. 2: apply Nat.le_0_l. reflexivity.
+Qed.
