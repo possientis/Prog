50689c6befd5ecd6f00652e3395fac7854ca0a8c
Paul Ossientis <paul_ossientis@riseup.net>
2026-09-12 12:22:00 +0100

    Meta.Check.Shift.Above

--- rocq/ZF/Meta/Check/Shift.v
+++ rocq/ZF/Meta/Check/Shift.v
@@ -2,6 +2,7 @@ Require Import Coq.Arith.PeanoNat.
 Require Import Coq.Lists.List.
 
 Require Import ZF.Meta.Check.Core.
+Require Import ZF.Meta.Check.Induction.
 Require Import ZF.Meta.Ctx.
 Require Import ZF.Meta.Env.
 Require Import ZF.Meta.Shift.
@@ -28,4 +29,72 @@ Proof.
   - assert ((n <? length G) = false) as H3. { apply Nat.ltb_ge. assumption. }
     rewrite H3. apply CheckVar.
     apply (TypeOf.ThreeR G M D); assumption.
+Qed.
+
+(* Lifting above a checked object leaves it unchanged.                          *)
+Proposition Above : forall (E:Env),
+  (forall (G:Ctx) (t:Term) (ty:Ty),
+    CheckT E G t ty -> forall (i j:nat), length G <= i ->
+    Shift.fromT i j t = t)                                               /\
+  (forall (G:Ctx) (ts:Terms) (tys:list Ty),
+    CheckTs E G ts tys -> forall (i j:nat), length G <= i ->
+    Shift.fromTs i j ts = ts)                                            /\
+  (forall (G:Ctx) (p:Proof) (t:Term),
+    CheckP E G p t -> forall (i j:nat), length G <= i ->
+    Shift.fromP i j p = p).
+Proof.
+  (* Proof by Hermes + gpt 5.5                                                  *)
+  intros E.
+  apply Induction.
+  - intros G i j H1. reflexivity.
+  - intros G i j H1. reflexivity.
+  - intros G n ty H1 i j H2. simpl.
+    (* A checked variable is below the length of its context.                   *)
+    assert (n < length G) as H3. { apply (TypeOf.LtLength G n ty). assumption. }
+    assert (n < i) as H4. { apply Nat.lt_le_trans with (m := length G); assumption. }
+    assert ((n <? i) = true) as H5. { apply Nat.ltb_lt. assumption. }
+    rewrite H5. reflexivity.
+  - intros G ty i j H1. reflexivity.
+  - intros G name args tys ty H1 H2 H3 i j H4. simpl.
+    rewrite H3. reflexivity. assumption.
+  - intros G x y H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G x y H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G x y H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G x y H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G x y H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G x y H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G x y H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G p q H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G p q H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G p q H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G p q H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G p H1 H2 i j H3. simpl. rewrite H2; try assumption. reflexivity.
+  - intros G p H1 H2 i j H3. simpl.
+    rewrite H2. reflexivity. apply le_n_S. assumption.
+  - intros G p H1 H2 i j H3. simpl.
+    rewrite H2. reflexivity. apply le_n_S. assumption.
+  - intros G p H1 H2 i j H3. simpl.
+    rewrite H2. reflexivity. apply le_n_S. assumption.
+  - intros G A x H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G A p q H1 H2 H3 H4 H5 H6 i j H7. simpl.
+    rewrite H2, H4, H6; try assumption. reflexivity.
+  - intros G i j H1. reflexivity.
+  - intros G t ts ty tys H1 H2 H3 H4 i j H5. simpl.
+    rewrite H2, H4; try assumption. reflexivity.
+  - intros G t H1 H2 i j H3. simpl. rewrite H2; try assumption. reflexivity.
+  - intros G t H1 H2 i j H3. simpl. rewrite H2; try assumption. reflexivity.
+  - intros G name args tys t H1 H2 H3 i j H4. simpl.
+    rewrite H3. reflexivity. assumption.
 Qed.
